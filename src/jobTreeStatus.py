fb50512d74ac0c9af084adfb9cb8d9a72a13d614
Benedict <benedictpaten@gmail.com>
2012-06-13 11:40:55 -0700

    Fix to jobtree status

--- src/jobTreeStatus.py
+++ src/jobTreeStatus.py
@@ -109,6 +109,8 @@ def main():
     if len(jobFiles) > 0:
         logger.info("Collating the colours of the job tree")
         for job, jobFile, in jobFiles:
+            if not colours.has_key(job.getColour()):
+                colours[job.getColour()] == 0
             colours[job.getColour()] += 1
     else:
         logger.info("There are no jobs to collate")
