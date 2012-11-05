1f063a39519f969d9381b60de13d18f6823e0c4d
Dent Earl <dearl@soe.ucsc.edu>
2012-11-05 09:44:06 -0800

    fix for argparse based option parsing

--- src/jobTreeRun.py
+++ src/jobTreeRun.py
@@ -139,55 +139,55 @@ def addOptions_argparse(parser):
     # BEFORE YOU ADD OR REMOVE OPTIONS TO THIS FUNCTION, BE SURE TO MAKE THE SAME CHANGES TO 
     # addOptions_optparse() OTHERWISE YOU WILL BREAK THINGS
     ##################################################
-    parser.add_option("--command", dest="command", default=None,
-                      help="The command to run (which will generate subsequent jobs)")
-    parser.add_option("--jobTree", dest="jobTree", default=None,
-                      help=("Directory in which to place job management files "
-                            "(this needs to be globally accessible by all machines running jobs).\n"
-                            "If you pass an existing directory it will check if it's a valid existing "
-                            "job tree, then try and restart the jobs in it"))
-    parser.add_option("--batchSystem", dest="batchSystem", default=detectQueueSystem(),
-                      help=("The type of batch system to run the job(s) with, currently can "
-                            "be 'singleMachine'/'parasol'/'acidTest'/'gridEngine'. default=%(default)s"))
-    parser.add_option("--parasolCommand", dest="parasolCommand", default="parasol",
-                      help="The command to run the parasol program default=%(default)s")
-    parser.add_option("--retryCount", dest="retryCount", type=int, default=0,
-                      help=("Number of times to try a failing job before giving up "
-                            "and labelling job failed. default=%(default)s"))
-    parser.add_option("--rescueJobsFrequency", dest="rescueJobsFrequency", type=int,
-                      help=("Period of time to wait (in seconds) between checking for "
-                            "missing/overlong jobs (default is set by the batch system)"))
-    parser.add_option("--maxJobDuration", dest="maxJobDuration", type=int, default=sys.maxint,
-                      help=("Maximum runtime of a job (in seconds) before we kill it "
-                            "(this is an approximate time, and the actual time before "
-                            "killing the job may be longer). default=%(default)s"))
-    parser.add_option("--jobTime", dest="jobTime", type=int, default=30,
-                      help=("The approximate time (in seconds) that you'd like a list of child "
-                            "jobs to be run serially before being parallised. This parameter allows "
-                            "one to avoid over parallelising tiny jobs, and therefore paying "
-                            "significant scheduling overhead, by running tiny jobs in series on a "
-                            "single node/core of the cluster. default=%(default)s"))
-    parser.add_option("--maxLogFileSize", dest="maxLogFileSize", type=int, default=50120,
-                      help=("The maximum size of a log file to keep (in bytes), log files "
-                            "larger than this will be truncated to the last X bytes. Default "
-                            "is 50 kilobytes, default=%(default)s"))
-    parser.add_option("--defaultMemory", dest="defaultMemory", default=2147483648,
-                      help=("The default amount of memory to request for a job (in bytes), "
-                            "by default is 2^31 = 2 gigabytes, default=%(default)s"))
-    parser.add_option("--defaultCpu", dest="defaultCpu", type=int, default=1,
-                      help="The default the number of cpus to dedicate a job. default=%(default)s")
-    parser.add_option("--maxJobs", dest="maxJobs", type=int, default=sys.maxint,
-                      help=("The maximum number of jobs to issue to the batch system at any one "
-                            "time. default=%(default)s"))
-    parser.add_option("--maxThreads", dest="maxThreads", type=int, default=4,
-                      help=("The maximum number of threads to use when running in single machine "
-                            "mode. default=%(default)s"))
-    parser.add_option("--stats", dest="stats", action="store_true", default=False,
-                      help="Records statistics about the job-tree to be used by jobTreeStats. default=%(default)s")
-    parser.add_option("--reportAllJobLogFiles", dest="reportAllJobLogFiles", action="store_true", default=False,
-                      help="Report the log files of all jobs, not just that fail. default=%(default)s")
-    parser.add_option("--noCheckPoints", dest="noCheckPoints", action="store_true", default=False,
-                      help="Switch off checkpointing in the master to speed up job processing default=%(default)s")
+    parser.add_argument("--command", dest="command", default=None,
+                        help="The command to run (which will generate subsequent jobs)")
+    parser.add_argument("--jobTree", dest="jobTree", default=None,
+                        help=("Directory in which to place job management files "
+                              "(this needs to be globally accessible by all machines running jobs).\n"
+                              "If you pass an existing directory it will check if it's a valid existing "
+                              "job tree, then try and restart the jobs in it"))
+    parser.add_argument("--batchSystem", dest="batchSystem", default=detectQueueSystem(),
+                        help=("The type of batch system to run the job(s) with, currently can "
+                              "be 'singleMachine'/'parasol'/'acidTest'/'gridEngine'. default=%(default)s"))
+    parser.add_argument("--parasolCommand", dest="parasolCommand", default="parasol",
+                        help="The command to run the parasol program default=%(default)s")
+    parser.add_argument("--retryCount", dest="retryCount", type=int, default=0,
+                        help=("Number of times to try a failing job before giving up "
+                              "and labelling job failed. default=%(default)s"))
+    parser.add_argument("--rescueJobsFrequency", dest="rescueJobsFrequency", type=int,
+                        help=("Period of time to wait (in seconds) between checking for "
+                              "missing/overlong jobs (default is set by the batch system)"))
+    parser.add_argument("--maxJobDuration", dest="maxJobDuration", type=int, default=sys.maxint,
+                        help=("Maximum runtime of a job (in seconds) before we kill it "
+                              "(this is an approximate time, and the actual time before "
+                              "killing the job may be longer). default=%(default)s"))
+    parser.add_argument("--jobTime", dest="jobTime", type=int, default=30,
+                        help=("The approximate time (in seconds) that you'd like a list of child "
+                              "jobs to be run serially before being parallised. This parameter allows "
+                              "one to avoid over parallelising tiny jobs, and therefore paying "
+                              "significant scheduling overhead, by running tiny jobs in series on a "
+                              "single node/core of the cluster. default=%(default)s"))
+    parser.add_argument("--maxLogFileSize", dest="maxLogFileSize", type=int, default=50120,
+                        help=("The maximum size of a log file to keep (in bytes), log files "
+                              "larger than this will be truncated to the last X bytes. Default "
+                              "is 50 kilobytes, default=%(default)s"))
+    parser.add_argument("--defaultMemory", dest="defaultMemory", default=2147483648,
+                        help=("The default amount of memory to request for a job (in bytes), "
+                              "by default is 2^31 = 2 gigabytes, default=%(default)s"))
+    parser.add_argument("--defaultCpu", dest="defaultCpu", type=int, default=1,
+                        help="The default the number of cpus to dedicate a job. default=%(default)s")
+    parser.add_argument("--maxJobs", dest="maxJobs", type=int, default=sys.maxint,
+                        help=("The maximum number of jobs to issue to the batch system at any one "
+                              "time. default=%(default)s"))
+    parser.add_argument("--maxThreads", dest="maxThreads", type=int, default=4,
+                        help=("The maximum number of threads to use when running in single machine "
+                              "mode. default=%(default)s"))
+    parser.add_argument("--stats", dest="stats", action="store_true", default=False,
+                        help="Records statistics about the job-tree to be used by jobTreeStats. default=%(default)s")
+    parser.add_argument("--reportAllJobLogFiles", dest="reportAllJobLogFiles", action="store_true", default=False,
+                        help="Report the log files of all jobs, not just that fail. default=%(default)s")
+    parser.add_argument("--noCheckPoints", dest="noCheckPoints", action="store_true", default=False,
+                        help="Switch off checkpointing in the master to speed up job processing default=%(default)s")
 def loadTheBatchSystem(config):
     """Load the batch system.
     """
