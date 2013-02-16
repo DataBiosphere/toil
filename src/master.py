b9f405ea8a18ead142ad1375c82a89887d15eaae
benedictpaten <benedictpaten@gmail.com>
2013-02-15 20:38:27 -0800

    test

--- src/master.py
+++ src/master.py
@@ -18,7 +18,7 @@
 #AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
+#THE SOFTWARE. 
 
 """The master component (of a master slave pattern) for a job manager used by 
 sontrace programs (cactus etc) for running hierarchical trees of jobs on the 
