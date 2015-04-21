226db29f68ea41b36363787212714cf2012cc860
Hannes Schmidt <hannes@ucsc.edu>
2015-04-21 16:29:59 -0700

    Improved workaround for target functions defined in __main__

--- scriptTree/target.py
+++ scriptTree/target.py
@@ -180,16 +180,34 @@ class FunctionWrappingTarget(Target):
         Target.__init__(self, time=time, memory=time, cpu=time)
         moduleName = fn.__module__
         if moduleName== '__main__':
-            #looks up corresponding module in sys.modules, gets base name, drops .py
-            moduleName = os.path.basename(sys.modules[moduleName].__file__)
-            moduleName = moduleName[:-3]
-        self.fnModule = str(moduleName) #Module of function
+            # FIXME: Document why we are doing this, explain why it works in single node
+            # TODO: Review with Benedict
+            # looks up corresponding module in sys.modules, gets base name, drops .py or .pyc
+            moduleDir,moduleName = os.path.split(sys.modules[moduleName].__file__)
+            if moduleName.endswith('.py'):
+                moduleName = moduleName[:-3]
+            elif moduleName.endswith('.pyc'):
+                moduleName = moduleName[:-4]
+            else:
+                raise RuntimeError(
+                    "Can only handle main modules loaded from .py or .pyc files, but not '%s'" %
+                    moduleName )
+        else:
+            moduleDir = None
+
+        self.fnModuleDir = moduleDir
+        self.fnModule = moduleName #Module of function
         self.fnName = str(fn.__name__) #Name of function
         self.args=args
         self.kwargs=kwargs
-        
+
+    def _getFunc( self ):
+        if self.fnModuleDir not in sys.path:
+            sys.path.append( self.fnModuleDir )
+        return getattr( importlib.import_module( self.fnModule ), self.fnName )
+
     def run(self):
-        func = getattr(importlib.import_module(self.fnModule), self.fnName)
+        func = self._getFunc( )
         func(*self.args, **self.kwargs)
 
 class TargetFunctionWrappingTarget(FunctionWrappingTarget):
@@ -200,8 +218,5 @@ class TargetFunctionWrappingTarget(FunctionWrappingTarget):
     Target function can not be closure.
     """
     def run(self):
-        print "module: "+self.fnModule
-        print "cwd: "+os.getcwd()
-        print "sys Path: "+repr(sys.path)
-        func = getattr(importlib.import_module(self.fnModule), self.fnName) # self.fnName
+        func = self._getFunc( )
         func(*((self,) + tuple(self.args)), **self.kwargs)
