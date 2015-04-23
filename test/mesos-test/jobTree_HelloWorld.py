0bab75d0a8d1941d9c4d29895c12be55df95dbab
cket <cketchum@ucsc.edu>
2015-04-23 10:01:09 -0700

    Added simple unit test for mesos batch system

--- test/mesos-test/jobTree_HelloWorld.py
+++ test/mesos-test/jobTree_HelloWorld.py
@@ -20,8 +20,7 @@ def hello_world_child(target):
     with open ('hello_world_child.txt', 'w') as file:
         file.write('Sorry, the cake is a lie.')
 
-if __name__ == '__main__':
-
+def main():
     # Boilerplate -- startJobTree requires options
     parser = OptionParser()
     Stack.addJobTreeOptions(parser)
@@ -29,3 +28,6 @@ if __name__ == '__main__':
 
     # Setup the job stack and launch jobTree job
     i = Stack(Target.makeTargetFn(hello_world)).startJobTree(options)
+
+if __name__ == '__main__':
+    main()
