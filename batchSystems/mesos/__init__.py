461f65296ce44ede734f27dd619539b23c7e8486
cket <cketchum@ucsc.edu>
2015-04-28 12:43:01 -0700

    Refactor to improve clarity and conciseness
    MesosFrameWork thread was combined with MesosScheduler to avoid passing constructor arguments from batch system to framework thread to scheduler. All references to those fields are now done via scheduler.field instead of each object possessing their own reference.

--- batchSystems/mesos/__init__.py
+++ batchSystems/mesos/__init__.py
@@ -1,35 +1,20 @@
 import os
-import sys
-from jobTree.batchSystems.mesos.mesosScheduler import MesosSchedulerDriver, MesosScheduler
+import time
+from jobTree.batchSystems.mesos.mesosScheduler import MesosScheduler
 from jobTree.batchSystems.mesos.JobTreeJob import JobTreeJob
 from jobTree.batchSystems.abstractBatchSystem import AbstractBatchSystem
 from jobTree.batchSystems.mesos import mesosExecutor
-from threading import Thread
 from Queue import Queue
 from sonLib.bioio import logger
-from mesos.interface import mesos_pb2
 
 
 class MesosBatchSystem(AbstractBatchSystem):
     def __init__(self, config, maxCpus, maxMemory):
         AbstractBatchSystem.__init__(self, config, maxCpus, maxMemory)
-        # job type queue is a dictionary of queues. Jobs are grouped in queues with resource requirements as keys.
-        self.queueDictionary = {}
-        # TODO: I never see any evidence of this queue being read from
-        self.killQueue = Queue()
-        # TODO: I never see any evidence of this field ever being used
-        self.killedQueue = Queue()
-        # TODO: Can we combine this with some
-        self.runningDictionary = {}
-        self.updatedJobsQueue = Queue()
+        self.killedQueue = Queue() # implement in scheduler
         self.currentJobs = []
         self.nextJobID = 0
-        # TODO: stick with one naming convention as much as possible
-        # TODO: I don't like the way all this state is threaded through from here down into the Scheduler instance ...
-        # ... Why not just pass a reference to the MesosBatchSystem around? Ask me to explain.
-        self.mesosThread = MesosFrameWorkThread(queue_dictionary=self.queueDictionary, master_ip="127.0.0.1:5050",
-                                                 kill_queue=self.killQueue, running_dictionary=self.runningDictionary,
-                                                 updated_job_queue=self.updatedJobsQueue)
+        self.mesosThread = MesosScheduler(masterIP="127.0.0.1:5050")
         self.mesosThread.setDaemon(True)
         self.mesosThread.start()
 
@@ -45,11 +30,11 @@ class MesosBatchSystem(AbstractBatchSystem):
         job_type = job.resources
 
         # if job type already described, add to queue. If not, create dictionary entry & add.
-        if job_type in self.queueDictionary:
-            self.queueDictionary[job_type].put(job)
+        if job_type in self.mesosThread.jobQueueDict:
+            self.mesosThread.jobQueueDict[job_type].put(job)
         else:
-            self.queueDictionary[job_type] = Queue()
-            self.queueDictionary[job_type].put(job)
+            self.mesosThread.jobQueueDict[job_type] = Queue()
+            self.mesosThread.jobQueueDict[job_type].put(job)
 
         logger.debug("Issued the job command: %s with job id: %s " % (command, str(jobID)))
         return jobID
@@ -57,21 +42,23 @@ class MesosBatchSystem(AbstractBatchSystem):
     def killJobs(self, jobIDs):
         # kills jobs described by jobIDs
         for jobID in jobIDs:
-            self.killQueue.put(jobID)
+            self.mesosThread.killQueue.put(jobID)
 
     def getIssuedJobIDs(self):
         # returning from our local current jobs, not those passed to mesos
         return list(self.currentJobs)
 
     def getRunningJobIDs(self):
-        return self.runningDictionary
+        currentTime= dict()
+        for x in self.mesosThread.jobTimeMap:
+            currentTime[x.key]= time.time()-x.value
 
     def getUpdatedJob(self, maxWait):
-        i = self.getFromQueueSafely(self.updatedJobsQueue, maxWait)
+        i = self.getFromQueueSafely(self.mesosThread.updatedJobsQueue, maxWait)
         if i == None:
             return None
         jobID, retcode = i
-        self.updatedJobsQueue.task_done()
+        self.mesosThread.updatedJobsQueue.task_done()
         return i
 
     def getWaitDuration(self):
@@ -84,103 +71,4 @@ class MesosBatchSystem(AbstractBatchSystem):
         return 1800 #Half an hour
 
     def getJobs(self):
-        return self.queueDictionary
-
-
-class MesosFrameWorkThread(Thread):
-    def __init__(self, queue_dictionary, master_ip, kill_queue, running_dictionary, updated_job_queue):
-        Thread.__init__(self)
-        self.killQueue = kill_queue
-        self.runningDictionary = running_dictionary
-        self.updatedJobQueue = updated_job_queue
-        self.queueDictionary = queue_dictionary
-        self.masterIP = master_ip
-
-    @staticmethod
-    def executorScriptPath():
-        path = mesosExecutor.__file__
-        if path.endswith('.pyc'):
-            path = path[:-1]
-        return path
-
-    def start_framework(self):
-
-        executor = mesos_pb2.ExecutorInfo()
-        executor.executor_id.value = "MesosExecutor"
-        executor.command.value = self.executorScriptPath()
-        executor.name = "Test Executor (Python)"
-        executor.source = "python_test"
-
-        cpus = executor.resources.add()
-        cpus.name = "cpus"
-        cpus.type = mesos_pb2.Value.SCALAR
-        cpus.scalar.value = 0.1
-
-        mem = executor.resources.add()
-        mem.name = "mem"
-        mem.type = mesos_pb2.Value.SCALAR
-        mem.scalar.value = 32
-
-        framework = mesos_pb2.FrameworkInfo()
-        framework.user = "" # Have Mesos fill in the current user.
-        framework.name = "JobTree Framework (Python)"
-
-        # TODO(vinod): Make checkpointing the default when it is default
-        # on the slave.
-        if os.getenv("MESOS_CHECKPOINT"):
-            print "Enabling checkpoint for the framework"
-            framework.checkpoint = True
-
-        implicitAcknowledgements = 1
-        if os.getenv("MESOS_EXPLICIT_ACKNOWLEDGEMENTS"):
-            print "Enabling explicit status update acknowledgements"
-            implicitAcknowledgements = 0
-
-        if os.getenv("MESOS_AUTHENTICATE"):
-
-            # TODO: let's delete this branch and replace it with raising a NotImplementedError
-
-            print "Enabling authentication for the framework"
-
-            if not os.getenv("DEFAULT_PRINCIPAL"):
-                print "Expecting authentication principal in the environment"
-                sys.exit(1)
-
-            if not os.getenv("DEFAULT_SECRET"):
-                print "Expecting authentication secret in the environment"
-                sys.exit(1)
-
-            credential = mesos_pb2.Credential()
-            credential.principal = os.getenv("DEFAULT_PRINCIPAL")
-            credential.secret = os.getenv("DEFAULT_SECRET")
-
-            framework.principal = os.getenv("DEFAULT_PRINCIPAL")
-
-            driver = MesosSchedulerDriver(
-                MesosScheduler(implicitAcknowledgements=implicitAcknowledgements, executor=executor, job_queues=self.queueDictionary,
-                               kill_queue=self.killQueue,
-                               running_dictionary=self.runningDictionary, updated_job_queue=self.updatedJobQueue),
-                framework,
-                self.masterIP,
-                implicitAcknowledgements,
-                credential)
-        else:
-            framework.principal = "test-framework-python"
-
-            driver = MesosSchedulerDriver(
-                MesosScheduler(implicitAcknowledgements=implicitAcknowledgements, executor=executor, job_queues=self.queueDictionary,
-                               kill_queue=self.killQueue,
-                               running_dictionary=self.runningDictionary, updated_job_queue=self.updatedJobQueue),
-                framework,
-                self.masterIP,
-                implicitAcknowledgements)
-
-        driver_result = driver.run()
-        status = 0 if driver_result == mesos_pb2.DRIVER_STOPPED else 1
-
-        # Ensure that the driver process terminates.
-        driver.stop()
-        sys.exit(status)
-
-    def run(self):
-        self.start_framework()
+        return self.mesosThread.jobQueueDict
--- batchSystems/mesos/mesosScheduler.py
+++ batchSystems/mesos/mesosScheduler.py
@@ -1,13 +1,18 @@
 from collections import namedtuple
 import pickle
 import mesos.interface
+import time
 import sys
 import os
 import Queue
 import mesos.native
 from mesos.native import MesosSchedulerDriver
+from threading import Thread
 from mesos.interface import mesos_pb2
-from jobTree.batchSystems.mesos import JobTreeJob, ResourceSummary
+from jobTree.batchSystems.mesos import JobTreeJob, ResourceRequirement
+import jobTree.batchSystems.abstractBatchSystem
+from jobTree.batchSystems.mesos import mesosExecutor
+import logging
 
 # TODO: document class with a short docstring
 
@@ -16,56 +21,69 @@ from jobTree.batchSystems.mesos import JobTreeJob, ResourceSummary
 # TODO: document each method, especially callbacks
 
 # TODO: rename internal method to start with double underscore (ask me for explanation)
+log = logging.getLogger( __name__ )
+class MesosScheduler(mesos.interface.Scheduler, Thread):
+    """
+    Part of mesos framework. Communicates via the MesosSchedulerDriver, which is launched
+    in the run() method. Instantiated in MesosBatchSystem.
+    """
+    def __init__(self, masterIP):
+        Thread.__init__(self)
 
-class MesosScheduler(mesos.interface.Scheduler):
-    def __init__(self, implicitAcknowledgements, executor, job_queues, kill_queue, running_dictionary, updated_job_queue):
-            # TODO: Document what each field is used for
-            self.jobQueues = job_queues
-            # TODO: is this used anywhere?
-            self.killQueue = kill_queue
-            self.runningDictionary = running_dictionary
-            self.updatedJobQueue = updated_job_queue
-            self.taskData = {}
+        self.jobQueueDict = {}
+        self.masterIP=masterIP
+        self.killQueue = Queue.Queue()
+        self.jobTimeMap = {}
+        self.updatedJobsQueue = Queue.Queue()
+        self.taskExecutorMap = {}
 
-            self.implicitAcknowledgements = implicitAcknowledgements
-            self.executor = executor
+        self.implicitAcknowledgements = self.getImplicit()
+        self.executor = self.buildExecutor()
 
-            self.tasksLaunched = 0
-            self.tasksFinished = 0
-            self.messagesSent = 0
-            self.messagesReceived = 0
+        self.tasksLaunched = 0
+        self.tasksFinished = 0
+        self.messagesSent = 0
+        self.messagesReceived = 0
 
     def registered(self, driver, frameworkId, masterInfo):
-        print "Registered with framework ID %s" % frameworkId.value
+        """
+        Invoked when the scheduler successfully registers with a Mesos master.
+
+        """
+        log.debug("Registered with framework ID %s" % frameworkId.value)
 
     def reregistered(self, driver, masterInfo):
-        print "I am still registered with the master."
+        """
+        Invoked when the scheduler re-registers with a newly elected Mesos master.
+        """
+        log.debug("Registered with new master")
 
     def executorLost(self, driver, executorId, slaveId, status):
-        print "executor %s lost".format(executorId)
+        """
+        Invoked when an executor has exited/terminated.
+        """
+        log.warning("executor %s lost.".format(executorId))
 
     @staticmethod
-    def bytesToMB(mem):
-        # TODO: shouldn't this be mem / 1024 / 1024 ?
-        return mem/1000000
+    def __bytesToMB(mem):
+        return mem/1024/1024
 
     def resourceOffers(self, driver, offers):
-
-        job_types = list(self.jobQueues.keys())
+        """
+        Invoked when resources have been offered to this framework.
+        """
+        job_types = list(self.jobQueueDict.keys())
         # sorts from largest to smallest cpu usage
-        # TODO: ResourceSummary should be resourceSummary
         # TODO: add a size() method to ResourceSummary and use it as the key. Ask me why.
-        job_types.sort(key=lambda ResourceSummary: ResourceSummary.cpu)
+        job_types.sort(key=lambda resourceRequirement: ResourceRequirement.ResourceRequirement.cpu)
         job_types.reverse()
 
         # right now, gives priority to largest jobs
         for offer in offers:
-            # TODO: the comment below should describe in more detail what race it is preventing
-            #prevents race condition bug
-            if len(job_types)==0:
-                driver.declineOffer(offer.id)
-                # TODO: What about the other offers? Shouldn't we decline them, too?
-                return
+            # if len(job_types)==0:
+            #     print "reject offer {}".format(offer.id.value)
+            #     driver.declineOffer(offer.id)
+            #     return
             tasks = []
 
             # TODO: In an offer, can there ever be more than one resource with the same name?
@@ -77,44 +95,40 @@ class MesosScheduler(mesos.interface.Scheduler):
                 elif resource.name == "mem":
                     offerMem += resource.scalar.value
 
-            print "Received offer %s with cpus: %s and mem: %s" \
-                  % (offer.id.value, offerCpus, offerMem)
+            log.debug( "Received offer %s with cpus: %s and mem: %s" \
+                  % (offer.id.value, offerCpus, offerMem))
 
             remainingCpus = offerCpus
             remainingMem = offerMem
 
             for job_type in job_types:
-                print "Memory Req: "+str(job_type.memory)
-                print "CPU Req "+str(job_type.cpu)
-                print "Unique Job Types: "+str(len(job_types))
-                # TODO: I don't understand the commment below
-                #not part of task object
-                while (not self.jobQueues[job_type].empty()) and \
+                while (not self.jobQueueDict[job_type].empty()) and \
                                 remainingCpus >= job_type.cpu and \
-                                remainingMem >= self.bytesToMB(job_type.memory): #job tree specifies its resources in bytes.
-                    jt_job = self.jobQueues[job_type].get()
+                                remainingMem >= self.__bytesToMB(job_type.memory): #job tree specifies its resources in bytes.
+                    jt_job = self.jobQueueDict[job_type].get()
 
-                    # TODO: If this is something that needs to be changed, create a FIXME comment for it
-                    # maps the id to the time running. Right now all running time is 1
-                    self.runningDictionary[jt_job.jobID] = 1
+                    self.jobTimeMap[jt_job.jobID] = time.time()
 
                     task = self.createTask(jt_job, offer)
-                    print "Launching mesos task %s using offer %s" \
-                          % (task.task_id.value, offer.id.value)
+                    log.debug( "Launching mesos task %s using offer %s" \
+                          % (task.task_id.value, offer.id.value))
 
                     tasks.append(task)
                     # TODO: You might want to simply place the entire task object into that dictionary
                     # TODO: When are entries removed from that dictionary?
-                    self.taskData[task.task_id.value] = (
+                    self.taskExecutorMap[task.task_id.value] = (
                         offer.slave_id, task.executor.executor_id)
 
                     remainingCpus -= job_type.cpu
-                    remainingMem -= self.bytesToMB(job_type.memory)
-            # TODO: this comment is a bit ominous
-            #if we launch offers in for loop, they are invalid...
+                    remainingMem -= self.__bytesToMB(job_type.memory)
             driver.launchTasks(offer.id, tasks)
+            # If we put the launch call inside the while, multiple accepts are used on the same offer. We dont want that.
+            # this explains why it works in the simple hello_world case: there is only one jobType, so offer is accepted once.
 
     def createTask(self, jt_job, offer):
+        """
+        build the mesos task object from the jobTree job
+        """
         tid = self.tasksLaunched
         self.tasksLaunched += 1
         task = mesos_pb2.TaskInfo()
@@ -140,8 +154,8 @@ class MesosScheduler(mesos.interface.Scheduler):
 
 
     def statusUpdate(self, driver, update):
-        print "Task %s is in a state %s" % \
-            (update.task_id.value, mesos_pb2.TaskState.Name(update.state))
+        log.debug( "Task %s is in a state %s" % \
+            (update.task_id.value, mesos_pb2.TaskState.Name(update.state)))
 
         # TODO: I think this is left over from the example and should be removed
         # Ensure the binary data came through.
@@ -149,17 +163,17 @@ class MesosScheduler(mesos.interface.Scheduler):
             print "The update data did not match!"
             print "  Expected: 'data with a \\x00 byte'"
             print "  Actual:  ", repr(str(update.data))
-            self.updatedJobQueue.put((int(update.task_id.value), 1))
+            self.updatedJobsQueue.put((int(update.task_id.value), 1))
             # message not going through. What exactly does this mean for the slave?
 
         if update.state == mesos_pb2.TASK_FINISHED:
             self.tasksFinished += 1
-            self.updatedJobQueue.put((int(update.task_id.value), 0))
+            self.updatedJobsQueue.put((int(update.task_id.value), 0))
             # problem: queues are approximate. Just make this queue.empty()?
-            if self.tasksFinished == len(self.runningDictionary):
-                print "All tasks done, waiting for final framework message"
+            if self.tasksFinished == len(self.jobTimeMap):
+                log.debug( "All tasks done, waiting for final framework message")
 
-            slave_id, executor_id = self.taskData[update.task_id.value]
+            slave_id, executor_id = self.taskExecutorMap[update.task_id.value]
 
             self.messagesSent += 1
             driver.sendFrameworkMessage(
@@ -170,10 +184,10 @@ class MesosScheduler(mesos.interface.Scheduler):
         if update.state == mesos_pb2.TASK_LOST or \
            update.state == mesos_pb2.TASK_KILLED or \
            update.state == mesos_pb2.TASK_FAILED:
-            print "Task %s is in unexpected state %s with message '%s'" \
-                % (update.task_id.value, mesos_pb2.TaskState.Name(update.state), update.message)
+            log.warning( "Task %s is in unexpected state %s with message '%s'" \
+                % (update.task_id.value, mesos_pb2.TaskState.Name(update.state), update.message))
             # driver.abort()
-            self.updatedJobQueue.put((int(update.task_id.value), 1))
+            self.updatedJobsQueue.put((int(update.task_id.value), 1))
 
         # Explicitly acknowledge the update if implicit acknowledgements
         # are not being used.
@@ -181,6 +195,9 @@ class MesosScheduler(mesos.interface.Scheduler):
             driver.acknowledgeStatusUpdate(update)
 
     def frameworkMessage(self, driver, executorId, slaveId, message):
+        """
+        Invoked when an executor sends a message.
+        """
         self.messagesReceived += 1
 
         # The message bounced back as expected.
@@ -192,9 +209,99 @@ class MesosScheduler(mesos.interface.Scheduler):
         print "Received message:", repr(str(message))
 
         # probably doesnt work. running dictionary can shrink.
-        if self.messagesReceived == len(self.runningDictionary):
+        if self.messagesReceived == len(self.jobTimeMap):
             if self.messagesReceived != self.messagesSent:
-                print "ERROR: sent", self.messagesSent,
-                print "but received", self.messagesReceived
+                log.error( "ERROR: sent {} but recieved {}".format(self.messagesSent,self.messagesReceived))
                 #sys.exit(1)
-            print "All tasks done, and all messages received, waiting for more tasks"
+            log.debug( "All tasks done, and all messages received, waiting for more tasks")
+
+    @staticmethod
+    def executorScriptPath():
+        path = mesosExecutor.__file__
+        if path.endswith('.pyc'):
+            path = path[:-1]
+        return path
+
+    def buildExecutor(self):
+        executor = mesos_pb2.ExecutorInfo()
+        executor.executor_id.value = "MesosExecutor"
+        executor.command.value = self.executorScriptPath()
+        executor.name = "Test Executor (Python)"
+        executor.source = "python_test"
+
+        cpus = executor.resources.add()
+        cpus.name = "cpus"
+        cpus.type = mesos_pb2.Value.SCALAR
+        cpus.scalar.value = 0.1
+
+        mem = executor.resources.add()
+        mem.name = "mem"
+        mem.type = mesos_pb2.Value.SCALAR
+        mem.scalar.value = 32
+        return executor
+
+    def getImplicit(self):
+        implicitAcknowledgements = 1
+        if os.getenv("MESOS_EXPLICIT_ACKNOWLEDGEMENTS"):
+            log.debug("Enabling explicit status update acknowledgements")
+            implicitAcknowledgements = 0
+
+        return implicitAcknowledgements
+
+    def start_framework(self):
+
+        framework = mesos_pb2.FrameworkInfo()
+        framework.user = "" # Have Mesos fill in the current user.
+        framework.name = "JobTree Framework (Python)"
+
+        # TODO(vinod): Make checkpointing the default when it is default
+        # on the slave.
+        if os.getenv("MESOS_CHECKPOINT"):
+            log.debug( "Enabling checkpoint for the framework")
+            framework.checkpoint = True
+
+
+        if os.getenv("MESOS_AUTHENTICATE"):
+
+            # TODO: let's delete this branch and replace it with raising a NotImplementedError
+
+            log.debug( "Enabling authentication for the framework")
+
+            if not os.getenv("DEFAULT_PRINCIPAL"):
+                log.error( "Expecting authentication principal in the environment")
+                sys.exit(1)
+
+            if not os.getenv("DEFAULT_SECRET"):
+                log.error( "Expecting authentication secret in the environment")
+                sys.exit(1)
+
+            credential = mesos_pb2.Credential()
+            credential.principal = os.getenv("DEFAULT_PRINCIPAL")
+            credential.secret = os.getenv("DEFAULT_SECRET")
+
+            framework.principal = os.getenv("DEFAULT_PRINCIPAL")
+
+            driver = MesosSchedulerDriver(
+                self,
+                framework,
+                self.masterIP,
+                self.implicitAcknowledgements,
+                credential)
+        else:
+            framework.principal = "test-framework-python"
+
+            driver = MesosSchedulerDriver(
+                self,
+                framework,
+                self.masterIP,
+                self.implicitAcknowledgements)
+
+        driver_result = driver.run()
+        status = 0 if driver_result == mesos_pb2.DRIVER_STOPPED else 1
+
+        # Ensure that the driver process terminates.
+        driver.stop()
+        sys.exit(status)
+
+    def run(self):
+        self.start_framework()
