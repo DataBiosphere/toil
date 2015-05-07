51e5f0347b426496bf0649f8fbf6b0f0c93a0975
Hannes Schmidt <hannes@ucsc.edu>
2015-05-07 01:18:21 -0700

    Added default parameters to Job constructor
    Makes my life easier when deserializing Jobs in AwsJobStrore

--- src/job.py
+++ src/job.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger( __name__ )
 
 
-class Job(object):
+class Job( object ):
     """
     A class encapsulating state about a jobTree job including its child commands and follow-on commands.
 
@@ -12,65 +12,65 @@ class Job(object):
     """
 
     @staticmethod
-    def create(command, memory, cpu, tryCount, jobStoreID, logJobStoreFileID):
+    def create( command, memory, cpu, tryCount, jobStoreID, logJobStoreFileID ):
         return Job(
             remainingRetryCount=tryCount,
             jobStoreID=jobStoreID,
-            children=[],
-            followOnCommands=[(command, memory, cpu, 0)],
-            messages=[],
-            logJobStoreFileID=logJobStoreFileID)
+            followOnCommands=[ (command, memory, cpu, 0) ],
+            messages=[ ],
+            logJobStoreFileID=logJobStoreFileID )
 
-    def __init__(self, remainingRetryCount, jobStoreID, children, followOnCommands, messages, logJobStoreFileID):
+    def __init__( self, remainingRetryCount, jobStoreID,
+                  children=None, followOnCommands=None, messages=None, logJobStoreFileID=None ):
         self.remainingRetryCount = remainingRetryCount
         self.jobStoreID = jobStoreID
-        self.children = children
-        self.followOnCommands = followOnCommands
-        self.messages = messages
+        self.children = children or [ ]
+        self.followOnCommands = followOnCommands or [ ]
+        self.messages = messages or [ ]
         self.logJobStoreFileID = logJobStoreFileID
 
-    def setupJobAfterFailure(self, config):
-        if len(self.followOnCommands) > 0:
-            self.remainingRetryCount = max(0, self.remainingRetryCount - 1)
-            logger.critical("Due to failure we are reducing the remaining retry \
-            count of job %s to %s" % (self.jobStoreID, self.remainingRetryCount))
+    def setupJobAfterFailure( self, config ):
+        if len( self.followOnCommands ) > 0:
+            self.remainingRetryCount = max( 0, self.remainingRetryCount - 1 )
+            logger.critical( "Due to failure we are reducing the remaining retry \
+            count of job %s to %s" % (self.jobStoreID, self.remainingRetryCount) )
             # Set the default memory to be at least as large as the default, in
             # case this was a malloc failure (we do this because of the combined
             # batch system)
-            self.followOnCommands[-1] = (self.followOnCommands[-1][0], \
-                                         max(self.followOnCommands[-1][1],
-                                             float(config.attrib["default_memory"]))) + \
-                                        self.followOnCommands[-1][2:]
-            logger.critical("We have set the default memory of the failed job to %s bytes" \
-                            % self.followOnCommands[-1][1])
+            self.followOnCommands[ -1 ] = (self.followOnCommands[ -1 ][ 0 ], \
+                max( self.followOnCommands[ -1 ][ 1 ],
+                     float( config.attrib[ "default_memory" ] ) )) + \
+                                          self.followOnCommands[ -1 ][ 2: ]
+            logger.critical( "We have set the default memory of the failed job to %s bytes" \
+                             % self.followOnCommands[ -1 ][ 1 ] )
         else:
-            logger.critical("The job %s has no follow on jobs to reset" % self.jobStoreID)
+            logger.critical( "The job %s has no follow on jobs to reset" % self.jobStoreID )
 
-    def clearLogFile(self, jobStore):
+    def clearLogFile( self, jobStore ):
         """Clears the log file, if it is set.
         """
         if self.logJobStoreFileID is not None:
-            jobStore.deleteFile(self.logJobStoreFileID)
+            jobStore.deleteFile( self.logJobStoreFileID )
             self.logJobStoreFileID = None
 
-    def setLogFile(self, logFile, jobStore):
+    def setLogFile( self, logFile, jobStore ):
         """Sets the log file in the file store. 
         """
         if self.logJobStoreFileID is not None:  # File already exists
-            jobStore.updateFile(self.logJobStoreFileID, logFile)
+            jobStore.updateFile( self.logJobStoreFileID, logFile )
         else:
-            self.logJobStoreFileID = jobStore.writeFile(self.jobStoreID, logFile)
+            self.logJobStoreFileID = jobStore.writeFile( self.jobStoreID, logFile )
             assert self.logJobStoreFileID is not None
 
-    def getLogFileHandle(self, jobStore):
+    def getLogFileHandle( self, jobStore ):
         """
         Returns a context manager that yields a file handle to the log file
         """
-        return jobStore.readFileStream(self.logJobStoreFileID)
+        return jobStore.readFileStream( self.logJobStoreFileID )
 
     # Serialization support methods
 
-    def toList(self):
+    def toList( self ):
         """
         Deprecated. Use toDict() instead.
         """
@@ -80,18 +80,30 @@ class Job(object):
             self.children,
             self.followOnCommands,
             self.messages,
-            self.logJobStoreFileID]
+            self.logJobStoreFileID ]
 
     @staticmethod
-    def fromList(l):
+    def fromList( l ):
         """
         Deprecated. Use fromDict() instead.
         """
-        return Job(*l)
+        return Job( *l )
 
-    def toDict(self):
-        return self.__dict__.copy()
+    def toDict( self ):
+        return self.__dict__.copy( )
 
     @staticmethod
-    def fromDict(d):
-        return Job(**d)
+    def fromDict( d ):
+        return Job( **d )
+
+    def __hash__( self ):
+        return hash( self.jobStoreID )
+
+    def __eq__( self, other ):
+        return isinstance( other, self.__class__ ) and self.__dict__ == other.__dict__
+
+    def __ne__( self, other ):
+        return not self.__eq__( other )
+
+    def __repr__( self ):
+        return 'Job( **%r )' % self.__dict__
