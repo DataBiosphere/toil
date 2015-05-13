ad7e4de706fd14f41c98e3eedcb1823a328ea7f1
John Vivian <jtvivian@gmail.com>
2015-05-13 11:57:34 -0700

    Changed named tuple to match class name.

--- batchSystems/mesos/ResourceRequirement.py
+++ batchSystems/mesos/ResourceRequirement.py
@@ -2,6 +2,7 @@ __author__ = 'CJ'
 from collections import namedtuple
 
 
-class ResourceRequirement(namedtuple("ResourceSummary", ["memory", "cpu"])):
+class ResourceRequirement(namedtuple("ResourceRequirement", ["memory", "cpu"])):
     pass
+
 # used to describe resource requirements. Used as key in queue dictionary
