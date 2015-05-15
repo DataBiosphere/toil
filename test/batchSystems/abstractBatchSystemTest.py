d5df7f5103cf5163ac7ddb288fd50ee697a80477
John Vivian <jtvivian@gmail.com>
2015-05-15 10:35:11 -0700

    Initial commit of abstractBatchSystemTest

--- test/batchSystems/abstractBatchSystemTest.py
+++ test/batchSystems/abstractBatchSystemTest.py
@@ -1,9 +1,12 @@
 from abc import ABCMeta, abstractmethod
 import os
+import shutil
+import subprocess
+import threading
 import unittest
 from xml.etree import ElementTree
+import time
 from jobTree.batchSystems.abstractBatchSystem import AbstractBatchSystem
-from jobTree.batchSystems.gridengine import GridengineBatchSystem
 from jobTree.batchSystems.mesos import MesosBatchSystem
 from jobTree.batchSystems.singleMachine import SingleMachineBatchSystem
 
@@ -12,17 +15,36 @@ class AbstractBatchSystemTest(unittest.TestCase):
 
     __metaclass__ = ABCMeta
 
-    def setUp(self):
-        self.config = self._createDummyConfig()
-        self.batchSystem = self.createBatchSystem()
-        self.test_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test.txt')
-
     @abstractmethod
     def createBatchSystem(self):
         """
         :rtype: AbstractBatchSystem
         """
         raise NotImplementedError
+
+
+    def setUp(self):
+        super(AbstractBatchSystemTest, self).setUp()
+        self.batchSystem = self.createBatchSystem()
+
+    config = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.config = cls._createDummyConfig()
+
+
+    def testIssueJob(self):
+        test_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test.txt')
+        self.robust_remove(test_path)
+
+        jobCommand = 'touch {}'.format(test_path)
+        self.batchSystem.issueJob(jobCommand, memory=10, cpu=1)
+
+        time.sleep(1)
+
+        self.assertTrue(os.path.exists(test_path))
+        os.remove(test_path)
 
     def testCheckResourceRequest(self):
         self.assertRaises(RuntimeError, self.batchSystem.checkResourceRequest, memory=1000, cpu=200)
@@ -32,35 +54,37 @@ class AbstractBatchSystemTest(unittest.TestCase):
             self.batchSystem.checkResourceRequest(memory=1, cpu=1)
         except RuntimeError:
             self.fail('checkResourceRequest raised exception unexpectedly.')
-    '''
-    def testIssueJob(self):
 
-        jobCommand = 'touch {}'.format(self.test_path)
-        self.batchSystem.issueJob(jobCommand, memory=10, cpu=1)
+    def testGetIssuedJobIDs(self):
+        self.batchSystem.issueJob('sleep 1', memory=10, cpu=1)
+        self.batchSystem.issueJob('sleep 1', memory=10, cpu=1)
 
-        # Wait for batchSystem to issue job, otherwise assertTrue happens too quickly
-        while not self.batchSystem.getIssuedJobIDs():
-            pass
+        self.assertEqual([0,1], self.batchSystem.getIssuedJobIDs())
 
-        self.assertTrue(os.path.exists(self.test_path))
-        # TODO: Ask Hannes how to move this step to teardown -- currently fails otherwise
-        #os.remove(self.test_path)
+    def testGetRunningJobsIDs(self):
+        self.batchSystem.issueJob('sleep 5', memory=10, cpu=1)
+        self.batchSystem.issueJob('sleep 5', memory=10, cpu=1)
 
+        time.sleep(4)
 
-    def testKillJobs(self):
+        self.assertEqual([0,1], self.batchSystem.getRunningJobIDs().keys())
 
-        jobCommand = 'ls'
-        self.batchSystem.issueJob(jobCommand, memory=10, cpu=1)
+    if False:
+        def testKillJobs(self):
 
-        print self.batchSystem.getIssuedJobIDs()
-        print 'test: {}'.format(self.batchSystem.getUpdatedJob(1))
+            jobCommand = 'sleep 100'
+            self.batchSystem.issueJob(jobCommand, memory=10, cpu=1)
 
-        print self.batchSystem.getIssuedJobIDs()
+            print self.batchSystem.getIssuedJobIDs()
+            print 'test: {}'.format(self.batchSystem.getUpdatedJob(1))
 
-        self.assertEqual({}, self.batchSystem.getRunningJobIDs())
+            print self.batchSystem.getIssuedJobIDs()
+            self.batchSystem.killJobs([0])
 
-    '''
-    def _createDummyConfig(self):
+            self.assertEqual({}, self.batchSystem.getRunningJobIDs())
+
+    @staticmethod
+    def _createDummyConfig():
         config = ElementTree.Element("config")
         config.attrib["log_level"] = 'DEBUG'
         config.attrib["job_store"] = '.'
@@ -78,24 +102,71 @@ class AbstractBatchSystemTest(unittest.TestCase):
 
         return config
 
-    def tearDown(self):
-        pass
-        #os.remove(self.test_path)
+    @classmethod
+    def robust_remove(cls, name):
+        try:
+            os.remove(name)
+        except OSError:
+            pass
 
 
 class MesosBatchSystemTest(AbstractBatchSystemTest):
 
     def createBatchSystem(self):
-        return MesosBatchSystem(config=self.config, maxCpus=2, maxMemory=10, badExecutor=False)
+        shutil.rmtree('/tmp/mesos/', ignore_errors=True)
+
+        # Launch Mesos Master and Slave if running MesosBatchSystemTest
+        self.master = MesosMasterThread()
+        self.master.start()
+
+        self.slave = MesosSlaveThread()
+        self.slave.start()
+
+
+        while self.master.popen is None or self.slave.popen is None:
+            time.sleep(2)
+            print self.master.popen, self.slave.popen
+
+        return MesosBatchSystem(config=self.config, maxCpus=2, maxMemory=20, badExecutor=False)
+
+    def setUp(self):
+        super(MesosBatchSystemTest, self).setUp()
+
+    def tearDown(self):
+        super(MesosBatchSystemTest, self).tearDown()
+        self.slave.popen.kill()
+        self.slave.join()
+
+        self.master.popen.kill()
+        self.master.join()
 
 
 class SingleMachineBatchSystemTest(AbstractBatchSystemTest):
 
     def createBatchSystem(self):
-        return SingleMachineBatchSystem(config=self.config, maxCpus=2, maxMemory=10)
+        return SingleMachineBatchSystem(config=self.config, maxCpus=2, maxMemory=20)
+
+# Lock is used because subprocess is NOT thread safe: http://tinyurl.com/pkp5pgq
+lock = threading.Lock()
+class MesosMasterThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.popen = None
+
+    def run(self):
+        with lock:
+            self.popen = subprocess.Popen(['/usr/local/sbin/mesos-master', '--registry=in_memory', '--ip=127.0.0.1'])
+        self.popen.wait()
+        print 'exiting master thread'
 
 
-class GridEngineTest(AbstractBatchSystemTest):
+class MesosSlaveThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.popen = None
 
-    def createBatchSystem(self):
-        return GridengineBatchSystem(config=self.config, maxCpus=2, maxMemory=10)
+    def run(self):
+        with lock:
+            self.popen = subprocess.Popen(['/usr/local/sbin/mesos-slave', '--ip=127.0.0.1', '--master=127.0.0.1:5050'])
+        self.popen.wait()
+        print 'exiting slave thread'
