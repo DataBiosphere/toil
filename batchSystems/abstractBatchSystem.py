2fcf2c208ed3ac2d7e3f39b632bbf8d8059402fa
John Vivian <jtvivian@gmail.com>
2015-05-21 14:17:44 -0700

    PEP 8ification

--- batchSystems/abstractBatchSystem.py
+++ batchSystems/abstractBatchSystem.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
 
-#Copyright (C) 2011 by Benedict Paten (benedictpaten@gmail.com)
-#
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in
-#all copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
+"""
+Copyright (C) 2011 by Benedict Paten (benedictpaten@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
 
 from Queue import Empty
+
 
 class AbstractBatchSystem:
     """An abstract (as far as python currently allows) base class
@@ -39,12 +42,13 @@ class AbstractBatchSystem:
     def checkResourceRequest(self, memory, cpu):
         """Check resource request is not greater than that available.
         """
-        assert memory != None
-        assert cpu != None
+        assert memory is not None
+        assert cpu is not None
         if cpu > self.maxCpus:
             raise InsufficientSystemResources('CPUs', cpu, self.maxCpus)
         if memory > self.maxMemory:
             raise InsufficientSystemResources('memory', memory, self.maxMemory)
+
     def issueJob(self, command, memory, cpu):
         """Issues the following command returning a unique jobID. Command
         is the string to run, memory is an int giving
@@ -103,11 +107,13 @@ class AbstractBatchSystem:
         except Empty:
             return None
 
+
 class InsufficientSystemResources(Exception):
     def __init__(self, cpu_or_mem, requested, available):
         self.requested = requested
         self.available = available
         self.cpu_or_mem = cpu_or_mem
+
     def __str__(self):
         return 'Requesting more {} than available. Requested: {}, Available: {}' \
                ''.format(self.cpu_or_mem, self.requested, self.available)
