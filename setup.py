968a08b3984cca0fac3690c6bf20252f6de31f9a
John Vivian <jtvivian@gmail.com>
2015-07-02 09:51:00 -0700

    Added mesos_requirements, version update

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
+mesos_requirements = []
+try:
+    import mesos.native
+    mesos_requirements = ['psutil>=3.0.1', 'mesos.interface>=0.22.1']
+except:
+    pass
+
 setup(
     name='jobTree',
-    version='2.0.dev1',
+    version='3.0.3.dev1',
     description='Pipeline management software for clusters.',
     author='Benedict Paten',
     author_email='benedict@soe.usc.edu',
     url="https://github.com/BD2KGenomics/jobTree",
-    
+    install_requires = mesos_requirements,
     package_dir={ '': 'src' },
     packages=find_packages( 'src', exclude=[ '*.test' ] ),
     entry_points={
