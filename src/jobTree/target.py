f496e398aa6c4450c022dbaf08be7f816e98a621
benedictpaten <benedictpaten@gmail.com>
2015-07-16 11:26:58 -0700

    Adding in some fixes + tests

--- src/jobTree/target.py
+++ src/jobTree/target.py
@@ -48,7 +48,7 @@ class Target(object):
     Represents a unit of work in jobTree. Targets are composed into graphs
     which make up a workflow. 
     
-    This public functions of this class and its  nested classes are the API 
+    This public functions of this class and its nested classes are the user API 
     to jobTree. 
     """
     
@@ -121,7 +121,7 @@ class Target(object):
     def addFollowOn(self, followOnTarget):
         """
         Adds a follow-on target, follow-on targets will be run
-        after the child targets and their successors have been run. 
+        after the child targets and their descendants have been run. 
         Returns followOnTarget.
         
         See Target.checkTargetGraphAcylic for formal definition of allowed forms of
@@ -211,18 +211,23 @@ class Target(object):
         Raises a RuntimeError exception if the target graph rooted at this target 
         contains any cycles of child/followOn dependencies in the augmented target graph
         (see below). Such cycles are not allowed in valid target graphs.
-        Is run during execution.
+        This function is run during execution.
+        
+        A target B that is on a directed path of child/followOn edges from a 
+        target A in the target graph is a descendant of A, 
+        similarly A is an ancestor of B.
         
         A follow-on edge (A, B) between two targets A and B is equivalent 
-        to adding a child edge from each child of A and its successors to B. We
+        to adding a child edge to B from (1) A, (2) from each child of A, 
+        and (3) from the descendants of each child of A. We
         call such an edge an "implied" edge. The augmented target graph is a 
         target graph including all the implied edges. 
 
         For a target (V, E) the algorithm is O(|V|^2). It is O(|V| + |E|) for 
         a graph with no follow-ons. The former follow on case could be improved!
         """
-        #Get augmented edges
-        extraEdges = self._getAugmentedEdges()
+        #Get implied edges
+        extraEdges = self._getImpliedEdges()
             
         #Check for directed cycles in the augmented graph
         self._checkTargetGraphAcylicDFS([], set(), extraEdges)
@@ -597,8 +602,8 @@ class Target(object):
     ####################################################
         
     def _dfs(self, visited):
-        """Adds all targets reacheable on a directed path from current node to the
-        set 'visited'.
+        """Adds the target and all targets reachable on a directed path from current 
+        node to the set 'visited'.
         """
         if self not in visited:
             visited.add(self) 
@@ -607,27 +612,28 @@ class Target(object):
         
     def _checkTargetGraphAcylicDFS(self, stack, visited, extraEdges):
         """
-        To DFS traversal to detect cycles in augmented target graph.
+        DFS traversal to detect cycles in augmented target graph.
         """
         if self not in visited:
             visited.add(self) 
             stack.append(self)
             for successor in self._children + self._followOns + extraEdges[self]:
                 successor._checkTargetGraphAcylicDFS(stack, visited, extraEdges)
-            stack.pop()
+            assert stack.pop() == self
         if self in stack:
             raise RuntimeError("Detected cycle in augmented target graph: %s" % stack)
         
-    def _getAugmentedEdges(self):
+    def _getImpliedEdges(self):
         """
-        Gets the set of augmented edges. See Target.checkTargetGraphAcylic
+        Gets the set of implied edges. See Target.checkTargetGraphAcylic
         """
         #Get nodes in target graph
         nodes = set()
         self._dfs(nodes)
         
         ##For each follow-on edge calculate the extra implied edges
-        #Map of targets to lists of targets connected by an implied follow-on edge 
+        #Adjacency list of implied edges, i.e. map of targets to lists of targets 
+        #connected by an implied edge 
         extraEdges = dict(map(lambda n : (n, []), nodes))
         for target in nodes:
             if len(target._followOns) > 0:
@@ -639,38 +645,7 @@ class Target(object):
                 #Now add extra edges
                 for descendant in reacheable:
                     extraEdges[descendant] += target._followOns[:]
-        return extraEdges
-    
-    def _removeRedudantEdges(self):
-        """
-        Removes unnecessary edges from the target graph. 
-        TODO: Use transitive reduction algorithm to remove all such edges.
-        
-        Currently for the target graph (V, E) we remove any edges in E that are also
-        in the set of augmented edges. Such edges in E are redundant, and create
-        a deadlock in the method used for scheduling targets/jobs.
-        TODO: Naive algorithm could be improved
-        """
-        nodes = set()
-        self._dfs(nodes)
-        for target in nodes:
-            if len(target._followOns) > 0:
-                visited = set()
-                for child in target._children:
-                    child._removeRedundantEdges2(set(target._followOns), visited)
-    
-    def _removeRedundantEdges2(self, augmentedEdges, visited):
-        """
-        Does a DFS, for each node removing edges in its set of children
-        or follow-ons that are in the set of augmentedEdges
-        """
-        if self not in visited:
-            visited.add(self)
-            self._children = [ i for i in self._children if i not in augmentedEdges ]
-            self._followOns = [ i for i in self._followOns if i not in augmentedEdges ]
-            for successor in self._children + self._followOns:
-                assert successor not in augmentedEdges
-                successor._removeRedundantEdges2(augmentedEdges, visited)  
+        return extraEdges 
     
     ####################################################
     #Function which worker calls to ultimately invoke
@@ -694,8 +669,6 @@ class Target(object):
         #Check if the target graph has created
         #any cycles of dependencies 
         self.checkTargetGraphAcylic()
-        #Remove redundant edges
-        self._removeRedudantEdges()
         #Set the promised value jobStoreFileIDs
         self._setFileIDsForPromisedValues(jobStore, job.jobStoreID, set())
         #Store the return values for any promised return value
--- src/jobTree/test/staticDeclaration/staticTest.py
+++ src/jobTree/test/staticDeclaration/staticTest.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import random
 
 from jobTree.lib.bioio import getTempFile
 from jobTree.target import Target
@@ -14,9 +15,9 @@ class StaticTest(JobTreeTest):
         """
         Create a dag of targets non-dynamically and run it.
         
-        A - F
+        A -> F
         \-------
-        B -> D \ 
+        B -> D  \ 
          \       \
           ------- C -> E
           
@@ -54,7 +55,179 @@ class StaticTest(JobTreeTest):
         #Cleanup
         os.remove(outFile)
         
+    ##TODO Add tests for cycle detection
+    
+    def testCycleDetection(self):
+        """
+        Randomly generate target graphs with various types of cycle in them and
+        check they cause an exception
+        """
+        for test in xrange(100): 
+            #Make a random DAG for the set of child edges
+            nodeNumber = random.choice(xrange(100))
+            childEdges = self.makeRandomDAG(nodeNumber)
+            #Get an adjacency list representation and check is acyclic 
+            adjacencyList = self.getAdjacencyList(nodeNumber, childEdges)
+            self.checkAcyclic(adjacencyList)
+            #Add in follow on edges - these are returned as a list, and as a set
+            #of augmented edges in the adjacency list
+            followOnEdges = self.addRandomFollowOnEdges(adjacencyList)
+            self.checkAcyclic(adjacencyList)
+            #Make the target graph
+            rootTarget = self.makeTargetGraph(nodeNumber, childEdges, followOnEdges, None)
+            rootTarget.checkTargetGraphAcylic() #This should not throw an exception
+            
+            #Now try adding edges that create a cycle
+            
+            ##Try adding a child edge from a descendant to an ancestor
+            
+            ##Try adding a self child edge
+            
+            ##Try adding a follow on edge from a descendant to an ancestor
+            
+            ##Try adding a self follow on edge
+            
+            ##Try adding a follow on edge between two nodes with shared descendants
+  
+    def testEvaluatingRandomDAG(self):
+        """
+        Randomly generate test input then check it
+        """
+        for test in xrange(100): 
+            #Temporary file
+            outFile = getTempFile(rootDir=os.getcwd())
+            #Make a random DAG for the set of child edges
+            nodeNumber = random.choice(xrange(100))
+            childEdges = self.makeRandomDAG(nodeNumber)
+            #Get an adjacency list representation and check is acyclic 
+            adjacencyList = self.getAdjacencyList(nodeNumber, childEdges)
+            self.checkAcyclic(adjacencyList)
+            #Add in follow on edges - these are returned as a list, and as a set
+            #of augmented edges in the adjacency list
+            followOnEdges = self.addRandomFollowOnEdges(adjacencyList)
+            self.checkAcyclic(adjacencyList)
+            #Make the target graph
+            rootTarget = self.makeTargetGraph(nodeNumber, childEdges, followOnEdges, outFile)
+            #Run the target  graph
+            options = Target.Runner.getDefaultOptions()
+            failedJobs = Target.Runner.startJobTree(rootTarget, options)
+            self.assertEquals(failedJobs, 0)
+            #Get the ordering add the implied ordering to the graph
+            with open(outFile, 'r') as fH:
+                ordering = fH.readline().split()
+            #Check all the targets were run
+            self.assertEquals(set(ordering), set(xrange(nodeNumber)))
+            #Add the ordering to the graph
+            for i in xrange(nodeNumber-1):
+                adjacencyList[ordering[i]].add(ordering[i+1])
+            #Check the ordering retains an acyclic graph
+            self.checkAcyclic(adjacencyList)
+            #Cleanup
+            os.remove(outFile)
+            
+    @staticmethod
+    def getRandomEdge(nodeNumber):
+        fNode = random.choice(xrange(nodeNumber))
+        return (fNode, random.choice(xrange(fNode+1,nodeNumber)))
+    
+    @staticmethod
+    def makeRandomDAG(nodeNumber):
+        """
+        Makes a random dag with "nodeNumber" nodes in which all nodes are 
+        connected. Return value is list of edges, each of form (a, b), 
+        where a and b are integers >= 0 < nodeNumber 
+        referring to nodes and the edge is from a to b.
+        """
+        #Pick number of total edges to create
+        edgeNumber = random.choice(xrange((nodeNumber-1, nodeNumber * (nodeNumber-1)) / 2)) 
+        #Make a spanning tree of edges so that nodes are connected
+        edges = set(map(lambda i : (random.choice(xrange(i)), i), xrange(1, nodeNumber)))
+        #Add extra random edges until there are edgeNumber edges
+        while edgeNumber < len(edges):
+            edges.add(StaticTest.getRandomEdge(nodeNumber))
+        return edges
+    
+    @staticmethod
+    def getAdjacencyList(nodeNumber, edges):
+        """
+        Make adjacency list representation of edges
+        """
+        adjacencyList = [ set() for i in xrange(nodeNumber) ]
+        for fNode, tNode in edges:
+            adjacencyList[fNode].add(tNode)
+        return adjacencyList
+    
+    @staticmethod
+    def addRandomFollowOnEdges(adjacencyList):
+        """
+        Adds random follow on edges to the graph, represented as an adjacency list.
+        The follow on edges are returned as a set and their augmented edges
+        are added to the adjacency list.
+        """
+        def reachable(node):
+            visited = set()
+            def dfs(fNode):
+                if fNode not in visited:
+                    visited.add(fNode)
+                    for tNode in adjacencyList[fNode]:
+                        dfs(tNode)
+            dfs(node)
+            return visited
+            
+        followOnEdges = set()
+        #Loop to create the follow on edges
+        while random.random() > 0.001:
+            fNode, tNode = StaticTest.getRandomEdge(len(adjacencyList))
+            fDescendants = reachable(fNode)
+            tDescendants = reachable(tNode)
+            #If there is no directed path from the fNode to the tNode can
+            #create any subset of follow on edges between fNode and tDescendants
+            if len(fDescendants.intersection(tDescendants)) == 0:
+                for tNode2 in tDescendants:
+                    if random.random() > 0.5:
+                        followOnEdges.add((fNode, tNode2))
+                        for descendant in fDescendants:
+                            adjacencyList[descendant].add(tNode2)
+                    
+        return followOnEdges
+    
+    @staticmethod   
+    def makeTargetGraph(nodeNumber, childEdges, followOnEdges, outFile):
+        """
+        Converts a DAG into a target graph. childEdges and followOnEdges are 
+        the lists of child and followOn edges.
+        """
+        targets = map(lambda i : Target.wrapFn(f, str(i) + " ", outFile), xrange(nodeNumber))
+        for fNode, tNode in childEdges:
+            targets[fNode].addChild(targets[tNode])
+        for fNode, tNode in followOnEdges:
+            targets[fNode].addFollowOn(targets[tNode])
+        return targets[0]
+    
+    def checkAcyclic(self, adjacencyList):
+        """
+        Checks if there are any cycles in the graph, which is represented as an
+        adjacency list. If there are cycles, the test will fail
+        """
+        def dfs(fNode, visited, stack):
+            if fNode not in visited:
+                assert fNode not in stack
+                stack.append(fNode)
+                visited.add(fNode)
+                for tNode in adjacencyList[fNode]:
+                    dfs(tNode, visited, stack)
+                assert stack.pop() == fNode
+            self.assertTrue(fNode not in stack)
+        visited = set()
+        for i in len(adjacencyList):
+            dfs(i, visited, [])
+
 def f(string, outFile):
+    """
+    Function appends string to output file, then returns the 
+    next ascii character of the first character in the string, e.g.
+    if string is "AA" returns "B"
+    """
     fH = open(outFile, 'a')
     fH.write(string)
     fH.close()   
