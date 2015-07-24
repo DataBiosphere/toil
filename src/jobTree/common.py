313241b805040a48b45b6c0d384d6d66751a0461
CJ <cketchum@ucsc.edu>
2015-07-23 22:23:47 -0700

    Avoid trying to pickle None by representing it as an empty string

--- src/jobTree/common.py
+++ src/jobTree/common.py
@@ -129,8 +129,8 @@ def _addOptions(addGroupFn, defaultStr):
     addOptionFn("--rescueJobsFrequency", dest="rescueJobsFrequency",
                       help=("Period of time to wait (in seconds) between checking for "
                             "missing/overlong jobs, that is jobs which get lost by the batch system. Expert parameter. (default is set by the batch system)"))
-    addOptionFn("--resume", dest="resume", action='store_true',
-                      help=("Pass this flag to resume a previous, incomplete job with an existing jobStore"))
+    addOptionFn("--resume", dest="resume", action='store_true', default=False,
+                      help=("Pass this flag to resume a previous, incomplete job with an existing jobStore. defualt = %s" % defaultStr))
     addOptionFn("--clean", dest="clean", choices=['always', 'onerror','never'], default='always',
                       help=("Determines the deletion of the jobStore upon completion of the program. If you wish to be able to restart the run, "
                             "choose \'never\'. Default=%s" % defaultStr))
@@ -228,7 +228,6 @@ def createConfig(options):
     config.attrib["max_memory"] = str(int(options.maxMemory))
     config.attrib["max_disk"] = str(int(options.maxDisk))
     config.attrib["scale"] = str(float(options.scale))
-    config.attrib["resume"] = options.resume
     config.attrib["clean"] = options.clean
     if options.bigBatchSystem is not None:
         config.attrib["big_batch_system"] = options.bigBatchSystem
@@ -238,6 +237,8 @@ def createConfig(options):
         config.attrib["big_max_memory"] = str(int(options.bigMaxMemory))
     if options.stats:
         config.attrib["stats"] = ""
+    if options.resume:
+        config.attrib["resume"] = ""
     return config
 
 
