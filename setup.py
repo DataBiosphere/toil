080e70d39120cf7981037b22791bd545124b17f3
Hannes Schmidt <hannes@ucsc.edu>
2015-08-03 22:53:12 -0700

    setup.py: Tweaked Mesos trial import logic in setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,38 @@
+import importlib
+
 from setuptools import setup, find_packages
 
-mesos_requirements = []
-# only set requirements that are not already satisfied. For some reason, setting requirements that are already satisfied
-# causes setup.py to reinstall them. This is bad, especially in cases like in cgcloud mesos right now, when mesos.native and
-# mesos.interface are installed via an egg. This then uninstalls mesos.interface, pip installs it, and now there are two
-# mesos modules instead of the one we want.
-try:
-    import mesos.native
+install_requires = [ ]
+
+
+def __try_import( name ):
     try:
-        import mesos.interface
-    except:
-        mesos_requirements.append('mesos.interface>=0.22.1')
-    try:
-        import psutil
-    except:
-        mesos_requirements.append('psutil>=3.0.1')
-except:
+        importlib.import_module( name )
+    except ImportError:
+        print "Failed to import '%s'" % name
+        return 0
+    else:
+        print "Successfully imported '%s'" % name
+        return 1
+
+
+# The mesos.native package can't be installed solely via pip, it must be installed by installing
+# Mesos as a whole. And while mesos.interface *can* be installed via pip, it is not sufficient
+# for our purposes. We therefore don't explicitly depend on neither mesos.native nor
+# mesos.interface. Instead, we require them both to be absent or present. In the latter case,
+# we dynamically add the dependencies we need to use Mesos.
+
+mesos_imports = __try_import( 'mesos.native' ) + __try_import( 'mesos.interface' )
+
+if mesos_imports == 0:
     pass
+elif mesos_imports == 1:
+    raise RuntimeError( "Please install matching versions of mesos.native and mesos.interface. "
+                        "One of them is missing at the moment." )
+elif mesos_imports == 2:
+    install_requires.append( 'psutil>=3.0.1' )
+else:
+    raise RuntimeError( "Internal error." )
 
 setup(
     name='toil',
@@ -25,7 +41,7 @@ setup(
     author='Benedict Paten',
     author_email='benedict@soe.usc.edu',
     url="https://github.com/BD2KGenomics/toil",
-    install_requires = mesos_requirements,
+    install_requires=install_requires,
     package_dir={ '': 'src' },
     packages=find_packages( 'src', exclude=[ '*.test' ] ),
     entry_points={
@@ -36,4 +52,4 @@ setup(
             'toilRestarts = toil.utils.toilRestarts:main',
             'multijob = toil.batchSystems.multijob:main'
         ],
-    })
+    } )
