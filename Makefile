e701d6de716e717a1a0e7132469d5fe2c85fde9b
Hannes Schmidt <hannes@ucsc.edu>
2015-08-07 23:58:10 -0700

    Streamlined setup.py and Makefile (resolves #207)
    This commit eliminates the need for setting PYTHONPATH in the Makefile or a developer's .profile by replacing it with setuptools' develop mode. But instead of using "setup.py develop" directly, we use "pip install -e" as a frontend. This allows for the use of setuptools' extras feature (see below) in conjunction with develop mode. Pip calls that "editable install". To perform the editable install, simply run "make develop" or just "make". To run tests, use "make test" as usual. Running "make clean" undoes develop mode.
    
    By default, "make develop" installs into the user-specific install location (via "pip install --user") so as to not require root privileges. However, it is smart enough to omit "--user "when it detects that it is running in a virtualenv. This by the way is the recommended way to develop with Toil: create a virtualenv, activate it and then run "make".
    
    Setup.py now specifies "extras". With this mechanism, a user can now chose whether to install Toil with AWS and/or Mesos support: "pip install toil[aws,mesos]" or "pip install toil".

--- Makefile
+++ Makefile
@@ -1,16 +1,55 @@
 python=/usr/bin/env python2.7
+pip=/usr/bin/env pip2.7
 
-.PHONY: all test clean
+all: sdist develop
 
-all :
-	echo Nothing to be done.
+clean: _develop _sdist
 
-clean :
-	echo Nothing to be done.
+# Override this on the command line if you don't want to use any of these extras. Note that doing
+# so will currently cause some unit tests to fail because they don't intelligently detect which
+# extras were requested.
+#
+extras=[aws,mesos]
 
-# Override on the command line to run a particular test, e.g. tests=toil.test.src.jobTest.JobTest
+# Inside a virtualenv, we can't use pip with --user (http://stackoverflow.com/questions/1871549).
+#
+__user=$(shell python -c 'import sys; print "" if hasattr(sys, "real_prefix") else "--user"')
+
+develop:
+	$(pip) install $(__user) -e .$(extras)
+
+_develop:
+	- $(pip) uninstall -y toil
+
+sdist:
+	$(python) setup.py sdist
+
+_sdist:
+	- rm -rf dist
+
+# Override this on the command line to run a particular test, e.g.
+#
+# make test tests=toil.test.src.jobTest.JobTest
+#
 tests=discover -s src -p "*Test.py"
 testLength=SHORT
 testLogLevel=INFO
-test :
-	PYTHONPATH=$${PWD}/src TOIL_TEST_ARGS="--logLevel=$(testLogLevel) --testLength=$(testLength)" $(python) -m unittest $(tests)
+
+test: develop
+	TOIL_TEST_ARGS="--logLevel=$(testLogLevel) --testLength=$(testLength)" $(python) -m unittest $(tests)
+
+# Override this to the empty string if you want to deploy a potentially dirty working copy to PyPI
+#
+pypi_prereqs=pypi_prereqs
+
+pypi: $(pypi_prereqs)
+	$(python) setup.py register sdist bdist_egg upload
+	@echo "\033[0;32mLooks like the upload to PyPI was successful.\033[0m"
+	@echo "\033[0;32mNow tag this release in git and bump the version in every setup.py.\033[0m"
+
+pypi_prereqs:
+	@echo "\033[0;32mChecking if your working copy is clean ...\033[0m"
+	git diff --exit-code > /dev/null
+	git diff --cached --exit-code > /dev/null
+	test -z "$(git ls-files --other --exclude-standard --directory)"
+	@echo "\033[0;32mLooks like your working copy is clean. Uploading to PyPI ...\033[0m"
--- setup.py
+++ setup.py
@@ -1,47 +1,19 @@
-import importlib
-
 from setuptools import setup, find_packages
-
-install_requires = [ ]
-
-
-def __try_import( name ):
-    try:
-        importlib.import_module( name )
-    except ImportError:
-        print "Failed to import '%s'" % name
-        return 0
-    else:
-        print "Successfully imported '%s'" % name
-        return 1
-
-
-# The mesos.native package can't be installed solely via pip, it must be installed by installing
-# Mesos as a whole. And while mesos.interface *can* be installed via pip, it is not sufficient
-# for our purposes. We therefore don't explicitly depend on neither mesos.native nor
-# mesos.interface. Instead, we require them both to be absent or present. In the latter case,
-# we dynamically add the dependencies we need in order to to use Mesos.
-
-mesos_imports = __try_import( 'mesos.native' ) + __try_import( 'mesos.interface' )
-
-if mesos_imports == 0:
-    pass
-elif mesos_imports == 1:
-    raise RuntimeError( "Please install matching versions of mesos.native and mesos.interface. "
-                        "One of them is missing at the moment." )
-elif mesos_imports == 2:
-    install_requires.append( 'psutil>=3.0.1' )
-else:
-    raise RuntimeError( "Internal error." )
 
 setup(
     name='toil',
-    version='3.0.3.dev2',
+    version='3.0.3.dev6',
     description='Pipeline management software for clusters.',
     author='Benedict Paten',
     author_email='benedict@soe.usc.edu',
     url="https://github.com/BD2KGenomics/toil",
-    install_requires=install_requires,
+    install_requires=[ ],
+    extras_require={
+        'mesos': [
+            'mesos.interface==0.22.0',
+            'psutil==3.0.1' ],
+        'aws': [
+            'boto==2.38.0' ] },
     package_dir={ '': 'src' },
     packages=find_packages( 'src', exclude=[ '*.test' ] ),
     entry_points={
@@ -50,6 +22,4 @@ setup(
             'toilStatus = toil.utils.toilStatus:main',
             'toilStats = toil.utils.toilStats:main',
             'toilRestarts = toil.utils.toilRestarts:main',
-            'multijob = toil.batchSystems.multijob:main'
-        ],
-    } )
+            'multijob = toil.batchSystems.multijob:main' ] } )
