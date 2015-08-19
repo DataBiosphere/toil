31f3676d44601ac43281dc3b4249788da7975623
Hannes Schmidt <hannes@ucsc.edu>
2015-08-19 16:31:18 -0700

    Prevent mesosTest from deleting cwd (fixes internal pytest error)
    ENOENT: [No such file or directory]: getcwd()

--- src/toil/test/mesos/mesosTest.py
+++ src/toil/test/mesos/mesosTest.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import logging
 import tempfile
 import shutil
 import os
@@ -10,52 +11,56 @@ from toil.test.mesos.stress import main as stressMain
 from toil.test import ToilTest, needs_mesos
 from toil.batchSystems.mesos.test import MesosTestSupport
 
-lock = threading.Lock( )
+lock = threading.Lock()
 numCores = 2
+
+log = logging.getLogger(__name__)
 
 
 @needs_mesos
-class MesosTest( ToilTest, MesosTestSupport ):
+class MesosTest(ToilTest, MesosTestSupport):
     """
     FIXME: Describe what this test is supposed to do
     """
 
     @classmethod
-    def setUpClass( cls ):
-        super( MesosTest, cls ).setUpClass( )
-        shutil.rmtree( '/tmp/mesos', ignore_errors=True )
+    def setUpClass(cls):
+        super(MesosTest, cls).setUpClass()
+        shutil.rmtree('/tmp/mesos', ignore_errors=True)
 
-    def setUp( self ):
-        super( MesosTest, self ).setUp( )
-        self._startMesos( numCores )
-        self.tempDir = tempfile.mkdtemp( )
-        print "Using %s for files and directories created by this test run" % self.tempDir
-        os.chdir( self.tempDir )
+    def setUp(self):
+        super(MesosTest, self).setUp()
+        self.savedWorkingDir = os.getcwd()
+        self.tempDir = tempfile.mkdtemp()
+        log.info("Using '%s' for files and directories created by this test run.", self.tempDir)
+        os.chdir(self.tempDir)
+        self._startMesos(numCores)
 
-    def tearDown( self ):
-        self._stopMesos( )
-        shutil.rmtree( self.tempDir )
-        super( MesosTest, self ).tearDown( )
+    def tearDown(self):
+        self._stopMesos()
+        os.chdir(self.savedWorkingDir)
+        shutil.rmtree(self.tempDir)
+        super(MesosTest, self).tearDown()
 
-    def test_hello_world( self ):
-        dirPath = os.path.dirname( os.path.abspath( __file__ ) )
-        subprocess.check_call( "python {dirPath}/helloWorld.py "
-                               "--batchSystem=mesos "
-                               "--logLevel={logLevel}".format( dirPath=dirPath,
-                                                               logLevel=getLogLevelString( ) ),
-                               shell=True )
+    def test_hello_world(self):
+        dirPath = os.path.dirname(os.path.abspath(__file__))
+        subprocess.check_call("python {dirPath}/helloWorld.py "
+                              "--batchSystem=mesos "
+                              "--logLevel={logLevel}".format(dirPath=dirPath,
+                                                             logLevel=getLogLevelString()),
+                              shell=True)
 
-    def __do_test_stress( self, useBadExecutor, numJobs ):
+    def __do_test_stress(self, useBadExecutor, numJobs):
         """
         Set task number to number of files you wish to create. Actual number of jobs is jobs+2
         Right now task is set to fail 1/2 tries. To change this, go to badExecutor launchTask method
         """
-        stressMain( numJobs, useBadExecutor=useBadExecutor )
+        stressMain(numJobs, useBadExecutor=useBadExecutor)
 
-    def test_stress_good( self ):
-        self.__do_test_stress( False, 2 )
+    def test_stress_good(self):
+        self.__do_test_stress(False, 2)
 
-    def test_stress_bad( self ):
+    def test_stress_bad(self):
         # the second argument is the number of jobs. BadExecutor fails odd tasks, so certain numbers of tasks
         # may never finish because of the "Despite" bug/feature
-        self.__do_test_stress( True, 2 )
+        self.__do_test_stress(True, 2)
