dc9dc061431103fb63309eb108e2eea1923e28d0
CJ <cketchum@ucsc.edu>
2015-08-27 12:04:55 -0700

    Doc string for toil clean

--- src/toil/utils/toilClean.py
+++ src/toil/utils/toilClean.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Removes the JobStore from a toil run.
+"""
 from __future__ import absolute_import
 import logging
 import os
