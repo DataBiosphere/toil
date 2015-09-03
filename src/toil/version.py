e1791d929bccd1f5e9382e45fb90bd8257ef597d
Hannes Schmidt <hannes@ucsc.edu>
2015-09-02 18:24:14 -0700

    Prepare next development cycle 3.1.0a1

--- src/toil/version.py
+++ src/toil/version.py
@@ -11,4 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-version='3.0.6'
+
+version = '3.1.0a1'
