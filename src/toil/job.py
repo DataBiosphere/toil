55303f0eabed0593b3139b050f69a39729347d32
benedictpaten <benedictpaten@gmail.com>
2015-10-08 16:39:46 -0700

    Fixing behavior of writeGlobalFile so that it does not place files not in the local temporary directory structure in the cache.

--- src/toil/job.py
+++ src/toil/job.py
@@ -429,41 +429,45 @@ class Job(object):
 
         def getLocalTempDir(self):
             """
-            Get a new local temporary directory. This directory will exist for the 
+            Get the absolute path to a new local temporary directory. 
+            This directory will exist for the 
             duration of the job only, and is guaranteed to be deleted once
             the job terminates, removing all files it contains recursively. 
             """
-            return tempfile.mkdtemp(prefix="t", dir=self.localTempDir)
+            return os.path.abspath(tempfile.mkdtemp(prefix="t", dir=self.localTempDir))
         
         def getLocalTempFile(self):
             """
-            Get a local temporary file. This file will exist for the duration of the job only, and
+            Get an absolute path to a local temporary file. 
+            This file will exist for the duration of the job only, and
             is guaranteed to be deleted once the job terminates.
             """
             handle, tmpFile = tempfile.mkstemp(prefix="tmp", 
                                                suffix=".tmp", dir=self.localTempDir)
             os.close(handle)
-            return tmpFile
+            return os.path.abspath(tmpFile)
 
         def writeGlobalFile(self, localFileName, cleanup=False):
             """
-            Takes a file (as a path) and uploads it to to the global file store, returns
-            an ID that can be used to retrieve the file. 
+            Takes a file (as a path) and uploads it to to the global file store, 
+            returns an ID that can be used to retrieve the file. 
             
             If cleanup is True then the global file will be deleted once the job
-            and all its successors have completed running. If not the global file must be deleted
-            manually.
+            and all its successors have completed running. If not the global file 
+            must be deleted manually.
             
-            The write is asynchronous, so further modifications to the file pointed by
-            localFileName will result in undetermined behavior. The local file is safely removed 
-            at the end of the job by placing it in a location returned 
-            by getLocalTempDir.
+            The write is asynchronous, so further modifications during execution 
+            to the file pointed by localFileName will result in undetermined behavior. 
             """
             jobStoreFileID = self.jobStore.getEmptyFileStoreID(None 
                             if not cleanup else self.jobWrapper.jobStoreID)
             self.queue.put((open(localFileName, 'r'), jobStoreFileID))
-            #Now put the file into the cache
-            self.jobStoreFileIDToCacheLocation[jobStoreFileID] = localFileName
+            #Now put the file into the cache if it is a path within localTempDir
+            absLocalFileName = os.path.abspath(localFileName)
+            if absLocalFileName.startswith(self.localTempDir):
+                #Chmod to make file read only
+                os.chmod(absLocalFileName, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+                self.jobStoreFileIDToCacheLocation[jobStoreFileID] = absLocalFileName
             return jobStoreFileID
         
         def writeGlobalFileStream(self, cleanup=False):
