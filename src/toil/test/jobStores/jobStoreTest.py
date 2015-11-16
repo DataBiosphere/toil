DataBiosphere/toil

commit e60e396d0ce6523bc153fe3de2f5a8876e12785d

awsJobStore fixes- awsSortTest passes on local machine.
Load awsJobStore properly in common, and deserialize memory and cpu as floats, not ints, serialize command as Noneable, allow deletion of keys without a version- possible if file is created empty and never modified, fix unicode decode error when created a uuid for sharedFiles.

src/jobTree/common.py
@@ -348,8 +348,8 @@ def loadJobStore( jobStoreString, config=None ):
         return FileJobStore( jobStoreArgs, config=config )
     elif jobStoreName == 'aws':
         from jobTree.jobStores.awsJobStore import AWSJobStore
-
-        return AWSJobStore.create( jobStoreArgs, config=config )
+        region, namePrefix = jobStoreArgs.split( ':', 1 )
+        return AWSJobStore( region, namePrefix, config=config )
     else:
         raise RuntimeError( "Unknown job store implementation '%s'" % jobStoreName )
 

src/jobTree/jobStores/awsJobStore.py
@@ -157,8 +157,11 @@ class AWSJobStore( AbstractJobStore ):
                 with attempt:
                     self.versions.batch_delete_attributes( { item.name: None for item in items } )
             for item in items:
-                self.files.delete_key( key_name=item.name,
-                                       version_id=item[ 'version' ] )
+                if 'version' in item:
+                    self.files.delete_key( key_name=item.name,
+                                           version_id=item[ 'version' ] )
+                else:
+                    self.files.delete_key( key_name=item.name)
 
     def writeFile( self, jobStoreID, localFilePath ):
         jobStoreFileID = self._newFileID( )
@@ -371,7 +374,7 @@ class AWSJobStore( AbstractJobStore ):
         if sharedFileName is None:
             return str( uuid.uuid4( ) )
         else:
-            return str( uuid.uuid5( self.sharedFileJobID, sharedFileName ) )
+            return str( uuid.uuid5( self.sharedFileJobID, str(sharedFileName) ) )
 
     def _getFileVersionAndBucket( self, jobStoreFileID ):
         """
@@ -764,10 +767,10 @@ class AWSJob( Job ):
     """
     fromItemTransform = defaultdict( lambda: passThrough,
                                      predecessorNumber=int,
-                                     memory=int,
-                                     cpu=int,
+                                     memory=float,
+                                     cpu=float,
                                      updateID=str,
-                                     command=str,
+                                     command=toNoneable,
                                      stack=lambda v:map( literal_eval, toList( v )),
                                      jobsToDelete=toList,
                                      predecessorsFinished=toSet,
@@ -789,7 +792,7 @@ class AWSJob( Job ):
         return cls( jobStoreID=jobStoreID, **item )
 
     toItemTransform = defaultdict( lambda: passThrough,
-                                   command=str,
+                                   command=fromNoneable,
                                    jobStoreID=skip,
                                    updateID=str,
                                    children=skip,

src/jobTree/test/sort/sortTest.py
@@ -76,7 +76,6 @@ class SortTest(JobTreeTest, MesosTestSupport):
     def testJobTreeSortOnAWS(self):
         """Tests scriptTree/jobTree by sorting a file in parallel.
         """
-        return
         self.jobTreeSortTest(jobStore="aws:us-west-2:sort-test-%s" % uuid4(),
                       lines=100, N=100)
 

commit ebc53d86bbecb568945e24548293c37ee8173c70

Validate AWS job store name (resolves #542)
Regression from e60e396d0ce6523bc153fe3de2f5a8876e12785d

src/toil/common.py
@@ -136,7 +136,7 @@ class Config(object):
 
         setOption("environment", parseSetEnv)
 
-        # Resource requirements
+        #Resource requirements
         setOption("defaultMemory", h2b, iC(1))
         setOption("defaultCores", h2b, iC(1))
         setOption("defaultDisk", h2b, iC(1))
@@ -387,8 +387,7 @@ def loadJobStore( jobStoreString, config=None ):
         return FileJobStore( jobStoreArgs, config=config )
     elif jobStoreName == 'aws':
         from toil.jobStores.aws.jobStore import AWSJobStore
-        region, namePrefix = jobStoreArgs.split( ':', 1 )
-        return AWSJobStore( region, namePrefix, config=config )
+        return AWSJobStore.createJobStore( jobStoreArgs, config=config )
     elif jobStoreName == 'azure':
         from toil.jobStores.azureJobStore import AzureJobStore
         account, namePrefix = jobStoreArgs.split( ':', 1 )

src/toil/jobStores/aws/jobStore.py
@@ -57,6 +57,37 @@ class AWSJobStore(AbstractJobStore):
     item representing the job. UUIDs are used to identify jobs and files.
     """
 
+    @classmethod
+    def createJobStore(cls, jobStoreString, config=None):
+        region, namePrefix = jobStoreString.split(':')
+        if not cls.bucketNameRe.match(namePrefix):
+            raise ValueError("Invalid name prefix '%s'. Name prefixes must contain only digits, "
+                             "hyphens or lower-case letters and must not start or end in a "
+                             "hyphen." % namePrefix)
+        # Reserve 13 for separator and suffix
+        if len(namePrefix) > cls.maxBucketNameLen - cls.maxNameLen - len(cls.nameSeparator):
+            raise ValueError("Invalid name prefix '%s'. Name prefixes may not be longer than 50 "
+                             "characters." % namePrefix)
+        if '--' in namePrefix:
+            raise ValueError("Invalid name prefix '%s'. Name prefixes may not contain "
+                             "%s." % (namePrefix, cls.nameSeparator))
+        return cls(region, namePrefix, config=config)
+
+    # Dots in bucket names should be avoided because bucket names are used in HTTPS bucket
+    # URLs where the may interfere with the certificate common name. We use a double
+    # underscore as a separator instead.
+    #
+    bucketNameRe = re.compile(r'^[a-z0-9][a-z0-9-]+[a-z0-9]$')
+
+    # See http://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html
+    #
+    minBucketNameLen = 3
+    maxBucketNameLen = 63
+    maxNameLen = 10
+    nameSeparator = '--'
+
+    # Do not invoke the constructor, use the factory method above.
+
     def __init__(self, region, namePrefix, config=None):
         """
         Create a new job store in AWS or load an existing one from there.
@@ -91,6 +122,7 @@ class AWSJobStore(AbstractJobStore):
                 self._checkJobStoreCreation(create, exists, region + ":" + namePrefix)
 
         def qualify(name):
+            assert len(name) <= self.maxNameLen
             return self.namePrefix + self.nameSeparator + name
 
         self.jobsDomain = self._getOrCreateDomain(qualify('jobs'))
@@ -303,32 +335,6 @@ class AWSJobStore(AbstractJobStore):
         assert self._validateSharedFileName(sharedFileName)
         return self.getPublicUrl(self._sharedFileID(sharedFileName))
 
-    # Dots in bucket names should be avoided because bucket names are used in HTTPS bucket
-    # URLs where the may interfere with the certificate common name. We use a double
-    # underscore as a separator instead.
-    bucketNameRe = re.compile(r'^[a-z0-9][a-z0-9-]+[a-z0-9]$')
-
-    nameSeparator = '--'
-
-    @classmethod
-    def _parseArgs(cls, jobStoreString):
-        region, namePrefix = jobStoreString.split(':')
-        # See http://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html,
-        # reserve 10 characters for separator and suffixes
-        if not cls.bucketNameRe.match(namePrefix):
-            raise ValueError("Invalid name prefix '%s'. Name prefixes must contain only digits, "
-                             "hyphens or lower-case letters and must not start or end in a "
-                             "hyphen." % namePrefix)
-        # reserve 13 for separator and suffix
-        if len(namePrefix) > 50:
-            raise ValueError("Invalid name prefix '%s'. Name prefixes may not be longer than 50 "
-                             "characters." % namePrefix)
-        if '--' in namePrefix:
-            raise ValueError("Invalid name prefix '%s'. Name prefixes may not contain "
-                             "%s." % (namePrefix, cls.nameSeparator))
-
-        return region, namePrefix
-
     def _connectSimpleDB(self):
         """
         :rtype: SDBConnection
@@ -355,8 +361,8 @@ class AWSJobStore(AbstractJobStore):
         """
         :rtype: Bucket
         """
+        assert self.minBucketNameLen <= len(bucket_name) <= self.maxBucketNameLen
         assert self.bucketNameRe.match(bucket_name)
-        assert 3 <= len(bucket_name) <= 63
         try:
             bucket = self.s3.get_bucket(bucket_name, validate=True)
             assert versioning is self.__getBucketVersioning(bucket)

src/toil/test/jobStores/jobStoreTest.py
@@ -461,7 +461,7 @@ class AWSJobStoreTest(hidden.AbstractJobStoreTest):
         for encrypted in (True, False):
             self.assertTrue(AWSJobStore.FileInfo.maxInlinedSize(encrypted) < partSize)
         AWSJobStore.FileInfo.s3PartSize = partSize
-        return AWSJobStore(self.testRegion, self.namePrefix, config=config)
+        return AWSJobStore.createJobStore(self.testRegion + ':' + self.namePrefix, config=config)
 
     def testInlinedFiles(self):
         from toil.jobStores.aws.jobStore import AWSJobStore
@@ -495,7 +495,7 @@ class AWSJobStoreTest(hidden.AbstractJobStoreTest):
                 buf = f.read(self._partSize())
                 if not buf: break
                 hashOut.update(buf)
-        self.assertEqual(hashIn.digest(),hashOut.digest())
+        self.assertEqual(hashIn.digest(), hashOut.digest())
 
     def _largeLogEntrySize(self):
         from toil.jobStores.aws.jobStore import AWSJobStore
@@ -505,6 +505,21 @@ class AWSJobStoreTest(hidden.AbstractJobStoreTest):
     def _batchDeletionSize(self):
         from toil.jobStores.aws.jobStore import AWSJobStore
         return AWSJobStore.itemsPerBatchDelete
+
+
+@needs_aws
+class InvalidAWSJobStoreTest(ToilTest):
+    def testInvalidJobStoreName(self):
+        from toil.jobStores.aws.jobStore import AWSJobStore
+        self.assertRaises(ValueError,
+                          AWSJobStore.createJobStore,
+                          'us-west-2:a--b')
+        self.assertRaises(ValueError,
+                          AWSJobStore.createJobStore,
+                          'us-west-2:' + ('a' * 100))
+        self.assertRaises(ValueError,
+                          AWSJobStore.createJobStore,
+                          'us-west-2:a_b')
 
 
 @needs_azure