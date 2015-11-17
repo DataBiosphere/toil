d08fe8c43d0d18d4d4bdafb9c9c921fd937557bd
Hannes Schmidt <hannes@ucsc.edu>
2015-11-17 02:18:11 -0800

    Fix: Mesos batch system conflates all non-zero exit codes to 1

--- src/toil/batchSystems/mesos/batchSystem.py
+++ src/toil/batchSystems/mesos/batchSystem.py
@@ -434,35 +434,34 @@ class MesosBatchSystem(AbstractBatchSystem, mesos.interface.Scheduler):
 
     def statusUpdate(self, driver, update):
         """
-        Invoked when the status of a task has changed (e.g., a slave is lost and so the task is lost, a task finishes
-        and an executor sends a status update saying so, etc). Note that returning from this callback _acknowledges_
-        receipt of this status update! If for whatever reason the scheduler aborts during this callback (or the
-        process exits) another status update will be delivered (note, however, that this is currently not true if the
+        Invoked when the status of a task has changed (e.g., a slave is lost and so the task is
+        lost, a task finishes and an executor sends a status update saying so, etc). Note that
+        returning from this callback _acknowledges_ receipt of this status update! If for
+        whatever reason the scheduler aborts during this callback (or the process exits) another
+        status update will be delivered (note, however, that this is currently not true if the
         slave sending the status update is lost/fails during that time).
         """
-        log.debug("Task %s is in a state %s" % \
-                  (update.task_id.value, mesos_pb2.TaskState.Name(update.state)))
-
-        intID = int(update.task_id.value)  # toil keeps jobIds as ints
-        stringID = update.task_id.value  # Mesos keeps jobIds as strings
+        taskID = int(update.task_id.value)
+        stateName = mesos_pb2.TaskState.Name(update.state)
+        log.debug('Task %i is in state %s', taskID, stateName)
 
         try:
-            self.killSet.remove(intID)
+            self.killSet.remove(taskID)
         except KeyError:
             pass
         else:
-            self.killedSet.add(intID)
+            self.killedSet.add(taskID)
 
         if update.state == mesos_pb2.TASK_FINISHED:
-            self.__updateState(intID, 0)
-
-        if update.state == mesos_pb2.TASK_LOST or \
-                        update.state == mesos_pb2.TASK_FAILED or \
-                        update.state == mesos_pb2.TASK_KILLED or \
-                        update.state == mesos_pb2.TASK_ERROR:
-            log.warning("Task %s is in unexpected state %s with message '%s'" \
-                        % (stringID, mesos_pb2.TaskState.Name(update.state), update.message))
-            self.__updateState(intID, 1)
+            self.__updateState(taskID, 0)
+        elif update.state == mesos_pb2.TASK_FAILED:
+            exitStatus = int(update.message)
+            log.warning('Task %i failed with exit status %i', taskID, exitStatus)
+            self.__updateState(taskID, exitStatus)
+        elif update.state in (mesos_pb2.TASK_LOST, mesos_pb2.TASK_KILLED, mesos_pb2.TASK_ERROR):
+            log.warning("Task %i is in unexpected state %s with message '%s'",
+                        taskID, stateName, update.message)
+            self.__updateState(taskID, 255)
 
         # Explicitly acknowledge the update if implicit acknowledgements are not being used.
         if not self.implicitAcknowledgements:
