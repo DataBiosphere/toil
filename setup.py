d8ab5b0221502955adf673d078b0eb6f1b2a2de6
Hannes Schmidt <hannes@ucsc.edu>
2015-11-21 18:46:00 -0800

    Bump and and pin cwltool dependency at 1.0.20151112194920
    http://jenkins.cgcloud.info/job/toil-pull-requests/653/testReport/junit/src.toil.test.cwl.cwlTest/CWLTest/test_run_revsort/

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ kwargs = dict(
         'encryption': [
             'pynacl==0.3.0'],
         'cwl': [
-            'cwltool>=1.0.20150918080732']},
+            'cwltool==1.0.20151112194920']},
     package_dir={'': 'src'},
     packages=find_packages('src', exclude=['*.test']),
     entry_points={
