3b4b93c34df1762c50678295850a6cc6cc28cea2
Hannes Schmidt <hannes@ucsc.edu>
2015-12-04 14:51:37 -0800

    Fix: stats/logging collator process fails if worker or general log is absent (resolves #609)

--- src/toil/leader.py
+++ src/toil/leader.py
@@ -44,20 +44,23 @@ def statsAndLoggingAggregatorProcess(jobStore, stop):
 
     def callback(fileHandle):
         stats = json.load(fileHandle, object_hook=Expando)
-        workers = stats.workers
         try:
-            logs = workers.log
+            logs = stats.workers.log
         except AttributeError:
             # To be expected if there were no calls to logToMaster()
             pass
         else:
             for message in logs:
                 logger.log(int(message.level),
-                           "Got message from job at time: %s : %s",
-                           time.strftime("%m-%d-%Y %H:%M:%S"), message.text)
-
-        for log in stats.logs:
-            logger.info("%s:     %s", log.jobStoreID, log.text)
+                           'Got message from job at time %s: %s',
+                           time.strftime('%m-%d-%Y %H:%M:%S'), message.text)
+        try:
+            logs = stats.logs
+        except AttributeError:
+            pass
+        else:
+            for log in logs:
+                logger.info("%s:    %s", log.jobStoreID, log.text)
 
     while True:
         # This is a indirect way of getting a message to the process to exit
