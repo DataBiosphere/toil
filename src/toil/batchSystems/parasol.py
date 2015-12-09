907200a78ab577bfa63dfab114e7f282cbc554c1
Hannes Schmidt <hannes@ucsc.edu>
2015-12-09 11:56:42 -0800

    Add sub-second wall time tracking to Parasol

--- src/toil/batchSystems/parasol.py
+++ src/toil/batchSystems/parasol.py
@@ -291,7 +291,7 @@ class ParasolBatchSystem(BatchSystemSupport):
         char *user;    /* User who ran job */
         char *errFile;    /* Location of stderr file on host */
 
-        plus you finally have the command name..
+        Plus you finally have the command name.
         """
         resultsFiles = set()
         resultsFileHandles = []
@@ -318,9 +318,22 @@ class ParasolBatchSystem(BatchSystemSupport):
                         else:
                             status = -status
                         self.cpuUsageQueue.put(jobId)
-                        endTime = float(endTime)
-                        startTime = float(startTime)
-                        self.updatedJobsQueue.put((jobId, status, endTime - startTime))
+                        startTime = int(startTime)
+                        endTime = int(endTime)
+                        if endTime == startTime:
+                            # Both, start and end time is an integer so to get sub-second
+                            # accuracy we use the ticks reported by Parasol as an approximation.
+                            # This isn't documented but what Parasol calls "ticks" is actually a
+                            # hundredth of a second. Parasol does the unit conversion early on
+                            # after a job finished. Search paraNode.c for ticksToHundreths. We
+                            # also cheat a little by always reporting at least one hundredth of a
+                            # second.
+                            usrTicks = int(usrTicks)
+                            sysTicks = int(sysTicks)
+                            wallTime = float( max( 1, usrTicks + sysTicks) ) * 0.01
+                        else:
+                            wallTime = float(endTime - startTime)
+                        self.updatedJobsQueue.put((jobId, status, wallTime))
                 time.sleep(1)
         except:
             logger.warn("Error occurred while parsing parasol results files.")
