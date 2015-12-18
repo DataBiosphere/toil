c8d28d21251d31496a83463c5f7dd07da1edce02
Hannes Schmidt <hannes@ucsc.edu>
2015-12-17 22:21:36 -0800

    Fix: Mesos tasks aren't sorted by type (resolves #625)

--- src/toil/batchSystems/mesos/batchSystem.py
+++ src/toil/batchSystems/mesos/batchSystem.py
@@ -295,7 +295,7 @@ class MesosBatchSystem(AbstractBatchSystem, mesos.interface.Scheduler):
         job_types = self.jobQueueList.keys()
         # sorts from largest to smallest core usage
         # TODO: add a size() method to ResourceSummary and use it as the key. Ask me why.
-        job_types.sort(key=lambda resourceRequirement: ResourceRequirement.cores)
+        job_types.sort(key=lambda resourceRequirement: resourceRequirement.cores)
         job_types.reverse()
         return job_types
 
