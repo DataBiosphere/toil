d5d91cd72cc13d62ebaa2217c3709bbafc1e17c5
Hannes Schmidt <hannes@ucsc.edu>
2016-01-19 16:47:57 -0800

    Add config.workflowID to distinguish between consecutive successful workflows

--- src/toil/common.py
+++ src/toil/common.py
@@ -35,7 +35,12 @@ class Config(object):
     Class to represent configuration operations for a toil workflow run. 
     """
     def __init__(self):
-        #Core options
+        # Core options
+        self.workflowID = None
+        """This attribute uniquely identifies the job store and therefore the workflow. It is
+        necessary in order to distinguish between two consequitive workflows for which
+        self.jobStore is the same, e.g. when a job store name is reused after a previous run has
+        finished sucessfully and its job store has been clean up."""
         self.jobStore = os.path.abspath("./toil")
         self.logLevel = getLogLevelString()
         self.workDir = None
--- src/toil/jobStores/fileJobStore.py
+++ src/toil/jobStores/fileJobStore.py
@@ -30,8 +30,9 @@ from toil.jobWrapper import JobWrapper
 logger = logging.getLogger( __name__ )
 
 class FileJobStore(AbstractJobStore):
-    """Represents the toil using a network file system. For doc-strings
-    of functions see AbstractJobStore.
+    """
+    Represents the toil using a network file system. For doc-strings of functions see
+    AbstractJobStore.
     """
 
     def __init__(self, jobStoreDir, config=None):
@@ -41,21 +42,23 @@ class FileJobStore(AbstractJobStore):
         :raise RuntimeError: if config != None and the jobStore already exists or
         config == None and the jobStore does not already exists. 
         """
-        #This is root directory in which everything in the store is kept
+        # This is root directory in which everything in the store is kept
         self.jobStoreDir = absSymPath(jobStoreDir)
         logger.info("Jobstore directory is: %s", self.jobStoreDir)
-        #Safety checks for existing jobStore
-        self._checkJobStoreCreation(config != None, os.path.exists(self.jobStoreDir), self.jobStoreDir)
-        #Directory where temporary files go
+        # Safety checks for existing jobStore
+        self._checkJobStoreCreation(create=config is not None,
+                                    exists=os.path.exists(self.jobStoreDir),
+                                    jobStoreString=self.jobStoreDir)
+        # Directory where temporary files go
         self.tempFilesDir = os.path.join(self.jobStoreDir, "tmp")
-        #Creation of jobStore, if necessary
-        if config != None:
+        # Creation of jobStore, if necessary
+        if config is not None:
             os.mkdir(self.jobStoreDir)
             os.mkdir(self.tempFilesDir)
-        #Parameters for creating temporary files
+        # Parameters for creating temporary files
         self.validDirs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
         self.levels = 2
-        super( FileJobStore, self ).__init__( config=config )
+        super(FileJobStore, self).__init__(config=config)
 
     def deleteJobStore(self):
         if os.path.exists(self.jobStoreDir):
