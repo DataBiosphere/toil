e5a2895ff8a43c2ed28fff2912812f9cf4650a24
Hannes Schmidt <hannes@ucsc.edu>
2016-02-10 00:58:10 -0800

    Fix: ValueError for failed Mesos tasks kills framework (resolves #671)
    This just makes handling of exceptions that occcur while running tasks in the executor more robust. Also logs the exception on the executor and includes the Popen setup in the try block.

--- src/toil/batchSystems/mesos/executor.py
+++ src/toil/batchSystems/mesos/executor.py
@@ -98,21 +98,25 @@ class MesosExecutor(mesos.interface.Executor):
         def runTask():
             log.debug("Running task %s", task.task_id.value)
             sendUpdate(mesos_pb2.TASK_RUNNING)
-            popen = runJob(pickle.loads(task.data))
-            self.runningTasks[task.task_id.value] = popen.pid
             try:
-                exitStatus = popen.wait()
-                if 0 == exitStatus:
-                    sendUpdate(mesos_pb2.TASK_FINISHED)
-                elif -9 == exitStatus:
-                    sendUpdate(mesos_pb2.TASK_KILLED)
-                else:
-                    sendUpdate(mesos_pb2.TASK_FAILED, message=str(exitStatus))
+                popen = runJob(pickle.loads(task.data))
+                self.runningTasks[task.task_id.value] = popen.pid
+                try:
+                    exitStatus = popen.wait()
+                    if 0 == exitStatus:
+                        sendUpdate(mesos_pb2.TASK_FINISHED)
+                    elif -9 == exitStatus:
+                        sendUpdate(mesos_pb2.TASK_KILLED)
+                    else:
+                        sendUpdate(mesos_pb2.TASK_FAILED, message=str(exitStatus))
+                finally:
+                    del self.runningTasks[task.task_id.value]
             except:
-                exc_type, exc_value, exc_trace = sys.exc_info()
-                sendUpdate(mesos_pb2.TASK_FAILED, message=str(traceback.format_exception_only(exc_type, exc_value)))
-            finally:
-                del self.runningTasks[task.task_id.value]
+                exc_info = sys.exc_info()
+                log.error('Exception while running task:', exc_info=exc_info)
+                exc_type, exc_value, exc_trace = exc_info
+                sendUpdate(mesos_pb2.TASK_FAILED,
+                           message=''.join(traceback.format_exception_only(exc_type, exc_value)))
 
         def runJob(job):
             """
