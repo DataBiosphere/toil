9ae4eb510f87d7891ffa20a89dbe93140633becc
Jacob Pfeil <jpfeil@ucsc.edu>
2016-06-07 21:30:10 -0700

    Fix typo in job.py docstring

--- src/toil/job.py
+++ src/toil/job.py
@@ -2244,12 +2244,12 @@ class Job(object):
         # method.
         with jobStore.writeFileStream(rootJobWrapper.jobStoreID) as (fileHandle, fileStoreID):
             cPickle.dump(self, fileHandle, cPickle.HIGHEST_PROTOCOL)
-        # Note that getUserScript() may have beeen overridden. This is intended. If we used
+        # Note that getUserScript() may have been overridden. This is intended. If we used
         # self.userModule directly, we'd be getting a reference to job.py if the job was
         # specified as a function (as opposed to a class) since that is where FunctionWrappingJob
         #  is defined. What we really want is the module that was loaded as __main__,
         # and FunctionWrappingJob overrides getUserScript() to give us just that. Only then can
-        # filter_main() in _unpickle( ) do its job of resolveing any user-defined type or function.
+        # filter_main() in _unpickle( ) do its job of resolving any user-defined type or function.
         userScript = self.getUserScript().globalize()
         jobsToJobWrappers[self].command = ' '.join( ('_toil', fileStoreID) + userScript)
         #Update the status of the jobWrapper on disk
