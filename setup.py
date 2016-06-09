DataBiosphere/toil

commit 99bf46e2f7f60d0baf29617cb644dc82139e201b

Add promised requirements (resolves #729)

setup.py
@@ -25,7 +25,12 @@ setup(
     author_email='benedict@soe.usc.edu',
     url="https://github.com/BD2KGenomics/toil",
     install_requires=[
-        'bd2k-python-lib==1.13.dev14'],
+        'bd2k-python-lib==1.13.dev14',
+        'dill==0.2.5'],
+    tests_require=[
+        'mock==1.0.1',
+        'pytest==2.8.3'],
+    test_suite='toil',
     extras_require={
         'mesos': [
             'psutil==3.0.1'],

src/toil/job.py
@@ -30,6 +30,7 @@ import sys
 import tempfile
 import time
 import uuid
+import dill
 
 from abc import ABCMeta, abstractmethod
 from argparse import ArgumentParser
@@ -209,7 +210,10 @@ class Job(object):
         :return: The new child job that wraps fn.
         :rtype: toil.job.FunctionWrappingJob
         """
-        return self.addChild(FunctionWrappingJob(fn, *args, **kwargs))
+        if PromisedRequirement.convertPromises(kwargs):
+            return self.addChild(PromisedRequirementFunctionWrappingJob(fn, *args, **kwargs))
+        else:
+            return self.addChild(FunctionWrappingJob(fn, *args, **kwargs))
 
     def addFollowOnFn(self, fn, *args, **kwargs):
         """
@@ -221,7 +225,10 @@ class Job(object):
         :return: The new follow-on job that wraps fn.
         :rtype: toil.job.FunctionWrappingJob
         """
-        return self.addFollowOn(FunctionWrappingJob(fn, *args, **kwargs))
+        if PromisedRequirement.convertPromises(kwargs):
+            return self.addFollowOn(PromisedRequirementFunctionWrappingJob(fn, *args, **kwargs))
+        else:
+            return self.addFollowOn(FunctionWrappingJob(fn, *args, **kwargs))
 
     def addChildJobFn(self, fn, *args, **kwargs):
         """
@@ -234,7 +241,10 @@ class Job(object):
         :return: The new child job that wraps fn.
         :rtype: toil.job.JobFunctionWrappingJob
         """
-        return self.addChild(JobFunctionWrappingJob(fn, *args, **kwargs))
+        if PromisedRequirement.convertPromises(kwargs):
+            return self.addChild(PromisedRequirementJobFunctionWrappingJob(fn, *args, **kwargs))
+        else:
+            return self.addChild(JobFunctionWrappingJob(fn, *args, **kwargs))
 
     def addFollowOnJobFn(self, fn, *args, **kwargs):
         """
@@ -247,7 +257,10 @@ class Job(object):
         :return: The new follow-on job that wraps fn.
         :rtype: toil.job.JobFunctionWrappingJob
         """
-        return self.addFollowOn(JobFunctionWrappingJob(fn, *args, **kwargs))
+        if PromisedRequirement.convertPromises(kwargs):
+            return self.addFollowOn(PromisedRequirementJobFunctionWrappingJob(fn, *args, **kwargs))
+        else:
+            return self.addFollowOn(JobFunctionWrappingJob(fn, *args, **kwargs))
 
     @staticmethod
     def wrapFn(fn, *args, **kwargs):
@@ -261,7 +274,10 @@ class Job(object):
         :return: The new function that wraps fn.
         :rtype: toil.job.FunctionWrappingJob
         """
-        return FunctionWrappingJob(fn, *args, **kwargs)
+        if PromisedRequirement.convertPromises(kwargs):
+            return PromisedRequirementFunctionWrappingJob(fn, *args, **kwargs)
+        else:
+            return FunctionWrappingJob(fn, *args, **kwargs)
 
     @staticmethod
     def wrapJobFn(fn, *args, **kwargs):
@@ -275,7 +291,10 @@ class Job(object):
         :return: The new job function that wraps fn.
         :rtype: toil.job.JobFunctionWrappingJob
         """
-        return JobFunctionWrappingJob(fn, *args, **kwargs)
+        if PromisedRequirement.convertPromises(kwargs):
+            return PromisedRequirementJobFunctionWrappingJob(fn, *args, **kwargs)
+        else:
+            return JobFunctionWrappingJob(fn, *args, **kwargs)
 
     def encapsulate(self):
         """
@@ -2428,12 +2447,14 @@ class Job(object):
         """
         return self.__class__.__name__
 
+
 class JobException( Exception ):
     """
     General job exception.
     """
     def __init__( self, message ):
         super( JobException, self ).__init__( message )
+
 
 class JobGraphDeadlockException( JobException ):
     """
@@ -2524,6 +2545,7 @@ class FunctionWrappingJob(Job):
     def _jobName(self):
         return ".".join((self.__class__.__name__,self.userFunctionModule.name,self.userFunctionName))
 
+
 class JobFunctionWrappingJob(FunctionWrappingJob):
     """
     A job function is a function whose first argument is a :class:`job.Job` \
@@ -2540,6 +2562,54 @@ class JobFunctionWrappingJob(FunctionWrappingJob):
         self.fileStore = fileStore
         rValue = userFunction(*((self,) + tuple(self._args)), **self._kwargs)
         return rValue
+
+
+class PromisedRequirementFunctionWrappingJob(FunctionWrappingJob):
+    """
+    Handles dynamic resource allocation using :class:`toil.job.Promise` instances.
+    Spawns child function using parent function parameters and fulfilled promised
+    resource requirements. See :class:`toil.job.FunctionWrappingJob` class
+    """
+    def __init__(self, userFunction, *args, **kwargs):
+        self.promisedRequirements = {}
+        requirements = {'disk': '1M', 'memory': '32M', 'cores': 0.1}
+        # Replace PromisedRequirements in intermediate job with small
+        # resource requirements.
+        for name, value in requirements.items():
+            try:
+                if isinstance(kwargs[name], PromisedRequirement):
+                    self.promisedRequirements[name] = kwargs[name]
+                    kwargs[name] = value
+            except KeyError:
+                pass
+        super(PromisedRequirementFunctionWrappingJob, self).__init__(userFunction, *args, **kwargs)
+
+    def run(self, fileStore):
+        # Assumes promises are fulfilled when parent job is run
+        self.evaluatePromisedRequirements()
+        userFunction = self._getUserFunction()
+        return self.addChildFn(userFunction, *self._args, **self._kwargs).rv()
+
+    def evaluatePromisedRequirements(self):
+        requirements = ["disk", "memory", "cores"]
+        for requirement in requirements:
+            try:
+                self._kwargs[requirement] = self.promisedRequirements[requirement].getValue()
+            except KeyError:
+                self._kwargs[requirement] = getattr(self, requirement)
+
+
+class PromisedRequirementJobFunctionWrappingJob(PromisedRequirementFunctionWrappingJob):
+    """
+    Handles dynamic resource allocation for job functions.
+    See :class:`toil.job.JobFunctionWrappingJob`
+    """
+
+    def run(self, fileStore):
+        self.evaluatePromisedRequirements()
+        userFunction = self._getUserFunction()
+        return self.addChildJobFn(userFunction, *self._args, **self._kwargs).rv()
+
 
 class EncapsulatedJob(Job):
     """
@@ -2744,3 +2814,63 @@ class Promise(object):
             # corrupted
             value = cPickle.load(fileHandle)
             return value
+
+
+class PromisedRequirement(object):
+    def __init__(self, valueOrCallable, *args):
+        """
+        Class for dynamically allocating job function resource requirements involving
+        :class:`toil.job.Promise` instances.
+
+        Use when resource requirements depend on the return value of a parent function.
+        PromisedRequirements can be modified by passing a function that takes the
+        :class:`Promise` as input.
+
+        For example, let f, g, and h be functions. Then a Toil workflow can be
+        defined as follows::
+            A = Job.wrapFn(f)
+            B = A.addChildFn(g, cores=PromisedRequirement(A.rv())
+            C = B.addChildFn(h, cores=PromisedRequirement(lambda x: 2*x, B.rv()))
+
+        :param valueOrCallable: A single Promise instance or a function that
+                                takes \*args as input parameters.
+        :param int|Promise *args: variable length argument list
+        """
+        if hasattr(valueOrCallable, '__call__'):
+            assert len(args) != 0, 'Need parameters for PromisedRequirement function.'
+            func = valueOrCallable
+        else:
+            assert len(args) == 0, 'Define a PromisedRequirement function to handle multiple arguments.'
+            func = lambda x: x
+            args = [valueOrCallable]
+
+        self._func = dill.dumps(func)
+        self._args = list(args)
+
+    def getValue(self):
+        """
+        Returns PromisedRequirement value
+        """
+        func = dill.loads(self._func)
+        return func(*self._args)
+
+    @staticmethod
+    def convertPromises(kwargs):
+        """
+        Returns True if reserved resource keyword is a Promise or
+        PromisedRequirement instance. Converts Promise instance
+        to PromisedRequirement.
+
+        :param kwargs: function keyword arguments
+        :return: bool
+        """
+        requirements = ["disk", "memory", "cores"]
+        foundPromisedRequirement = False
+        for r in requirements:
+            if isinstance(kwargs.get(r), Promise):
+                kwargs[r] = PromisedRequirement(kwargs[r])
+                foundPromisedRequirement = True
+            elif isinstance(kwargs.get(r), PromisedRequirement):
+                foundPromisedRequirement = True
+        return foundPromisedRequirement
+

src/toil/test/batchSystems/batchSystemTest.py
@@ -204,9 +204,9 @@ class hidden:
 
         __metaclass__ = ABCMeta
 
-        cpu_count = multiprocessing.cpu_count()
-        allocated_cores = sorted({1, 2, cpu_count})
-        sleep_time = 5
+        cpuCount = multiprocessing.cpu_count()
+        allocatedCores = sorted({1, 2, cpuCount})
+        sleepTime = 5
 
         @abstractmethod
         def getBatchSystemName(self):
@@ -226,27 +226,26 @@ class hidden:
             """
             Tests that the batch system is allocating core resources properly for concurrent tasks.
             """
-            for cores_per_job in self.allocated_cores:
-                temp_dir = self._createTempDir('testFiles')
+            for coresPerJob in self.allocatedCores:
+                tempDir = self._createTempDir('testFiles')
 
                 options = Job.Runner.getDefaultOptions(self._getTestJobStorePath())
-                options.workDir = temp_dir
-                options.maxCores = self.cpu_count
+                options.workDir = tempDir
+                options.maxCores = self.cpuCount
                 options.batchSystem = self.batchSystemName
 
-                counter_path = os.path.join(temp_dir, 'counter')
-                resetCounters(counter_path)
-                value, max_value = getCounters(counter_path)
-                assert (value, max_value) == (0, 0)
+                counterPath = os.path.join(tempDir, 'counter')
+                resetCounters(counterPath)
+                value, maxValue = getCounters(counterPath)
+                assert (value, maxValue) == (0, 0)
 
                 root = Job()
-                for _ in range(self.cpu_count):
-                    root.addFollowOn(Job.wrapFn(measureConcurrency, counter_path, self.sleep_time,
-                                                cores=cores_per_job, memory='1M', disk='1Mi'))
+                for _ in range(self.cpuCount):
+                    root.addFollowOn(Job.wrapFn(measureConcurrency, counterPath, self.sleepTime,
+                                                cores=coresPerJob, memory='1M', disk='1Mi'))
                 Job.Runner.startToil(root, options)
-
-                _, max_value = getCounters(counter_path)
-                self.assertEqual(max_value, self.cpu_count / cores_per_job)
+                _, maxValue = getCounters(counterPath)
+                self.assertEqual(maxValue, self.cpuCount / coresPerJob)
 
 
 @needs_mesos
@@ -394,7 +393,7 @@ class MaxCoresSingleMachineBatchSystemTest(ToilTest):
                                 jobIds.remove(jobId)
                         finally:
                             bs.shutdown()
-                        concurrentTasks, maxConcurrentTasks = self.getCounters()
+                        concurrentTasks, maxConcurrentTasks = getCounters(self.counterPath)
                         self.assertEquals(concurrentTasks, 0)
                         log.info('maxCores: {maxCores}, '
                                  'coresPerJob: {coresPerJob}, '
@@ -402,18 +401,8 @@ class MaxCoresSingleMachineBatchSystemTest(ToilTest):
                         # This is the key assertion:
                         expectedMaxConcurrentTasks = min(maxCores / coresPerJob, jobs)
                         self.assertEquals(maxConcurrentTasks, expectedMaxConcurrentTasks)
-                        self.resetCounters()
+                        resetCounters(self.counterPath)
 
-    def getCounters(self):
-        with open(self.counterPath, 'r+') as f:
-            s = f.read()
-            log.info('Counter is %s', s)
-            concurrentTasks, maxConcurrentTasks = map(int, s.split(','))
-        return concurrentTasks, maxConcurrentTasks
-
-    def resetCounters(self):
-        with open(self.counterPath, 'w') as f:
-            f.write('0,0')
 
     @skipIf(SingleMachineBatchSystem.numCores < 3, 'Need at least three cores to run this test')
     def testServices(self):
@@ -425,7 +414,7 @@ class MaxCoresSingleMachineBatchSystemTest(ToilTest):
         with open(self.counterPath, 'r+') as f:
             s = f.read()
         log.info('Counter is %s', s)
-        self.assertEqual(self.getCounters(), (0, 3))
+        self.assertEqual(getCounters(self.counterPath), (0, 3))
 
 
 # Toil can use only top-level functions so we have to add them here:
@@ -607,7 +596,7 @@ class MesosBatchSystemJobTest(hidden.AbstractBatchSystemJobTest, MesosTestSuppor
     """
 
     def getBatchSystemName(self):
-        self._startMesos(self.cpu_count)
+        self._startMesos(self.cpuCount)
         return "mesos"
 
     def tearDown(self):

src/toil/test/src/promisedRequirementTest.py
@@ -1,0 +1,163 @@
+# Copyright (C) 2015 UCSC Computational Genomics Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import, print_function
+import os
+import logging
+import toil.test.batchSystems.batchSystemTest as batchSystemTest
+
+from toil.job import Job
+from toil.job import PromisedRequirement
+from toil.test import needs_mesos
+from toil.batchSystems.mesos.test import MesosTestSupport
+
+log = logging.getLogger(__name__)
+
+
+class hidden:
+    """
+    Hide abstract base class from unittest's test case loader.
+
+    http://stackoverflow.com/questions/1323455/python-unit-test-with-base-and-sub-class#answer-25695512
+    """
+
+    class AbstractPromisedRequirementsTest(batchSystemTest.hidden.AbstractBatchSystemJobTest):
+        """
+        An abstract base class for testing Toil workflows with promised requirements.
+        """
+
+        def testPromisedRequirementDynamic(self):
+            """
+            Asserts that promised core resources are allocated properly using a dynamic Toil workflow
+            """
+            for coresPerJob in self.allocatedCores:
+                tempDir = self._createTempDir('testFiles')
+                counterPath = self.getCounterPath(tempDir)
+
+                root = Job.wrapJobFn(maxConcurrency, self.cpuCount, counterPath, coresPerJob,
+                                     cores=1, memory='1M', disk='1M')
+                values = Job.Runner.startToil(root, self.getOptions(tempDir))
+                maxValue = max(values)
+                self.assertEqual(maxValue, self.cpuCount / coresPerJob)
+
+        def testPromisedRequirementStatic(self):
+            """
+            Asserts that promised core resources are allocated properly using a static DAG
+            """
+            for coresPerJob in self.allocatedCores:
+                tempDir = self._createTempDir('testFiles')
+                counterPath = self.getCounterPath(tempDir)
+
+                root = Job()
+                one = Job.wrapFn(getOne, cores=0.1, memory='32M', disk='1M')
+                thirtyTwoMb = Job.wrapFn(getThirtyTwoMb, cores=0.1, memory='32M', disk='1M')
+                root.addChild(one)
+                root.addChild(thirtyTwoMb)
+                for _ in range(self.cpuCount):
+                    root.addFollowOn(Job.wrapFn(batchSystemTest.measureConcurrency, counterPath,
+                                                cores=PromisedRequirement(lambda x: x * coresPerJob, one.rv()),
+                                                memory=PromisedRequirement(thirtyTwoMb.rv()),
+                                                disk='1M'))
+                Job.Runner.startToil(root, self.getOptions(tempDir))
+                _, maxValue = batchSystemTest.getCounters(counterPath)
+                self.assertEqual(maxValue, self.cpuCount / coresPerJob)
+
+        def getOptions(self, tempDir):
+            """
+            Configures options for Toil workflow and makes job store.
+            :param str tempDir: path to test directory
+            :return: Toil options object
+            """
+            options = Job.Runner.getDefaultOptions(self._getTestJobStorePath())
+            options.logLevel = "DEBUG"
+            options.batchSystem = self.batchSystemName
+            options.workDir = tempDir
+            options.maxCores = self.cpuCount
+            return options
+
+        def getCounterPath(self, tempDir):
+            """
+            Returns path to a counter file
+            :param str tempDir: path to test directory
+            :return: path to counter file
+            """
+            counterPath = os.path.join(tempDir, 'counter')
+            batchSystemTest.resetCounters(counterPath)
+            minValue, maxValue = batchSystemTest.getCounters(counterPath)
+            assert (minValue, maxValue) == (0, 0)
+            return counterPath
+
+        def testJobConcurrency(self):
+            """
+            Override test in batchSystemTest.hidden.AbstractBatchSystemJobTest
+            """
+            pass
+
+
+def maxConcurrency(job, cpuCount, filename, coresPerJob):
+    """
+    Returns the max number of concurrent tasks when using a PromisedRequirement instance
+    to allocate the number of cores per job.
+
+    :param int cpuCount: number of available cpus
+    :param str filename: path to counter file
+    :param int coresPerJob: number of cores assigned to each job
+    :return int max concurrency value:
+    """
+    one = job.addChildFn(getOne, cores=0.1, memory='32M', disk='1M')
+    thirtyTwoMb = job.addChildFn(getThirtyTwoMb, cores=0.1, memory='32M', disk='1M')
+
+    values = []
+    for _ in range(cpuCount):
+        value = job.addFollowOnFn(batchSystemTest.measureConcurrency, filename,
+                                  cores=PromisedRequirement(lambda x: x * coresPerJob, one.rv()),
+                                  memory=PromisedRequirement(thirtyTwoMb.rv()),
+                                  disk='1M').rv()
+        values.append(value)
+    return values
+
+
+def getOne():
+    return 1
+
+
+def getThirtyTwoMb():
+    return '32M'
+
+
+class SingleMachinePromisedRequirementsTest(hidden.AbstractPromisedRequirementsTest):
+    """
+    Tests against the SingleMachine batch system
+    """
+
+    def getBatchSystemName(self):
+        return "singleMachine"
+
+    def tearDown(self):
+        pass
+
+
+@needs_mesos
+class MesosPromisedRequirementsTest(hidden.AbstractPromisedRequirementsTest, MesosTestSupport):
+    """
+    Tests against the Mesos batch system
+    """
+
+    def getBatchSystemName(self):
+        self._startMesos(self.cpuCount)
+        return "mesos"
+
+    def tearDown(self):
+        self._stopMesos()
+

commit d3562b532fa8c75ea2013f75089e273899009fd0

Fix merge error introduced by 99bf46e2f7f60d0baf29617cb644dc82139e201b

setup.py
@@ -27,10 +27,6 @@ setup(
     install_requires=[
         'bd2k-python-lib==1.13.dev14',
         'dill==0.2.5'],
-    tests_require=[
-        'mock==1.0.1',
-        'pytest==2.8.3'],
-    test_suite='toil',
     extras_require={
         'mesos': [
             'psutil==3.0.1'],