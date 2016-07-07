5e82316b1b527ce6a2ae0f5fd33ad5a0b9215733
Hannes Schmidt <hannes@ucsc.edu>
2016-07-06 20:10:11 -0700

    Refactor how job stores handle pipes during streaming (fixes #1044)
    This greatly reduces the duplication of this rather complex piece.

--- src/toil/jobStores/aws/jobStore.py
+++ src/toil/jobStores/aws/jobStore.py
@@ -31,7 +31,6 @@ import repr as reprlib
 from bd2k.util import strict_bool
 from bd2k.util.exceptions import panic
 from bd2k.util.objects import InnerClass
-from bd2k.util.threading import ExceptionalThread
 from boto.sdb.domain import Domain
 from boto.s3.bucket import Bucket
 from boto.s3.connection import S3Connection
@@ -58,6 +57,7 @@ from toil.jobStores.aws.utils import (SDBHelper,
                                       retry_s3,
                                       bucket_location_to_region,
                                       region_to_bucket_location)
+from toil.jobStores.utils import WritablePipe, ReadablePipe
 from toil.jobWrapper import JobWrapper
 import toil.lib.encryption as encryption
 
@@ -1021,66 +1021,64 @@ class AWSJobStore(AbstractJobStore):
 
         @contextmanager
         def uploadStream(self, multipart=True, allowInlining=True):
+            info = self
             store = self.outer
-            readable_fh, writable_fh = os.pipe()
-            with os.fdopen(readable_fh, 'r') as readable:
-                with os.fdopen(writable_fh, 'w') as writable:
-                    def multipartReader():
-                        buf = readable.read(store.partSize)
-                        if allowInlining and len(buf) <= self._maxInlinedSize():
-                            self.content = buf
-                        else:
-                            headers = self._s3EncryptionHeaders()
-                            for attempt in retry_s3():
-                                with attempt:
-                                    upload = store.filesBucket.initiate_multipart_upload(
-                                        key_name=self.fileID,
-                                        headers=headers)
-                            try:
-                                for part_num in itertools.count():
-                                    # There must be at least one part, even if the file is empty.
-                                    if len(buf) == 0 and part_num > 0:
-                                        break
-                                    for attempt in retry_s3():
-                                        with attempt:
-                                            upload.upload_part_from_file(fp=StringIO(buf),
-                                                                         # part numbers are 1-based
-                                                                         part_num=part_num + 1,
-                                                                         headers=headers)
-                                    if len(buf) == 0:
-                                        break
-                                    buf = readable.read(self.outer.partSize)
-                            except:
-                                with panic(log=log):
-                                    for attempt in retry_s3():
-                                        with attempt:
-                                            upload.cancel_upload()
-                            else:
+
+            class MultiPartPipe(WritablePipe):
+                def readFrom(self, readable):
+                    buf = readable.read(store.partSize)
+                    if allowInlining and len(buf) <= info._maxInlinedSize():
+                        info.content = buf
+                    else:
+                        headers = info._s3EncryptionHeaders()
+                        for attempt in retry_s3():
+                            with attempt:
+                                upload = store.filesBucket.initiate_multipart_upload(
+                                    key_name=info.fileID,
+                                    headers=headers)
+                        try:
+                            for part_num in itertools.count():
+                                # There must be at least one part, even if the file is empty.
+                                if len(buf) == 0 and part_num > 0:
+                                    break
                                 for attempt in retry_s3():
                                     with attempt:
-                                        self.version = upload.complete_upload().version_id
-
-                    def reader():
-                        buf = readable.read()
-                        if allowInlining and len(buf) <= self._maxInlinedSize():
-                            self.content = buf
+                                        upload.upload_part_from_file(fp=StringIO(buf),
+                                                                     # part numbers are 1-based
+                                                                     part_num=part_num + 1,
+                                                                     headers=headers)
+                                if len(buf) == 0:
+                                    break
+                                buf = readable.read(info.outer.partSize)
+                        except:
+                            with panic(log=log):
+                                for attempt in retry_s3():
+                                    with attempt:
+                                        upload.cancel_upload()
                         else:
-                            key = store.filesBucket.new_key(key_name=self.fileID)
-                            buf = StringIO(buf)
-                            headers = self._s3EncryptionHeaders()
                             for attempt in retry_s3():
                                 with attempt:
-                                    assert buf.len == key.set_contents_from_file(fp=buf,
-                                                                                 headers=headers)
-                            self.version = key.version_id
+                                    info.version = upload.complete_upload().version_id
 
-                    thread = ExceptionalThread(target=multipartReader if multipart else reader)
-                    thread.start()
-                    yield writable
-                # The writable is now closed. This will send EOF to the readable and cause that
-                # thread to finish.
-                thread.join()
-                assert bool(self.version) == (self.content is None)
+            class SinglePartPipe(WritablePipe):
+                def readFrom(self, readable):
+                    buf = readable.read()
+                    if allowInlining and len(buf) <= info._maxInlinedSize():
+                        info.content = buf
+                    else:
+                        key = store.filesBucket.new_key(key_name=info.fileID)
+                        buf = StringIO(buf)
+                        headers = info._s3EncryptionHeaders()
+                        for attempt in retry_s3():
+                            with attempt:
+                                assert buf.len == key.set_contents_from_file(fp=buf,
+                                                                             headers=headers)
+                        info.version = key.version_id
+
+            with MultiPartPipe() if multipart else SinglePartPipe() as writable:
+                yield writable
+
+            assert bool(self.version) == (self.content is None)
 
         def copyFrom(self, srcKey):
             """
@@ -1178,10 +1176,8 @@ class AWSJobStore(AbstractJobStore):
                     else:
                         assert False
 
-                    thread = ExceptionalThread(target=writer)
-                    thread.start()
-                    yield readable
-                    thread.join()
+            with DownloadPipe() as readable:
+                yield readable
 
         def delete(self):
             store = self.outer
--- src/toil/jobStores/azureJobStore.py
+++ src/toil/jobStores/azureJobStore.py
@@ -39,9 +39,7 @@ import requests
 from bd2k.util import strict_bool, memoize
 from bd2k.util.exceptions import panic
 
-from bd2k.util.threading import ExceptionalThread
-
-from toil.jobStores.utils import retry
+from toil.jobStores.utils import retry, WritablePipe, ReadablePipe
 from toil.jobWrapper import JobWrapper
 from toil.jobStores.abstractJobStore import (AbstractJobStore,
                                              NoSuchJobException,
@@ -544,64 +542,62 @@ class AzureJobStore(AbstractJobStore):
         if encrypted:
             # There is a small overhead for encrypted data.
             maxBlockSize -= encryption.overhead
-        readable_fh, writable_fh = os.pipe()
-        with os.fdopen(readable_fh, 'r') as readable:
-            with os.fdopen(writable_fh, 'w') as writable:
-                def reader():
-                    blockIDs = []
-                    try:
-                        while True:
-                            buf = readable.read(maxBlockSize)
-                            if len(buf) == 0:
-                                # We're safe to break here even if we never read anything, since
-                                # putting an empty block list creates an empty blob.
-                                break
-                            if encrypted:
-                                buf = encryption.encrypt(buf, self.keyPath)
-                            blockID = self._newFileID()
-                            container.put_block(blob_name=jobStoreFileID,
-                                                block=buf,
-                                                blockid=blockID)
-                            blockIDs.append(blockID)
-                    except:
-                        with panic(log=logger):
-                            # This is guaranteed to delete any uncommitted blocks.
-                            container.delete_blob(blob_name=jobStoreFileID)
 
-                    if checkForModification and expectedVersion is not None:
-                        # Acquire a (60-second) write lock,
-                        leaseID = container.lease_blob(blob_name=jobStoreFileID,
-                                                       x_ms_lease_action='acquire')['x-ms-lease-id']
-                        # check for modification,
-                        blobProperties = container.get_blob_properties(blob_name=jobStoreFileID)
-                        if blobProperties['etag'] != expectedVersion:
-                            container.lease_blob(blob_name=jobStoreFileID,
-                                                 x_ms_lease_action='release',
-                                                 x_ms_lease_id=leaseID)
-                            raise ConcurrentFileModificationException(jobStoreFileID)
-                        # commit the file,
-                        container.put_block_list(blob_name=jobStoreFileID,
-                                                 block_list=blockIDs,
-                                                 x_ms_lease_id=leaseID,
-                                                 x_ms_meta_name_values=dict(
-                                                     encrypted=str(encrypted)))
-                        # then release the lock.
+        store = self
+
+        class UploadPipe(WritablePipe):
+
+            def readFrom(self, readable):
+                blockIDs = []
+                try:
+                    while True:
+                        buf = readable.read(maxBlockSize)
+                        if len(buf) == 0:
+                            # We're safe to break here even if we never read anything, since
+                            # putting an empty block list creates an empty blob.
+                            break
+                        if encrypted:
+                            buf = encryption.encrypt(buf, store.keyPath)
+                        blockID = store._newFileID()
+                        container.put_block(blob_name=jobStoreFileID,
+                                            block=buf,
+                                            blockid=blockID)
+                        blockIDs.append(blockID)
+                except:
+                    with panic(log=logger):
+                        # This is guaranteed to delete any uncommitted blocks.
+                        container.delete_blob(blob_name=jobStoreFileID)
+
+                if checkForModification and expectedVersion is not None:
+                    # Acquire a (60-second) write lock,
+                    leaseID = container.lease_blob(blob_name=jobStoreFileID,
+                                                   x_ms_lease_action='acquire')['x-ms-lease-id']
+                    # check for modification,
+                    blobProperties = container.get_blob_properties(blob_name=jobStoreFileID)
+                    if blobProperties['etag'] != expectedVersion:
                         container.lease_blob(blob_name=jobStoreFileID,
                                              x_ms_lease_action='release',
                                              x_ms_lease_id=leaseID)
-                    else:
-                        # No need to check for modification, just blindly write over whatever
-                        # was there.
-                        container.put_block_list(blob_name=jobStoreFileID,
-                                                 block_list=blockIDs,
-                                                 x_ms_meta_name_values=dict(
-                                                     encrypted=str(encrypted)))
-                thread = ExceptionalThread(target=reader)
-                thread.start()
-                yield writable
-            # The writable is now closed. This will send EOF to the readable and cause the reader
-            # thread to finish.
-            thread.join()
+                        raise ConcurrentFileModificationException(jobStoreFileID)
+                    # commit the file,
+                    container.put_block_list(blob_name=jobStoreFileID,
+                                             block_list=blockIDs,
+                                             x_ms_lease_id=leaseID,
+                                             x_ms_meta_name_values=dict(
+                                                 encrypted=str(encrypted)))
+                    # then release the lock.
+                    container.lease_blob(blob_name=jobStoreFileID,
+                                         x_ms_lease_action='release',
+                                         x_ms_lease_id=leaseID)
+                else:
+                    # No need to check for modification, just blindly write over whatever
+                    # was there.
+                    container.put_block_list(blob_name=jobStoreFileID,
+                                             block_list=blockIDs,
+                                             x_ms_meta_name_values=dict(encrypted=str(encrypted)))
+
+        with UploadPipe() as writable:
+            yield writable
 
     @contextmanager
     def _downloadStream(self, jobStoreFileID, container):
@@ -613,34 +609,23 @@ class AzureJobStore(AbstractJobStore):
         if encrypted and self.keyPath is None:
             raise AssertionError('Content is encrypted but no key was provided.')
 
-        readable_fh, writable_fh = os.pipe()
-        with os.fdopen(readable_fh, 'r') as readable:
-            with os.fdopen(writable_fh, 'w') as writable:
-                def writer():
-                    try:
-                        chunkStartPos = 0
-                        fileSize = int(blobProps['Content-Length'])
-                        while chunkStartPos < fileSize:
-                            chunkEndPos = chunkStartPos + self._maxAzureBlockBytes - 1
-                            buf = container.get_blob(blob_name=jobStoreFileID,
-                                                     x_ms_range="bytes=%d-%d" % (chunkStartPos,
-                                                                                 chunkEndPos))
-                            if encrypted:
-                                buf = encryption.decrypt(buf, self.keyPath)
-                            writable.write(buf)
-                            chunkStartPos = chunkEndPos + 1
-                    finally:
-                        # Ensure readers aren't left blocking if this thread crashes.
-                        # This close() will send EOF to the reading end and ultimately cause the
-                        # yield to return. It also makes the implict .close() done by the enclosing
-                        # "with" context redundant but that should be ok since .close() on file
-                        # objects are idempotent.
-                        writable.close()
+        outer_self = self
 
-                thread = ExceptionalThread(target=writer)
-                thread.start()
-                yield readable
-                thread.join()
+        class DownloadPipe(ReadablePipe):
+            def writeTo(self, writable):
+                chunkStart = 0
+                fileSize = int(blobProps['Content-Length'])
+                while chunkStart < fileSize:
+                    chunkEnd = chunkStart + outer_self._maxAzureBlockBytes - 1
+                    buf = container.get_blob(blob_name=jobStoreFileID,
+                                             x_ms_range="bytes=%d-%d" % (chunkStart, chunkEnd))
+                    if encrypted:
+                        buf = encryption.decrypt(buf, outer_self.keyPath)
+                    writable.write(buf)
+                    chunkStart = chunkEnd + 1
+
+        with DownloadPipe() as readable:
+            yield readable
 
 
 class AzureTable(object):
