d754e0e6e4e066278de6d8cb610b76a7ec3f753a
John Vivian <jtvivian@gmail.com>
2016-12-21 13:41:27 -0800

    Add wrapper functions for dockerCall and dockerCheckOutput
    s/dockerCall/Docker

--- src/toil/lib/docker.py
+++ src/toil/lib/docker.py
@@ -7,40 +7,89 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-def dockerCall(job,
-               tool,
-               parameters=None,
-               workDir=None,
-               dockerParameters=None,
-               outfile=None,
-               checkOutput=False,
-               defer=None):
+def dockerCall(job, tool, parameters=None, workDir=None, dockerParameters=None, outfile=None, defer=None):
     """
-        Calls Docker for a particular tool with the specified parameters. Assumes `docker` is on the PATH.
+    Wrapper call to Docker. See docstring for Docker()
 
-        Example of using docker_call in a Toil pipeline to index a FASTA file with SAMtools:
-            def toil_job(job):
-                work_dir = job.fileStore.getLocalTempDir()
-                path = job.fileStore.readGlobalFile(ref_id, os.path.join(work_dir, 'ref.fasta')
-                parameters = ['faidx', path]
-                docker_call(job, tool='quay.io/ucgc_cgl/samtools', work_dir=work_dir, parameters=parameters)
+    :param toil.Job.job job: The Job instance for the calling function.
+    :param str tool: Name of the Docker image to be used (e.g. quay.io/ucsc_cgl/samtools).
+    :param list[str] parameters: Command line arguments to be passed to the tool.
+    :param str workDir: Directory to mount into the container via `-v`. Destination convention is /data
+    :param list[str] dockerParameters: Parameters to pass to Docker. Default parameters are `--rm`,
+            `--log-driver none`, and the mountpoint `-v work_dir:/data` where /data is the destination convention.
+             These defaults are removed if docker_parmaters is passed, so be sure to pass them if they are desired.
+    :param file outfile: Pipe output of Docker call to file handle
+    :param int defer: What action should be taken on the container upon job completion?
+           docker_call.FORGO (0) will leave the container untouched.
+           docker_call.STOP (1) will attempt to stop the container with `docker stop` (useful for
+           debugging).
+           docker_call.RM (2) will stop the container and then forcefully remove it from the system
+           using `docker rm -f`.
+           The default value is None and that shadows docker_call.FORGO, unless --rm is being passed in.
+    """
+    Docker(job, tool=tool, parameters=parameters, workDir=workDir, dockerParameters=dockerParameters,
+           outfile=outfile, checkOutput=False, defer=defer)
 
-        :param toil.Job.job job: The Job instance for the calling function.
-        :param str tool: Name of the Docker image to be used (e.g. quay.io/ucsc_cgl/samtools).
-        :param list[str] parameters: Command line arguments to be passed to the tool.
-        :param str workDir: Directory to mount into the container via `-v`. Destination convention is /data
-        :param list[str] dockerParameters: Parameters to pass to Docker. Default parameters are `--rm`,
-                `--log-driver none`, and the mountpoint `-v work_dir:/data` where /data is the destination convention.
-                 These defaults are removed if docker_parmaters is passed, so be sure to pass them if they are desired.
-        :param file outfile: Pipe output of Docker call to file handle
-        :param bool checkOutput: When True, this function returns docker's output.
-        :param int defer: What action should be taken on the container upon job completion?
-               docker_call.FORGO (0) will leave the container untouched.
-               docker_call.STOP (1) will attempt to stop the container with `docker stop` (useful for
-               debugging).
-               docker_call.RM (2) will stop the container and then forcefully remove it from the system
-               using `docker rm -f`.
-               The default value is None and that shadows docker_call.FORGO, unless --rm is being passed in.
+
+def dockerCheckOutput(job, tool, parameters=None, workDir=None, dockerParameters=None, defer=None):
+    """
+    Wrapper call to Docker. See docstring for Docker()
+
+    :param toil.Job.job job: The Job instance for the calling function.
+    :param str tool: Name of the Docker image to be used (e.g. quay.io/ucsc_cgl/samtools).
+    :param list[str] parameters: Command line arguments to be passed to the tool.
+    :param str workDir: Directory to mount into the container via `-v`. Destination convention is /data
+    :param list[str] dockerParameters: Parameters to pass to Docker. Default parameters are `--rm`,
+            `--log-driver none`, and the mountpoint `-v work_dir:/data` where /data is the destination convention.
+             These defaults are removed if docker_parmaters is passed, so be sure to pass them if they are desired.
+    :param int defer: What action should be taken on the container upon job completion?
+           docker_call.FORGO (0) will leave the container untouched.
+           docker_call.STOP (1) will attempt to stop the container with `docker stop` (useful for
+           debugging).
+           docker_call.RM (2) will stop the container and then forcefully remove it from the system
+           using `docker rm -f`.
+           The default value is None and that shadows docker_call.FORGO, unless --rm is being passed in.
+    :returns: Stdout from the docker call
+    :rtype: str
+    """
+    return Docker(job, tool=tool, parameters=parameters, workDir=workDir,
+                  dockerParameters=dockerParameters, checkOutput=True, defer=defer)
+
+
+def Docker(job,
+           tool,
+           parameters=None,
+           workDir=None,
+           dockerParameters=None,
+           outfile=None,
+           checkOutput=False,
+           defer=None):
+    """
+    Calls Docker for a particular tool with the specified parameters. Assumes `docker` is on the PATH.
+
+    Example of using docker_call in a Toil pipeline to index a FASTA file with SAMtools:
+        def toil_job(job):
+            work_dir = job.fileStore.getLocalTempDir()
+            path = job.fileStore.readGlobalFile(ref_id, os.path.join(work_dir, 'ref.fasta')
+            parameters = ['faidx', path]
+            docker_call(job, tool='quay.io/ucgc_cgl/samtools', work_dir=work_dir, parameters=parameters)
+
+    :param toil.Job.job job: The Job instance for the calling function.
+    :param str tool: Name of the Docker image to be used (e.g. quay.io/ucsc_cgl/samtools).
+    :param list[str] parameters: Command line arguments to be passed to the tool.
+    :param str workDir: Directory to mount into the container via `-v`. Destination convention is /data
+    :param list[str] dockerParameters: Parameters to pass to Docker. Default parameters are `--rm`,
+            `--log-driver none`, and the mountpoint `-v work_dir:/data` where /data is the destination convention.
+             These defaults are removed if docker_parmaters is passed, so be sure to pass them if they are desired.
+    :param file outfile: Pipe output of Docker call to file handle
+    :param bool checkOutput: When True, this function returns docker's output.
+    :param int defer: What action should be taken on the container upon job completion?
+           docker_call.FORGO (0) will leave the container untouched.
+           docker_call.STOP (1) will attempt to stop the container with `docker stop` (useful for
+           debugging).
+           docker_call.RM (2) will stop the container and then forcefully remove it from the system
+           using `docker rm -f`.
+           The default value is None and that shadows docker_call.FORGO, unless --rm is being passed in.
     """
     if parameters is None:
         parameters = []
@@ -56,7 +105,7 @@ def dockerCall(job,
                              os.path.abspath(workDir) + ':/data']
 
     # Ensure the user has passed a valid value for defer
-    require(defer in (None, dockerCall.FORGO, dockerCall.STOP, dockerCall.RM),
+    require(defer in (None, Docker.FORGO, Docker.STOP, Docker.RM),
             'Please provide a valid value for defer.')
 
     # Get container name which is needed for _dockerKill
@@ -73,8 +122,8 @@ def dockerCall(job,
 
     # Defer the container on-exit action
     if '--rm' in baseDockerCall and defer is None:
-        defer = dockerCall.RM
-    if '--rm' in baseDockerCall and defer is not dockerCall.RM:
+        defer = Docker.RM
+    if '--rm' in baseDockerCall and defer is not Docker.RM:
         _logger.warn('--rm being passed to docker call but defer not set to dockerCall.RM, defer set to: ' + str(defer))
     job.defer(_dockerKill, containerName, action=defer)
     # Defer the permission fixing function which will run after this job concludes.
@@ -95,9 +144,9 @@ def dockerCall(job,
             subprocess.check_call(call)
 
 
-dockerCall.FORGO = 0
-dockerCall.STOP = 1
-dockerCall.RM = 2
+Docker.FORGO = 0
+Docker.STOP = 1
+Docker.RM = 2
 
 
 def _dockerKill(containerName, action):
@@ -115,7 +164,7 @@ def _dockerKill(containerName, action):
         _logger.info('The container with name "%s" appears to have already been removed.  Nothing to '
                     'do.', containerName)
     else:
-        if action in (None, dockerCall.FORGO):
+        if action in (None, Docker.FORGO):
             _logger.info('The container with name %s continues to exist as we were asked to forgo a '
                         'post-job action on it.', containerName)
         else:
@@ -126,7 +175,7 @@ def _dockerKill(containerName, action):
                 subprocess.check_call(['docker', 'stop', containerName])
             else:
                 _logger.info('The container "%s" was not found to be running.', containerName)
-            if action == dockerCall.RM:
+            if action == Docker.RM:
                 # If the container was run with --rm, then stop will most likely remove the
                 # container.  We first check if it is running then remove it.
                 running = _containerIsRunning(containerName)
