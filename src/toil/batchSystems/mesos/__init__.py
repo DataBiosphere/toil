aa6148c2bf2d61b5a04a2d252dd27c5550b6b4fd
Christopher 'CJ' Ketchum <cketchum@ucsc.edu>
2017-03-13 09:45:46 -0700

    Improve efficiency of Mesos batch system (resolves #1531)

--- src/toil/batchSystems/mesos/__init__.py
+++ src/toil/batchSystems/mesos/__init__.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 from __future__ import absolute_import
 from collections import namedtuple
+from functools import total_ordering
 
 TaskData = namedtuple('TaskData', (
     # Time when the task was started
@@ -27,20 +28,105 @@ TaskData = namedtuple('TaskData', (
     'cores'))
 
 
-class ResourceRequirement( namedtuple('_ResourceRequirement', (
-    # Number of bytes (!) needed for a task
-    'memory',
-    # Number of CPU cores needed for a task
-    'cores',
-    # Number of bytes (!) needed for task on disk
-    'disk',
-    # True, if job can be run on a preemptable node, False otherwise
-    'preemptable'))):
+class JobQueue(object):
+
+    def __init__(self):
+        # mapping of jobTypes to queues of jobs of that type
+        self.queues = {}
+        # list of jobTypes in decreasing resource expense
+        self.sortedTypes = []
+
+    def insertJob(self, job, jobType):
+        # logn insertion
+        if jobType not in self.queues:
+            index = bisect(self.sortedTypes, jobType)
+            self.sortedTypes.index(index, jobType)
+            self.queues[jobType] = [job]
+        else:
+            self.queues[jobType].append(job)
+
+    def sorted(self):
+        return self.sortedTypes
+
+    def jobIDs(self):
+        # O(N)
+        return [ids for idList in self.queues.values() for ids in idList]
+
+    def nextJobOfType(self, jobType):
+        # constant lookup, pop off first element
+        # also remove from taskMap
+        job = self.queues[jobType].pop(0)
+        return job
+
+    def jobsOfType(self, jobType):
+        return self.queues.get(jobType, [])
+
+
+@total_ordering
+class ResourceRequirement(object):
+    def __init__(self, memory, cores, disk, preemptable):
+        # Number of bytes (!) needed for a task
+        self.memory = memory
+        # Number of CPU cores needed for a task
+        self.cores = cores
+        # Number of bytes (!) needed for task on disk
+        self.disk = disk
+        # True, if job can be run on a preemptable node, False otherwise
+        self.preemptable = preemptable
+
     def size(self):
         """
         The scalar size of an offer. Can be used to compare offers.
         """
         return self.cores
+
+
+    def __gt__(self, other):
+        """
+        Returns True if self is greater than other, else returns False.
+        Note that we take an unintuitive definition of "greater than" since
+        we want the ResoureRequirement with the largest requirements to be considered "lesser".
+        This is mainly because we want our jobTypes to be sorted in decreasing order, giving expensive jobs
+        priority.
+
+        :param other:
+        :return:
+        """
+        if not self.preemptable and other.preemptable:
+            # The dominant criteria is preemptability of jobs. Non-preemptable (NP) jobs should be
+            # considered first because they can only be run on on NP nodes while P jobs can run on
+            # both. Without this prioritization of NP jobs, P jobs could steal NP cores from NP jobs,
+            # leaving subsequently offered P cores unused. Despite the prioritization of NP jobs,
+            # NP jobs can not steal P cores from P jobs, simply because the offer-acceptance logic
+            # would not accept a P offer with a NP job.
+            return False
+        elif self.preemptable and not other.preemptable:
+            return True
+
+        if self.cores > other.cores:
+            return False
+        elif self.cores < other.cores:
+            return True
+
+        if self.memory > other.memory:
+            return False
+        elif self.memory < other.memory:
+            return True
+
+        if self.disk > other.disk:
+            return False
+        elif self.disk < other.disk:
+            return True
+
+        return False
+
+    def __eq__(self, other):
+        if (self.preemptable == other.preemptable and
+           self.cores == other.cores and
+           self.memory == other.memory and
+           self.disk == other.disk):
+            return True
+        return False
 
 
 ToilJob = namedtuple('ToilJob', (
