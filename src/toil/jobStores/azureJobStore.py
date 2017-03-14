bfaadf4fa83935174dde7662a783256f0e5338ef
Christopher 'CJ' Ketchum <cketchum@ucsc.edu>
2017-03-14 13:32:55 -0700

    Retry with Azure importFile (resolves #1564)

--- src/toil/jobStores/azureJobStore.py
+++ src/toil/jobStores/azureJobStore.py
@@ -278,9 +278,11 @@ class AzureJobStore(AbstractJobStore):
     @classmethod
     def _readFromUrl(cls, url, writable):
         blob = cls._parseWasbUrl(url)
-        blob.service.get_blob_to_file(container_name=blob.container,
-                                      blob_name=blob.name,
-                                      stream=writable)
+        for attempt in retry_azure():
+            with attempt:
+                blob.service.get_blob_to_file(container_name=blob.container,
+                                              blob_name=blob.name,
+                                              stream=writable)
 
     @classmethod
     def _writeToUrl(cls, readable, url):
