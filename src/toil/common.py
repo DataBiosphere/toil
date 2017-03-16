d3824709f3c83a6c112b42d3a1e01e42ead660eb
Andrey Tovchigrechko <tovchigrechkoa@medimmune.com>
2017-03-16 12:21:10 -0400

    Fix race condition when workDir is on a shared file system. Before, a worker in a call to fileStore.findAndHandleDeadJobs() would see jobs from workers from other nodes (hosts), would not find their PIDs among the live jobs on its own node, and delete their job directories as presumed dead. getNodeID is a method to get the best possible version of unique host ID

--- src/toil/common.py
+++ src/toil/common.py
@@ -568,6 +568,62 @@ def addOptions(parser, config=Config()):
                            "argparse.ArgumentParser" % parser.__class__)
 
 
+def getNodeID(extraIDFiles=[]):
+    """Return unique ID of the current node (host).
+    Tries several methods until success.
+    The returned ID should be identical across calls from
+    different processes on the same node at least until 
+    the next OS reboot.
+
+    The last resort method is uuid.getnode() that in some
+    rare OS configurations may return a random ID each time it is called.
+    However, this method should never be reached on a Linux system,
+    because reading from /proc/sys/kernel/random/boot_id will be tried prior
+    to that. If uuid.getnode() is reached, it will be called twice,
+    and exception raised if the values are not identical.
+
+    :param list extraIDFiles: Optional list of additional file names
+    to try reading node ID before trying default methods. ID
+    should be a single word (no spaces) on the first line of the file.
+
+    """
+    import os
+    idSourceFiles = extraIDFiles + ["/var/lib/dbus/machine-id",
+        "/proc/sys/kernel/random/boot_id"]
+    for idSourceFile in idSourceFiles:
+        if os.path.exists(idSourceFile):
+            try:
+                with open(idSourceFile,"r") as inp:
+                    nodeID = inp.readline().strip()
+            except EnvironmentError:
+                logger.warning(("Exception when trying to read ID file {}. "+\
+                        "Will try next method to get node ID").\
+                        format(idSourceFile), exc_info=True)
+            else:
+                if len(nodeID.split()) == 1:
+                    logger.debug("Obtained node ID {} from file {}".\
+                            format(nodeID,idSourceFile))
+                    break
+                else:
+                    logger.warning(("Node ID {} from file {} contains spaces."+\
+                            " Will try next method to get node ID").\
+                            format(nodeID,idSourceFile))
+    else:
+        import uuid
+        nodeIDs = []
+        for i_call in range(2):
+            nodeID = str(uuid.getnode()).strip()
+            assert len(nodeID.split())==1,("Node ID {} from uuid.getnode() "+\
+                    "contains spaces").format(nodeID)
+            nodeIDs.append(nodeID)
+        assert nodeIDs[0] == nodeIDs[1], ("Different node IDs {} received from"+\
+                " repeated calls to uuid.getnode(). You should use another"+\
+                " method to generate node ID.").format(nodeIDs)
+        nodeID = nodeIDs[0]
+        logger.debug("Obtained node ID {} from uuid.getnode()".\
+                format(nodeID))
+    return nodeID
+
 class Toil(object):
     """
     A context manager that represents a Toil workflow, specifically the batch system, job store,
@@ -934,8 +990,10 @@ class Toil(object):
         if not os.path.exists(workDir):
             raise RuntimeError("The directory specified by --workDir or TOIL_WORKDIR (%s) does not "
                                "exist." % workDir)
-        # Create the workflow dir
-        workflowDir = os.path.join(workDir, 'toil-%s' % workflowID)
+        # Create the workflow dir, make it unique to each host in case workDir is 
+        # on a shared FS.
+        # This prevents workers on different nodes from erasing each other's directories.
+        workflowDir = os.path.join(workDir, 'toil-%s-%s' % (workflowID, getNodeID()))
         try:
             # Directory creation is atomic
             os.mkdir(workflowDir)
@@ -981,7 +1039,6 @@ class Toil(object):
     def _assertContextManagerUsed(self):
         if not self._inContextManager:
             raise ToilContextManagerException()
-
 
 class ToilRestartException(Exception):
     def __init__(self, message):
