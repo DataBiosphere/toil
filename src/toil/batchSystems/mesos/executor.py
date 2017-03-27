7b948dbdc6737ee869f0190928f1c82fb143763c
Christopher 'CJ' Ketchum <cketchum@ucsc.edu>
2017-03-27 15:04:27 -0700

    Ensure unpickling errors in executor are propagated to leader

--- src/toil/batchSystems/mesos/executor.py
+++ src/toil/batchSystems/mesos/executor.py
@@ -127,8 +127,17 @@ class MesosExecutor(mesos.interface.Executor):
         def runTask():
             log.debug("Running task %s", task.task_id.value)
             sendUpdate(mesos_pb2.TASK_RUNNING)
+            try:
+                taskData = pickle.loads(task.data)
+            except:
+                exc_info = sys.exc_info()
+                log.error('Exception while unpickling task:', exc_info=exc_info)
+                exc_type, exc_value, exc_trace = exc_info
+                sendUpdate(mesos_pb2.TASK_FAILED, wallTime=None,
+                           message=''.join(traceback.format_exception_only(exc_type, exc_value)))
+                return
+
             # This is where task.data is first invoked. Using this position to setup cleanupInfo
-            taskData = pickle.loads(task.data)
             if self.workerCleanupInfo is not None:
                 assert self.workerCleanupInfo == taskData.workerCleanupInfo
             else:
