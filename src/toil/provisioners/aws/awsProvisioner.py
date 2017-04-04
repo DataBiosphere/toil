a3c1bf2a9bc60dc77353f295914a7ce7ce2a8532
Joel Armstrong <jcarmstr@ucsc.edu>
2017-04-04 14:07:19 -0700

    Respect AZ config setting (when not using spot instances)
    Fixes #1609.

--- src/toil/provisioners/aws/awsProvisioner.py
+++ src/toil/provisioners/aws/awsProvisioner.py
@@ -386,7 +386,8 @@ class AWSProvisioner(AbstractProvisioner):
         kwargs = {'key_name': keyName, 'security_group_ids': [sg.id for sg in sgs],
                   'instance_type': instanceType,
                   'user_data': userData, 'block_device_map': bdm,
-                  'instance_profile_arn': profileARN}
+                  'instance_profile_arn': profileARN,
+                  'placement': zone}
         if vpcSubnet:
             kwargs["subnet_id"] = vpcSubnet
         if not spotBid:
@@ -541,7 +542,8 @@ class AWSProvisioner(AbstractProvisioner):
                   'instance_type': self.instanceType.name,
                   'user_data': userData,
                   'block_device_map': bdm,
-                  'instance_profile_arn': arn}
+                  'instance_profile_arn': arn,
+                  'placement': getCurrentAWSZone()}
         kwargs["subnet_id"] = self._getClusterInstance(self.instanceMetaData).subnet_id
 
         instancesLaunched = []
