5aabe17e93acaf624e8be0e8213706041f16d1a1
Natan Lao <natanlao@users.noreply.github.com>
2017-06-08 12:46:06 -0700

    Fixed logging being initialized twice (fixes #1591)

--- src/toil/utils/toilSSHCluster.py
+++ src/toil/utils/toilSSHCluster.py
@@ -17,10 +17,10 @@ SSHs into the toil appliance container running on the leader of the cluster
 import argparse
 import logging
 from toil.provisioners import Cluster
-from toil.lib.bioio import parseBasicOptions, setLoggingFromOptions, getBasicOptionParser
+from toil.lib.bioio import parseBasicOptions, getBasicOptionParser
 from toil.utils import addBasicProvisionerOptions
 
-logger = logging.getLogger( __name__ )
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -28,7 +28,6 @@ def main():
     parser = addBasicProvisionerOptions(parser)
     parser.add_argument('args', nargs=argparse.REMAINDER)
     config = parseBasicOptions(parser)
-    setLoggingFromOptions(config)
     cluster = Cluster(provisioner=config.provisioner,
                       clusterName=config.clusterName, zone=config.zone)
     cluster.sshCluster(args=config.args)
