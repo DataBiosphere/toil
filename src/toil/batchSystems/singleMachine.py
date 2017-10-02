231b6665bde956aae5b3469a7147e2358097fca4
Michael R. Crusoe <mr-c@users.noreply.github.com>
2017-10-02 16:48:14 +0300

    fix forking logic
    Thank you @ejacox for the catch!

--- src/toil/batchSystems/singleMachine.py
+++ src/toil/batchSystems/singleMachine.py
@@ -143,7 +143,7 @@ class SingleMachineBatchSystem(BatchSystemSupport):
         startTime = time.time()  # Time job is started
         popen = None
         statusCode = None
-        forkWorker = not (self.debugWorker and "_toil_worker" not in jobCommand)
+        forkWorker = not (self.debugWorker and "_toil_worker" in jobCommand)
         if forkWorker:
             with self.popenLock:
                 popen = subprocess.Popen(jobCommand,
