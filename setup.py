ab077d26c51c3e6db6e82bd256b14563373587fe
Jesse Brennan <brennan@ucsc.edu>
2017-11-07 12:10:55 -0800

    Resolve requests version conflict

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def runSetup():
             'dill==0.2.5',
             'six>=1.10.0',
             'future',
-            'requests',
+            'requests==2.18.4',
             'docker==2.5.1'],
         extras_require={
             'mesos': [
