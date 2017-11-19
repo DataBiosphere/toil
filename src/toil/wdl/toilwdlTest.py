8cb41b9cc2949e1ed3db24cbb10e189069f42b92
DailyDreaming <lblauvel@ucsc.edu>
2017-11-18 17:49:25 -0800

    Modified file paths.

--- src/toil/wdl/toilwdlTest.py
+++ src/toil/wdl/toilwdlTest.py
@@ -15,9 +15,9 @@ class TestCase(unittest.TestCase):
         Initial set up of variables for the test.
         """
 
-        self.program = 'toilwdl.py'
-        self.tutorial_test_output_dir = 'toil_outputs'
-        self.output_file = "toilwdl_compiled.py"
+        self.program = 'src/toil/wdl/toilwdl.py'
+        self.tutorial_test_output_dir = 'src/toil/wdl/toil_outputs'
+        self.output_file = "src/toil/wdl/toilwdl_compiled.py"
 
         if not os.path.exists(self.tutorial_test_output_dir):
             try:
@@ -26,14 +26,14 @@ class TestCase(unittest.TestCase):
                 raise OSError(
                     'Could not create directory.  Insufficient permissions or disk space most likely.')
 
-        with zipfile.ZipFile('ENCODE_data.zip', 'r') as zip_ref:
-            zip_ref.extractall('.')
-        with zipfile.ZipFile('GATK_data.zip', 'r') as zip_ref:
-            zip_ref.extractall('.')
-        with zipfile.ZipFile('toil_templates.zip', 'r') as zip_ref:
-            zip_ref.extractall('.')
-        with zipfile.ZipFile('wdl_templates.zip', 'r') as zip_ref:
-            zip_ref.extractall('.')
+        with zipfile.ZipFile('src/toil/wdl/ENCODE_data.zip', 'r') as zip_ref:
+            zip_ref.extractall('src/toil/wdl/')
+        with zipfile.ZipFile('src/toil/wdl/GATK_data.zip', 'r') as zip_ref:
+            zip_ref.extractall('src/toil/wdl/')
+        with zipfile.ZipFile('src/toil/wdl/toil_templates.zip', 'r') as zip_ref:
+            zip_ref.extractall('src/toil/wdl/')
+        with zipfile.ZipFile('src/toil/wdl/wdl_templates.zip', 'r') as zip_ref:
+            zip_ref.extractall('src/toil/wdl/')
 
     def tearDown(self):
         """Default tearDown for unittest."""
@@ -44,9 +44,9 @@ class TestCase(unittest.TestCase):
     def testTut01(self):
         '''Test if toilwdl produces the same outputs as known good outputs for WDL's
         GATK tutorial #1.'''
-        wdl = "wdl_templates/t01/helloHaplotypeCaller.wdl"
-        json = "wdl_templates/t01/helloHaplotypeCaller_inputs.json"
-        tutorial_good_output_dir = 'wdl_templates/t01/output/'
+        wdl = "src/toil/wdl/wdl_templates/t01/helloHaplotypeCaller.wdl"
+        json = "src/toil/wdl/wdl_templates/t01/helloHaplotypeCaller_inputs.json"
+        tutorial_good_output_dir = 'src/toil/wdl/wdl_templates/t01/output/'
 
         subprocess.check_call(['python', self.program, wdl, json, '-o', self.tutorial_test_output_dir])
         subprocess.check_call(['python', self.output_file])
@@ -79,9 +79,9 @@ class TestCase(unittest.TestCase):
     def testTut02(self):
         '''Test if toilwdl produces the same outputs as known good outputs for WDL's
         GATK tutorial #2.'''
-        wdl = "wdl_templates/t02/simpleVariantSelection.wdl"
-        json = "wdl_templates/t02/simpleVariantSelection_inputs.json"
-        tutorial_good_output_dir = 'wdl_templates/t02/output/'
+        wdl = "src/toil/wdl/wdl_templates/t02/simpleVariantSelection.wdl"
+        json = "src/toil/wdl/wdl_templates/t02/simpleVariantSelection_inputs.json"
+        tutorial_good_output_dir = 'src/toil/wdl/wdl_templates/t02/output/'
 
         subprocess.check_call(['python', self.program, wdl, json, '-o', self.tutorial_test_output_dir])
         subprocess.check_call(['python', self.output_file])
@@ -114,9 +114,9 @@ class TestCase(unittest.TestCase):
     def testTut03(self):
         '''Test if toilwdl produces the same outputs as known good outputs for WDL's
         GATK tutorial #3.'''
-        wdl = "wdl_templates/t03/simpleVariantDiscovery.wdl"
-        json = "wdl_templates/t03/simpleVariantDiscovery_inputs.json"
-        tutorial_good_output_dir = 'wdl_templates/t03/output/'
+        wdl = "src/toil/wdl/wdl_templates/t03/simpleVariantDiscovery.wdl"
+        json = "src/toil/wdl/wdl_templates/t03/simpleVariantDiscovery_inputs.json"
+        tutorial_good_output_dir = 'src/toil/wdl/wdl_templates/t03/output/'
 
         subprocess.check_call(['python', self.program, wdl, json, '-o', self.tutorial_test_output_dir])
         subprocess.check_call(['python', self.output_file])
@@ -149,9 +149,9 @@ class TestCase(unittest.TestCase):
     def testTut04(self):
         '''Test if toilwdl produces the same outputs as known good outputs for WDL's
         GATK tutorial #4.'''
-        wdl = "wdl_templates/t04/jointCallingGenotypes.wdl"
-        json = "wdl_templates/t04/jointCallingGenotypes_inputs.json"
-        tutorial_good_output_dir = 'wdl_templates/t04/output/'
+        wdl = "src/toil/wdl/wdl_templates/t04/jointCallingGenotypes.wdl"
+        json = "src/toil/wdl/wdl_templates/t04/jointCallingGenotypes_inputs.json"
+        tutorial_good_output_dir = 'src/toil/wdl/wdl_templates/t04/output/'
 
         subprocess.check_call(['python', self.program, wdl, json, '-o', self.tutorial_test_output_dir])
         subprocess.check_call(['python', self.output_file])
@@ -184,9 +184,9 @@ class TestCase(unittest.TestCase):
     def testENCODE(self):
         '''Test if toilwdl produces the same outputs as known good outputs for
         a short ENCODE run.'''
-        wdl = "wdl_templates/testENCODE/encode_mapping_workflow.wdl"
-        json = "wdl_templates/testENCODE/encode_mapping_workflow.wdl.json"
-        tutorial_good_output_dir = 'wdl_templates/testENCODE/output/'
+        wdl = "src/toil/wdl/wdl_templates/testENCODE/encode_mapping_workflow.wdl"
+        json = "src/toil/wdl/wdl_templates/testENCODE/encode_mapping_workflow.wdl.json"
+        tutorial_good_output_dir = 'src/toil/wdl/wdl_templates/testENCODE/output/'
 
         subprocess.check_call(['python', self.program, wdl, json, '-o', self.tutorial_test_output_dir])
         subprocess.check_call(['python', self.output_file])
@@ -217,9 +217,9 @@ class TestCase(unittest.TestCase):
     # estimated run time 2 sec
     def testPipe(self):
         '''Test basic bash input functionality with a pipe.'''
-        wdl = "wdl_templates/testPipe/call.wdl"
-        json = "wdl_templates/testPipe/call.json"
-        tutorial_good_output_dir = 'wdl_templates/testPipe/output/'
+        wdl = "src/toil/wdl/wdl_templates/testPipe/call.wdl"
+        json = "src/toil/wdl/wdl_templates/testPipe/call.json"
+        tutorial_good_output_dir = 'src/toil/wdl/wdl_templates/testPipe/output/'
 
         subprocess.check_call(['python', self.program, wdl, json, '-o', self.tutorial_test_output_dir])
         subprocess.check_call(['python', self.output_file])
@@ -250,19 +250,19 @@ class TestCase(unittest.TestCase):
     # estimated run time <1 sec
     def testCSV(self):
         default_csv_output = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
-        t = ToilWDL("wdl_templates/t01/helloHaplotypeCaller.wdl",
-                    "wdl_templates/t01/helloHaplotypeCaller_inputs.json",
-                    "toil_outputs")
-        csv_array = t.create_csv_array('test.csv')
+        t = ToilWDL("src/toil/wdl/wdl_templates/t01/helloHaplotypeCaller.wdl",
+                    "src/toil/wdl/wdl_templates/t01/helloHaplotypeCaller_inputs.json",
+                    "src/toil/wdl/toil_outputs")
+        csv_array = t.create_csv_array('src/toil/wdl/test.csv')
         assert csv_array == default_csv_output
 
     # estimated run time <1 sec
     def testTSV(self):
         default_tsv_output = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
-        t = ToilWDL("wdl_templates/t01/helloHaplotypeCaller.wdl",
-                    "wdl_templates/t01/helloHaplotypeCaller_inputs.json",
-                    "toil_outputs")
-        tsv_array = t.create_tsv_array('test.tsv')
+        t = ToilWDL("src/toil/wdl/wdl_templates/t01/helloHaplotypeCaller.wdl",
+                    "src/toil/wdl/wdl_templates/t01/helloHaplotypeCaller_inputs.json",
+                    "src/toil/wdl/toil_outputs")
+        tsv_array = t.create_tsv_array('src/toil/wdl/test.tsv')
         assert tsv_array == default_tsv_output
 
     # estimated run time <1 sec
@@ -274,10 +274,10 @@ class TestCase(unittest.TestCase):
                                     u'GATK': u'GATK_data/GenomeAnalysisTK.jar',
                                     u'RefDict': u'GATK_data/ref/human_g1k_b37_20.dict',
                                     u'RefFasta': u'GATK_data/ref/human_g1k_b37_20.fasta'}
-        t = ToilWDL("wdl_templates/t01/helloHaplotypeCaller.wdl",
-                    "wdl_templates/t01/helloHaplotypeCaller_inputs.json",
-                    "toil_outputs")
-        json_dict = t.dict_from_JSON("wdl_templates/t01/helloHaplotypeCaller_inputs.json")
+        t = ToilWDL("src/toil/wdl/wdl_templates/t01/helloHaplotypeCaller.wdl",
+                    "src/toil/wdl/wdl_templates/t01/helloHaplotypeCaller_inputs.json",
+                    "src/toil/wdl/toil_outputs")
+        json_dict = t.dict_from_JSON("src/toil/wdl/wdl_templates/t01/helloHaplotypeCaller_inputs.json")
         assert json_dict == default_json_dict_output
 
 if __name__ == "__main__":
