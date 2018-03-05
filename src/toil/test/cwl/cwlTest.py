bec22894060a4fc045efa404862022d163a2a90e
DailyDreaming <lblauvel@ucsc.edu>
2018-03-05 12:37:15 -0800

    Update cwlTest.py

--- src/toil/test/cwl/cwlTest.py
+++ src/toil/test/cwl/cwlTest.py
@@ -137,8 +137,7 @@ class CWLTest(ToilTest):
             shutil.move("common-workflow-language-%s" % testhash, cwlSpec)
             os.remove("spec.zip")
         try:
-            cmd = ["bash", "run_test.sh", "RUNNER=toil-cwl-runner",
-                   "DRAFT=v1.0", "-j4"]
+            cmd = ["bash", "run_test.sh", "RUNNER=toil-cwl-runner", "DRAFT=v1.0", "-j", "4"]
             if batchSystem:
                 cmd.extend(["--batchSystem", batchSystem])
             subprocess.check_output(cmd, cwd=cwlSpec, stderr=subprocess.STDOUT)
