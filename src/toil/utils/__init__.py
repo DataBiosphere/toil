f6097c0afed0a4b58b522a3021e64835a679dbd4
Natan Lao <natanlao@users.noreply.github.com>
2018-04-26 13:18:07 -0700

    Require zone arg if no default set (fixes #2191)

--- src/toil/utils/__init__.py
+++ src/toil/utils/__init__.py
@@ -17,7 +17,7 @@ def addBasicProvisionerOptions(parser):
     except ImportError:
         currentZone = None
     zoneString = currentZone if currentZone else 'No zone could be determined'
-    parser.add_argument('-z', '--zone', dest='zone', required=False, default=currentZone,
+    parser.add_argument('-z', '--zone', dest='zone', required=not currentZone, default=currentZone,
                         help="The AWS availability zone of the master. This parameter can also be "
                              "set via the TOIL_AWS_ZONE environment variable, or by the ec2_region_name "
                              "parameter in your .boto file, or derived from the instance metadata if "
