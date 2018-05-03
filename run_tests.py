84ca2ea7714855ea4bc60d6a728c190256741f0e
ejacox <jacox_edwin@hotmail.com>
2018-05-03 10:03:54 -0700

    fixed single test

--- run_tests.py
+++ run_tests.py
@@ -68,7 +68,7 @@ def run_parallel_to_xml(suite, args):
     :param args: auxiliary arguments to pass to pyTest
     :return:  exit status (number of failures)
     """
-    suite = [] #test_suites[suite]
+    suite = [None] #test_suites[suite]
     for name in glob.glob('test-report-*.xml'):
         os.unlink(name)
     num_failures = 0
