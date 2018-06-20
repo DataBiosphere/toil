de0a33c9494bfdcf52b05b7e8164d16ae4853d97
Adam Novak <anovak@soe.ucsc.edu>
2018-06-20 16:01:29 -0700

    Use native Python 2 str because Azure can't understand newstr

--- src/toil/jobStores/azureJobStore.py
+++ src/toil/jobStores/azureJobStore.py
@@ -16,7 +16,6 @@ from __future__ import absolute_import
 
 from future import standard_library
 standard_library.install_aliases()
-from builtins import str
 from builtins import range
 from builtins import object
 import bz2
