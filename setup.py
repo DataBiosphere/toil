22d0acf2b1f924ffa57174572c37fb0e73f65317
Ben Van de Brooke <bencvdb93@gmail.com>
2018-07-12 17:50:32 -0700

    boto 2.38.0 to 2.48.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def runSetup():
     Calls setup(). This function exists so the setup() invocation preceded more internal
     functionality. The `version` module is imported dynamically by importVersion() below.
     """
-    boto = 'boto==2.38.0'
+    boto = 'boto==2.48.0'  # Success 'boto==2.38.0'
     boto3 = 'boto3>=1.4.7, <2.0'
     futures = 'futures==3.1.1'
     pycryptodome = 'pycryptodome==3.5.1'
