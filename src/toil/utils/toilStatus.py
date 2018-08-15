096e89e5b8776df03655910523daabbbd7a722a3
Ben Van de Brooke <bencvdb93@gmail.com>
2018-08-15 10:27:09 -0700

    Replace moduel level docstring

--- src/toil/utils/toilStatus.py
+++ src/toil/utils/toilStatus.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Tool for reporting on job status.
+"""
+
 # python 2/3 compatibility imports
 from __future__ import absolute_import
 from __future__ import print_function
