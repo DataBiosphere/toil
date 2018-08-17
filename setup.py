c605a3d87ce0dcd683c183663cd98aee08b34ded
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-08-17 10:25:38 -0700

    CWL:fix support for user space overrides

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def runSetup():
     gcs = 'google-cloud-storage==1.6.0'
     gcs_oauth2_boto_plugin = 'gcs_oauth2_boto_plugin==1.14'
     apacheLibcloud = 'apache-libcloud==2.2.1'
-    cwltool = 'cwltool==1.0.20180518123035'
+    cwltool = 'cwltool==1.0.20180809224403'
     schemaSalad = 'schema-salad>=2.6, <3'
     galaxyLib = 'galaxy-lib==17.9.3'
     htcondor = 'htcondor>=8.6.0'
