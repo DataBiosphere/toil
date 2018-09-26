09c60da8a1f87a78885f75c63272c17515c37340
Lon Blauvelt <lblauvel@ucsc.edu>
2018-09-26 15:36:28 -0700

    Filter AWS images by name.

--- src/toil/provisioners/aws/awsProvisioner.py
+++ src/toil/provisioners/aws/awsProvisioner.py
@@ -332,7 +332,7 @@ class AWSProvisioner(AbstractProvisioner):
     @memoize
     def _discoverAMI(self):
         def descriptionMatches(ami):
-            return ami.description is not None and 'stable 1800.7.0' in ami.description
+            return ami.description is not None and 'stable 1688.5.3' in ami.description
         coreOSAMI = os.environ.get('TOIL_AWS_AMI')
         if coreOSAMI is not None:
             return coreOSAMI
@@ -341,7 +341,7 @@ class AWSProvisioner(AbstractProvisioner):
             # SSLError is thrown when get_all_images times out
             with attempt:
                 # 679593333241 is the aws marketplace account
-                amis = self._ctx.ec2.get_all_images(owners=['679593333241'])
+                amis = self._ctx.ec2.get_all_images(owners=['679593333241'], filters={'name': 'CoreOS-stable-1688.5.3-hvm-0d1e0bd0-eaea-4397-9a3a-c56f861d2a14-ami-9e2685e3.4'})
 
         coreOSAMI = [ami for ami in amis if descriptionMatches(ami)]
         logger.debug('Found the following matching AMIs: %s', coreOSAMI)
