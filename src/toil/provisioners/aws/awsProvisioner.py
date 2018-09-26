81f1baa967a4fe082ec78ffecc9580861fd60acb
Lon Blauvelt <lblauvel@ucsc.edu>
2018-09-26 13:20:39 -0700

    Try version 1800.5.0.

--- src/toil/provisioners/aws/awsProvisioner.py
+++ src/toil/provisioners/aws/awsProvisioner.py
@@ -332,7 +332,7 @@ class AWSProvisioner(AbstractProvisioner):
     @memoize
     def _discoverAMI(self):
         def descriptionMatches(ami):
-            return ami.description is not None and 'stable 1745.7.0' in ami.description
+            return ami.description is not None and 'stable 1800.5.0' in ami.description
         coreOSAMI = os.environ.get('TOIL_AWS_AMI')
         if coreOSAMI is not None:
             return coreOSAMI
