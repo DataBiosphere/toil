06445b55b1256b1169ea190e83565ce0511a1afc
DailyDreaming <lblauvel@ucsc.edu>
2018-11-25 14:22:34 -0800

    Slash.

--- docker/Dockerfile.py
+++ docker/Dockerfile.py
@@ -61,12 +61,9 @@ motd = ''.join(l + '\\n\\\n' for l in motd.splitlines())
 print(heredoc('''
     FROM ubuntu:16.04
 
-    RUN echo "#!/bin/bash" \
-        > ./mod_permissions.sh \
-        && echo "umask 0000" \
-        >> ./mod_permissions.sh \
-        && echo "/bin/bash" \
-        >> ./mod_permissions.sh
+    RUN echo "#!/bin/bash" > ./mod_permissions.sh \
+        && echo "umask 0000" >> ./mod_permissions.sh \
+        && echo "/bin/bash" >> ./mod_permissions.sh \
         && chmod +x ./mod_permissions.sh
     
     ENTRYPOINT ["./mod_permissions.sh"]
