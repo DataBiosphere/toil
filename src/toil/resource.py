ca3513946d1758e22e865bd8613fed3c41cdf323
Mark Asbach <mark.asbach@pixolus.de>
2018-11-30 13:52:46 +0100

    reverted change in docstring to allow for doctests to run through on both Python 2 and Python 3

--- src/toil/resource.py
+++ src/toil/resource.py
@@ -349,7 +349,7 @@ class ModuleDescriptor(namedtuple('ModuleDescriptor', ('dirPath', 'name', 'fromV
     >>> with open(path,'w') as f: 
     ...     _ = f.write('from toil.resource import ModuleDescriptor\\n'
     ...                 'print(ModuleDescriptor.forModule(__name__))')
-    >>> subprocess.check_output([ sys.executable, path ]).decode('utf-8') # doctest: +ELLIPSIS +ALLOW_BYTES
+    >>> subprocess.check_output([ sys.executable, path ]) # doctest: +ELLIPSIS +ALLOW_BYTES
     b"ModuleDescriptor(dirPath='...', name='foo', fromVirtualEnv=False)\\n"
 
     >>> from shutil import rmtree
