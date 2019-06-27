66434bd97751bd0698bc53d492e8a36eb4747425
Adam Novak <anovak@soe.ucsc.edu>
2019-06-27 14:15:50 -0700

    Spec out tables and implement state accessors

--- src/toil/fileStores/cachingFileStore.py
+++ src/toil/fileStores/cachingFileStore.py
@@ -34,6 +34,7 @@ import errno
 import logging
 import os
 import shutil
+import sqlite3
 import stat
 import tempfile
 import time
@@ -98,10 +99,36 @@ class CachingFileStore(AbstractFileStore):
     directory for the node, if permitted by the workflow.
 
     Also attempts to write files back to the backing JobStore asynchronously,
-    after quickly taking them into the cache.
+    after quickly taking them into the cache. Writes are only required to
+    finish when the job's actual state after running is committed back to the
+    job store.
     
-    TODO: is that even possible? How can another node tell a file being
-    asynchronously written from one that doesn't exist?
+    Internaly, manages caching using a database. Each node has its own
+    database, shared between all the workers on the node. The database contains
+    several tables:
+
+    files contains one entry for each file in the cache. Each entry knows the
+    path to its data on disk. It also knows its global file ID, whether it is
+    in the process of being read (and if so by what worker), and whether it is
+    in the process of being written (and if so by what worker). It also knows
+    its size.
+
+    references contains one entry for each outstanding reference to a cached
+    file (hard link, symlink, or full copy). It remembers what job ID has the
+    reference, and the path the reference is at.
+    Only files with no references are eligible for eviction.
+    Note that some references may be full copies. We nonetheless treat them as
+    references and don't let the file leave the cache until the references are
+    gone.
+
+    jobs contains one entry for each job currently running. It keeps track of
+    the job's ID, the worker that is supposed to be running the job, the job's
+    disk requirement, and the job's local temp dir path that will need to be
+    cleaned up. When workers check for jobs whose workers have died, they take
+    ownership of the jobs and clean them and their references up.
+
+    properties contains key, value pairs for tracking total space available,
+    and whether caching is free for this run.
     
     """
 
@@ -122,17 +149,58 @@ class CachingFileStore(AbstractFileStore):
         self.jobID = sha1(self.jobName.encode('utf-8')).hexdigest()
         logger.debug('Starting job (%s) with ID (%s).', self.jobName, self.jobID)
 
+        # When the job actually starts, we will fill this in with the job's disk requirement.
+        self.jobDiskBytes = None
+
         # We need to track what attempt of the workflow we are, to prevent crosstalk between attempts' caches.
         self.workflowAttemptNumber = self.jobStore.config.workflowAttemptNumber
 
         # Make sure the cache directory exists
         mkdir_p(self.localCacheDir)
 
-        # TODO: Connect to the cache database in there, or create it if not present
+        # Determine if caching is free
 
-        # We need to track what local files exist for this job for a given
-        # global file ID, because we expose an operation to delete them all.
-        self.localFileMap = defaultdict(list)
+        # Connect to the cache database in there, or create it if not present
+        self.db = sqlite3.connect(os.path.join(self.localCacheDir, 'cache-{}.db'.format(self.workflowAttemptNumber))).cursor()
+
+        # Set up the tables
+        self.db.execute("""
+            CREATE TABLE IF NOT EXISTS files (
+                id TEXT NOT NULL PRIMARY KEY, 
+                path TEXT UNIQUE NOT NULL,
+                size INT NOT NULL,
+                reader INT,
+                writer INT
+            )
+        """)
+        self.db.execute("""
+            CREATE TABLE IF NOT EXISTS references (
+                path TEXT NOT NULL PRIMARY KEY,
+                file_id TEXT NOT NULL,
+                job_id TEXT NOT NULL
+            )
+        """)
+        self.db.execute("""
+            CREATE TABLE IF NOT EXISTS jobs (
+                id TEXT NOT NULL PRIMARY KEY,
+                temp TEXT NOT NULL,
+                disk INT NOT NULL,
+                worker INT NOT NULL
+            )
+        """)
+        self.db.execute("""
+            CREATE TABLE IF NOT EXISTS properties (
+                name TEXT NOT NULL PRIMARY KEY,
+                value INT NOT NULL
+            )
+        """)
+
+        
+        # Initialize the space accounting properties
+        freeSpace, _ = getFileSystemSize(tempCacheDir)
+        self.db.execute('INSERT OR IGNORE INTO properties VALUES (?, ?)', ('maxSpace', freeSpace))
+
+        # Space used by caching and by jobs is accounted with queries
 
     # Caching-specific API
 
@@ -143,6 +211,8 @@ class CachingFileStore(AbstractFileStore):
         If no limit is in place, return None.
         """
 
+        for row in self.db.execute('SELECT value FROM properties WHERE name = ?', ('maxSpace',)):
+            return row[0]
         return None
 
     def getCacheUsed(self):
@@ -152,6 +222,8 @@ class CachingFileStore(AbstractFileStore):
         If no value is available, return None.
         """
 
+        for row in self.db.execute('SELECT SUM(size) FROM files'):
+            return row[0]
         return None
 
     def getCacheExtraJobSpace(self):
@@ -159,9 +231,21 @@ class CachingFileStore(AbstractFileStore):
         Return the total number of bytes of disk space requested by jobs
         running against this cache but not yet used.
 
+        We can get into a situation where the jobs on the node take up all its
+        space, but then they want to write to or read from the cache. So when
+        that happens, we need to debit space from them somehow...
+
         If no value is available, return None.
         """
 
+        # Total up the sizes of all the reads of files and subtract it from the total disk reservation of all jobs
+        for row in self.db.execute("""
+            SELECT (
+                (SELECT SUM(disk) FROM jobs) - 
+                (SELECT SUM(files.size) FROM references INNER JOIN files ON references.file_id = files.id)
+            ) as result
+        """):
+            return row[0]
         return None
 
     def getCacheJobRequirement(self):
@@ -175,21 +259,25 @@ class CachingFileStore(AbstractFileStore):
         If no value is available, return None.
         """
 
-        return None
+        # TODO: remove "cache" from name?
+    
+        return self.jobDiskBytes
 
 
     def adjustCacheLimit(self, newTotalBytes):
         """
-        Adjust the total cache size limit to the given number of megabytes.
+        Adjust the total cache size limit to the given number of bytes.
         """
 
-        pass
+        self.db.execute('UPDATE properties SET value = ? WHERE name = ?', (newTotalBytes, 'maxSpace')):
 
     def fileIsCached(self, fileID):
         """
         Return true if the given file is currently cached, and false otherwise.
         """
 
+        for row in self.db.execute('SELECT COUNT(*) FROM files WHERE id = ?', (fileID,)):
+            return True
         return False
 
     def getFileReaderCount(self, fileID):
@@ -198,6 +286,8 @@ class CachingFileStore(AbstractFileStore):
         the cache.
         """
 
+        for row in self.db.execute('SELECT COUNT(*) FROM references WHERE file_id = ?', (fileID,)):
+            return row[0]
         return 0
         
 
@@ -210,7 +300,55 @@ class CachingFileStore(AbstractFileStore):
         configurations, most notably the FileJobStore.
         """
 
-        return True
+        for row in self.db.execute('SELECT value FROM properties WHERE name = ?', ('freeCaching',)):
+            return row[0] == 1
+
+        # Otherwise we need to set it
+        from toil.jobStores.fileJobStore import FileJobStore
+        if isinstance(self.jobStore, FileJobStore):
+            # Caching may be free since we are using a file job store.
+
+            # Create an empty file.
+            emptyID = self.jobStore.getEmptyFileStoreID()
+
+            # Read it out to a generated name.
+            destDir = tempfile.mkdtemp(dir=self.localCacheDir)
+            cachedFile = os.path.join(destDir, 'sniffLinkCount') 
+            self.jobStore.readFile(emptyID, cachedFile, symlink=False)
+
+            # Check the link count
+            if os.stat(cachedFile).st_nlink == 2:
+                # Caching must be free
+                free = 1
+            else:
+                # If we only have one link, caching costs disk.
+                free = 0
+
+            # Clean up
+            os.unlink(cachedFile)
+            os.rmdir(destDir)
+            self.jobStore.deleteFile(emptyID)
+        else:
+            # Caching is only ever free with the file job store
+            free = 0
+
+        # Save to the database if we're the first to work this out
+        self.db.execute('INSERT OR IGNORE INTO properties VALUES (?, ?)', ('freeCaching', free))
+
+        # Return true if we said caching was free
+        return free == 1
+
+
+    # Internal caching logic
+
+    def _freeUpBytes(self, newJobReqs):
+        """ 
+        Cleanup all files in the cache directory to ensure that at lead newJobReqs are available
+        for use.
+        :param float newJobReqs: the total number of bytes of files allowed in the cache.
+        """
+
+        pass
 
     # Normal AbstractFileStore API
 
@@ -229,10 +367,10 @@ class CachingFileStore(AbstractFileStore):
         self._removeDeadJobs()
         # Get the requirements for the job and clean the cache if necessary. _freeUpBytes will
         # ensure that the requirements for this job are stored in the state file.
-        jobReqs = job.disk
+        self.jobDiskBytes = job.disk
         # Cleanup the cache to free up enough space for this job (if needed)
-        # TODO: Atomically record that jobReqs bytes of disk space are in use by this job.
-        self._freeUpBytes(jobReqs)
+        # TODO: Atomically record that self.jobDiskBytes bytes of disk space are in use by this job.
+        self._freeUpBytes(self.jobDiskBytes)
         try:
             os.chdir(self.localTempDir)
             yield
@@ -244,14 +382,14 @@ class CachingFileStore(AbstractFileStore):
             logString = ("Job {jobName} used {percent:.2f}% ({humanDisk}B [{disk}B] used, "
                          "{humanRequestedDisk}B [{requestedDisk}B] requested) at the end of "
                          "its run.".format(jobName=self.jobName,
-                                           percent=(float(diskUsed) / jobReqs * 100 if
-                                                    jobReqs > 0 else 0.0),
+                                           percent=(float(diskUsed) / self.jobDiskBytes * 100 if
+                                                    self.jobDiskBytes > 0 else 0.0),
                                            humanDisk=bytes2human(diskUsed),
                                            disk=diskUsed,
-                                           humanRequestedDisk=bytes2human(jobReqs),
-                                           requestedDisk=jobReqs))
+                                           humanRequestedDisk=bytes2human(self.jobDiskBytes),
+                                           requestedDisk=self.jobDiskBytes))
             self.logToMaster(logString, level=logging.DEBUG)
-            if diskUsed > jobReqs:
+            if diskUsed > self.jobDiskBytes:
                 self.logToMaster("Job used more disk than requested. Please reconsider modifying "
                                  "the user script to avoid the chance  of failure due to "
                                  "incorrectly requested resources. " + logString,
@@ -261,7 +399,7 @@ class CachingFileStore(AbstractFileStore):
             os.chdir(startingDir)
             self.cleanupInProgress = True
             
-            # TODO: record that jobReqs bytes of disk space are now no longer used by this job.
+            # TODO: record that self.jobDiskBytes bytes of disk space are now no longer used by this job.
 
     def writeGlobalFile(self, localFileName, cleanup=False):
         absLocalFileName = self._resolveAbsoluteLocalPath(localFileName)
@@ -320,15 +458,6 @@ class CachingFileStore(AbstractFileStore):
 
     def exportFile(self, jobStoreFileID, dstUrl):
         self.jobStore.exportFile(jobStoreFileID, dstUrl)
-
-    def _freeUpBytes(self, newJobReqs):
-        """
-        Cleanup all files in the cache directory to ensure that at lead newJobReqs are available
-        for use.
-        :param float newJobReqs: the total number of bytes of files allowed in the cache.
-        """
-       
-        pass
 
     def waitForCommit(self):
         # there is no asynchronicity in this file store so no need to block at all
