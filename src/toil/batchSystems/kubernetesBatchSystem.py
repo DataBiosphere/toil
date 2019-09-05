43ac84a1de3f5a8185eb646f30f0f53f9d6eaca5
Adam Novak <anovak@soe.ucsc.edu>
2019-09-05 16:28:59 -0700

    Find Kubernetes namespace and define a prefix for jobs

--- src/toil/batchSystems/kubernetesBatchSystem.py
+++ src/toil/batchSystems/kubernetesBatchSystem.py
@@ -4,6 +4,7 @@
 run.py: run a command via Kubernetes
 """
 
+import getpass
 import kubernetes
 import uuid
 import time
@@ -15,6 +16,21 @@ class kubernetesBatchSystem():
    
     def __init__(self, config, maxCores, maxMemory, maxDisk):
         super(kubernetesBatchSystem, self).__init__(config, maxCores, maxMemory, maxDisk)
+
+        # Load ~/.kube/config
+        kubernetes.config.load_kube_config()
+
+        # Find all contexts and the active context.
+        # The active context gets us our namespace.
+        contexts, active_context = kubernetes.config.list_kube_config_contexts()
+        if not contexts:
+            raise RuntimeError("No Kubernetes contexts available in ~/.kube/config")
+
+        # Identify the namespace to work in
+        self.namespace = active_context.get('context', {}).get('namespace', 'default')
+
+        # Create a prefix for jobs, starting with our username
+        self.jobPrefix = '{}-toil-{}-'.format(getpass.getuser(), uuid.uuid4())
 
     
     def issueBatchJob(self, jobNode):
