32ce9f76b8e9ee395735b0b9a66f8501d4e27e41
Adam Novak <anovak@soe.ucsc.edu>
2019-09-12 13:54:04 -0700

    Use the local batch system support to ID jobs, and add helper methods

--- src/toil/batchSystems/kubernetes.py
+++ src/toil/batchSystems/kubernetes.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python3
+# Copyright (C) 2019 Regents of the University of California
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
-run.py: run a command via Kubernetes
+Batch system for running Toil workflows on Kubernetes.
+
+Ony useful with network-based job stores, like AWSJobStore.
+
+Within non-priveleged Kubernetes containers, additional Docker containers
+cannot yet be launched. That functionality will need to wait for user-mode
+Docker
 """
 
 from __future__ import absolute_import
@@ -26,7 +44,7 @@ from toil.batchSystems.abstractBatchSystem import (AbstractBatchSystem,
 log = logging.getLogger(__name__)
 
 
-class KubernetesBatchSystem(AbstractBatchSystem):
+class KubernetesBatchSystem(BatchSystemLocalSupport):
 
     @classmethod
     def supportsAutoDeployment(cls):
@@ -47,12 +65,19 @@ class KubernetesBatchSystem(AbstractBatchSystem):
         contexts, active_context = kubernetes.config.list_kube_config_contexts()
         if not contexts:
             raise RuntimeError("No Kubernetes contexts available in ~/.kube/config")
+            
+        # Connect to Kubernetes
+        self.api = kubernetes.client.BatchV1Api()
 
         # Identify the namespace to work in
         self.namespace = active_context.get('context', {}).get('namespace', 'default')
 
         # Create a prefix for jobs, starting with our username
         self.jobPrefix = '{}-toil-{}-'.format(getpass.getuser(), uuid.uuid4())
+        
+        # Instead of letting Kubernetes assign unique job names, we assign our
+        # own based on a numerical job ID. This functionality is managed by the
+        # BatchSystemLocalSupport.
 
         # Here is where we will store the user script resource object if we get one.
         self.userScript = None
@@ -65,77 +90,135 @@ class KubernetesBatchSystem(AbstractBatchSystem):
         self.userScript = userScript
     
     def issueBatchJob(self, jobNode):
-        self.checkResourceRequest(jobNode.memory, jobNode.cores, jobNode.disk)
-
-        # Make a job dict to send to the executor.
-        # First just wrap the command and the environment to run it in
-        job = {'command': jobNode.command,
-               'environment': self.environment.copy()}
-
-        if self.userScript is not None:
-            # If there's a user script resource be sure to send it along
-            job['userScript'] = self.userScript
-
-        # Encode it in a form we can send in a command-line argument
-        encodedJob = base64.encode(pickle.dumps(job))
-
-        # TODO: remove some of these arguments and integrate the _runCommand method better into the object
-        # TODO: Propagate the job's requirements here
-        # TODO: Do something useful with the returned job like remembering we launched it
-        self._runCommand(['_toil_kubernetes_executor', encodedJob], self.dockerImage, self.namespace, basename=self.jobPrefix)
-
-
-    def _runCommand(command, image, namespace, cores=1.0, mem_bytes=1 * 1024 * 1024 * 1024, disk_bytes=10 * 1024 * 1024 * 1024, basename="job-"):
-        """
-        Run the given command (a list of strings) in the given Docker image
-        specifier. Run the job under the given Kubernetes namespace name.
+        # TODO: get a sensible self.maxCores, etc. so we can checkResourceRequest.
+        # How do we know if the cluster will autoscale?
         
-        Uses the given float number of CPU cores and the given number of bytes of
-        memory and (ephemeral) disk to run the job.
+        # Try the job as local
+        localID = self.handleLocalJob(jobNode)
+        if localID:
+            # It is a local job
+            return localID
+        else:
+            # We actually want to send to the cluster
+            
+            # Make a batch system scope job ID
+            jobID = self.getNextJobID()
+            # Make a unique name
+            jobName = self.jobPrefix + str(jobID)
 
-        Returns the Kubernetes V1Job, which has a .metadata.name field that can be
-        used to get its status.
+            # Make a job dict to send to the executor.
+            # First just wrap the command and the environment to run it in
+            job = {'command': jobNode.command,
+                   'environment': self.environment.copy()}
+
+            if self.userScript is not None:
+                # If there's a user script resource be sure to send it along
+                job['userScript'] = self.userScript
+
+            # Encode it in a form we can send in a command-line argument
+            encodedJob = base64.encode(pickle.dumps(job))
+
+            # TODO: remove some of these arguments and integrate the _runCommand method better into the object
+            # TODO: Propagate the job's requirements here
+            # TODO: Do something useful with the returned job like remembering we launched it
+            self._runCommand(, self.dockerImage, self.namespace, basename=self.jobPrefix)
+            
+            # The Kubernetes API makes sense only in terms of the YAML format. Objects
+            # represent sections of the YAML files. Except from our point of view, all
+            # the internal nodes in the YAML structure are named and typed.
+
+            # For docs, start at the root of the job hierarchy:
+            # https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/V1Job.md
+
+            # Make a definition for the container's resource requirements
+            requirements_dict = {'cpu': jobNode.cores,
+                                 'memory': jobNode.memory,
+                                 'ephemeral-storage': jobNode.disk}
+            resources = kubernetes.client.V1ResourceRequirements(limits=requirements_dict,
+                                                                 requests=requirements_dict)
+            # Make a volume to provision disk
+            volume_name = 'tmp'
+            volume_source = kubernetes.client.V1EmptyDirVolumeSource()
+            volume = kubernetes.client.V1Volume(name=volume_name, empty_dir=volume_source)
+            # Make a mount for the volume
+            volume_mount = kubernetes.client.V1VolumeMount(mount_path='/tmp', name=volume_name)
+            # Make a container definition
+            container = kubernetes.client.V1Container(command=['_toil_kubernetes_executor', encodedJob],
+                                                      image=image,
+                                                      name="runner-container",
+                                                      resources=resources,
+                                                      volume_mounts=[volume_mount])
+            # Wrap the container in a spec
+            pod_spec = kubernetes.client.V1PodSpec(containers=[container],
+                                                   volumes=[volume],
+                                                   restart_policy="Never")
+            # Wrap the spec in a template
+            template = kubernetes.client.V1PodTemplateSpec(spec=pod_spec)
+            # Make another spec for the job, asking to run the template with backoff
+            job_spec = kubernetes.client.V1JobSpec(template=template, backoff_limit=1)
+            # Make metadata to tag the job with info.
+            # We use generate_name to ensure a unique name
+            metadata = kubernetes.client.V1ObjectMeta(name=jobName)
+            # And make the actual job
+            job = kubernetes.client.V1Job(spec=job_spec,
+                                          metadata=metadata,
+                                          api_version="batch/v1",
+                                          kind="Job")
+            
+            # Make the job
+            launched = self.api.create_namespaced_job(namespace, job)
+
+            log.debug('Launched job: %s', str(launched))
+            
+            return jobID
+            
+            
+    def _isJobOurs(self, jobObject):
         """
-
-        # The Kubernetes API makes sense only in terms of the YAML format. Objects
-        # represent sections of the YAML files. Except from our point of view, all
-        # the internal nodes in the YAML structure are named and typed.
-
-        # For docs, start at the root of the job hierarchy:
-        # https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/V1Job.md
-
-        # Make a definition for the container's resource requirements
-        requirements_dict = {'cpu': cores, 'memory': mem_bytes, 'ephemeral-storage': disk_bytes}
-        resources = kubernetes.client.V1ResourceRequirements(limits=requirements_dict, requests=requirements_dict)
-        # Make a volume to provision disk
-        volume_name = 'tmp'
-        volume_source = kubernetes.client.V1EmptyDirVolumeSource()
-        volume = kubernetes.client.V1Volume(name=volume_name, empty_dir=volume_source)
-        # Make a mount for the volume
-        volume_mount = kubernetes.client.V1VolumeMount(mount_path='/tmp', name=volume_name)
-        # Make a container definition
-        container = kubernetes.client.V1Container(command=command, image=image, name="runner-container", resources=resources, volume_mounts=[volume_mount])
-        # Wrap the container in a spec
-        pod_spec = kubernetes.client.V1PodSpec(containers=[container], volumes=[volume], restart_policy="Never")
-        # Wrap the spec in a template
-        template = kubernetes.client.V1PodTemplateSpec(spec=pod_spec)
-        # Make another spec for the job, asking to run the template with backoff
-        job_spec = kubernetes.client.V1JobSpec(template=template, backoff_limit=1)
-        # Make metadata to tag the job with info.
-        # We use generate_name to ensure a unique name
-        metadata = kubernetes.client.V1ObjectMeta(generate_name=basename)
-        # And make the actual job
-        job = kubernetes.client.V1Job(spec=job_spec, metadata=metadata, api_version="batch/v1", kind="Job")
+        Determine if a Kubernetes job belongs to us.
         
-        # Get a versioned Kubernetes batch API
-        api = kubernetes.client.BatchV1Api()
+        :param kubernetes.client.V1Job jobObject: a Kubernetes job being considered.
 
-        # Make the job
-        launched = api.create_namespaced_job(namespace, job)
-
-        log.debug('Launched job: %s', str(launched))
-
-        return launched
+        :return: True if the job is our responsibility, and false otherwise.
+        :rtype: bool
+        """
+        
+        return jobObject.metadata.name.startswith(self.jobPrefix)
+        
+        
+    
+    def _allOurJobObjects(self):
+        """
+        Yield all Kubernetes V1Job objects that we are responsible for that the cluster knows about.
+        """
+        
+        # We need to page through the list from the cluster with a continuation
+        # token. These expire after about 5 minutes. If we use an expired one,
+        # we get a 410 error and a new token, and we can use the new token to
+        # get the rest of the list, but the list will be updated.
+        #
+        # TODO: How to get the new token isn't clear. See
+        # https://github.com/kubernetes-client/python/issues/953. For now we
+        # will just throw an error if we don't get to the end of the list in
+        # time.
+        token = None
+        
+        while True:
+            try:
+                results = self.api.list_namespaced_job(self.namespace, _continue = token)
+            except:
+                
+            
+            for job in results.items:
+                if self._isJobOurs(job):
+                    # This job belongs to us
+                    yield job
+                    
+            # Remember the continuation token
+            
+        
+        for job in 
+        
 
     def shutdown(self):
         # needed api to shutdown cluster
