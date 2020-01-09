29041e460c3731a56e7a67da44cb28f47063a98f
Adam Novak <anovak@soe.ucsc.edu>
2020-01-09 12:55:40 -0800

    Instrument Quay login

--- setup_gitlab_docker.py
+++ setup_gitlab_docker.py
@@ -5,13 +5,30 @@ import sys
 
 stderr = 'Login was not attempted'
 
+env_var = 'GITLAB_SECRET_FILE_QUAY_CREDENTIALS'
+
 try:
-    with open(os.environ['GITLAB_SECRET_FILE_QUAY_CREDENTIALS'], 'r') as cred_json_file:
+    if env_var not in os.environ:
+        print('Error: could not find environment variable ' + env_var)
+        sys.exit(1)
+
+    filename = os.environ[env_var]
+
+    if not os.path.exists(filename):
+        print('Error: could not find file referenced by ' + env_var)
+        sys.exit(1)
+        
+    print('Opening key file...')
+    with open(env_var, 'r') as cred_json_file:
+        print('Reading keys...')
         keys = json.loads(cred_json_file.read())
+        print('Read and decoded keys')
     
+    print('Starting login process...')
     process = subprocess.Popen('docker login quay.io -u "{user}" --password-stdin'.format(user=keys['user']),
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,
                                shell=True)
+    print('Logging in...')
     stdout, stderr = process.communicate(input=keys['password'])
     if 'Login Succeeded' in stdout:
         print('Login Succeeded')
