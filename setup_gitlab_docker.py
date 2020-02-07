6d2458b0ea0004a8cbddc15756fc540a4595f1fa
DailyDreaming <lblauvel@ucsc.edu>
2020-02-06 19:25:16 -0800

    Clean up.

--- setup_gitlab_docker.py
+++ setup_gitlab_docker.py
@@ -3,37 +3,23 @@ import os
 import subprocess
 import sys
 
-stderr = 'Login was not attempted'
-
 env_var = 'GITLAB_SECRET_FILE_QUAY_CREDENTIALS'
 
-try:
-    if env_var not in os.environ:
-        print('Error: could not find environment variable ' + env_var)
-        sys.exit(1)
+if env_var not in os.environ:
+    print('Error: could not find environment variable ' + env_var)
+    sys.exit(1)
 
-    filename = os.environ[env_var]
+print('Starting quay.io login process...')
+with open(os.environ[env_var], 'r') as f:
+    keys = json.load(f)
+process = subprocess.Popen('docker login quay.io -u "{user}" --password-stdin'.format(user=keys['user']),
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,
+                           shell=True)
+print('Logging into quay.io...')
+stdout, stderr = process.communicate(input=keys['password'].encode('utf-8'))
 
-    if not os.path.exists(filename):
-        print('Error: could not find file referenced by ' + env_var)
-        sys.exit(1)
-        
-    print('Opening key file...')
-    with open(filename, 'r') as cred_json_file:
-        print('Reading keys...')
-        keys = json.loads(cred_json_file.read())
-        print('Read and decoded keys')
-    
-    print('Starting login process...')
-    process = subprocess.Popen('docker login quay.io -u "{user}" --password-stdin'.format(user=keys['user']),
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,
-                               shell=True)
-    print('Logging in...')
-    stdout, stderr = process.communicate(input=keys['password'])
-    if 'Login Succeeded' in stdout:
-        print('Login Succeeded')
-    else:
-        raise RuntimeError
-except:
+if 'Login Succeeded' in str(stdout):
+    print('Login Succeeded.')
+else:
     print('Error while attempting to log into quay.io:\n' + str(stderr))
     sys.exit(1)
--- setup_gitlab_ssh.py
+++ setup_gitlab_ssh.py
@@ -1,18 +1,25 @@
 import json
 import os
-import subprocess
-import sys
 
 good_spot = os.path.expanduser('~/.ssh')
-os.mkdir(good_spot)
+if not os.path.exists(good_spot):
+    os.mkdir(good_spot)
 
-try:
-    with open(os.environ['GITLAB_SECRET_FILE_SSH_KEYS'], 'r') as keys_json_file:
-        keys = json.loads(keys_json_file.read())
-    with open(os.path.join(good_spot, 'id_rsa.pub'), 'w') as f:
-        f.write(keys['public'])
-    with open(os.path.join(good_spot, 'id_rsa'), 'w') as f:
-        f.write(keys['private'])
-except:
-    print('While attempting to set up the ssh keys.')
-    sys.exit(1)
+
+def format_private_key(private_key):
+    private_key_header = '-----BEGIN RSA PRIVATE KEY-----'
+    private_key_footer = '-----END RSA PRIVATE KEY-----'
+
+    assert private_key.startswith(private_key_header)
+    private_key_body = private_key[len(private_key_header):]
+    assert private_key_body.endswith(private_key_footer)
+    private_key_body = private_key_body[:-len(private_key_footer)]
+    return private_key_header + private_key_body.replace(' ', '\n') + private_key_footer
+
+
+with open(os.environ['GITLAB_SECRET_FILE_SSH_KEYS'], 'r') as keys_json_file:
+    keys = json.loads(keys_json_file.read())
+with open(os.path.join(good_spot, 'id_rsa.pub'), 'w') as f:
+    f.write(keys['public'])
+with open(os.path.join(good_spot, 'id_rsa'), 'w') as f:
+    f.write(format_private_key(keys['private']))
