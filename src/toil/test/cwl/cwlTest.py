bb5d1818d8559d23d2e5b972925cc10b917d87b4
Michael R. Crusoe <michael.crusoe@gmail.com>
2020-07-07 22:07:07 +0200

    upgrade cwltool to cwl v1.2.0-dev4

--- src/toil/test/cwl/cwlTest.py
+++ src/toil/test/cwl/cwlTest.py
@@ -390,7 +390,7 @@ class CWLv12Test(ToilTest):
         cls.test_yaml = os.path.join(cls.cwlSpec, 'conformance_tests.yaml')
         # TODO: Use a commit zip in case someone decides to rewrite master's history?
         url = 'https://github.com/common-workflow-language/cwl-v1.2.git'
-        commit = '58d7ac9923a4233251c9b9680f5bd27315b20c87'
+        commit = '936961d1c1c5c4ea13df253b1316a264e48df740'
         p = subprocess.Popen(f'git clone {url} {cls.cwlSpec} && cd {cls.cwlSpec} && git checkout {commit}', shell=True)
         p.communicate()
 
