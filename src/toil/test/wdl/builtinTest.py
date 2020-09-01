8bd68782a83447da25c365c9f51faf82bd5a3be7
DailyDreaming <lblauvel@ucsc.edu>
2020-09-01 12:56:35 -0400

    WDL test naming.

--- src/toil/test/wdl/builtinTest.py
+++ src/toil/test/wdl/builtinTest.py
@@ -1,14 +1,21 @@
 import unittest
 import os
 import subprocess
-from toil.version import exactPython
-from toil.test import ToilTest
 import shutil
 import uuid
 
+from toil.version import exactPython
+from toil.test import ToilTest
 
-class wdlBuiltInsTest(ToilTest):
-    """A set of test cases for toil's conformance with the WDL built-in standard library."""
+
+class WdlStandardLibraryWorkflowsTest(ToilTest):
+    """
+    A set of test cases for toil's conformance with the WDL built-in standard library:
+
+    https://github.com/openwdl/wdl/blob/main/versions/development/SPEC.md#standard-library
+
+    All tests should include a simple wdl and json file for toil to run that checks the output.
+    """
     def setUp(self):
         """Runs anew before each test to create farm fresh temp dirs."""
         self.output_dir = os.path.join('/tmp/', 'toil-wdl-test-' + str(uuid.uuid4()))
@@ -24,8 +31,8 @@ class wdlBuiltInsTest(ToilTest):
         unittest.TestCase.tearDown(self)
 
     def test_ceil(self):
-        wdl_file = os.path.abspath('src/toil/test/wdl/standard_library/basic_ceil.wdl')
-        json_file = os.path.abspath('src/toil/test/wdl/standard_library/basic_ceil.json')
+        wdl_file = os.path.abspath('src/toil/test/wdl/standard_library/ceil.wdl')
+        json_file = os.path.abspath('src/toil/test/wdl/standard_library/ceil.json')
         subprocess.check_call([exactPython, self.program, wdl_file, json_file, '-o', self.output_dir])
         output = os.path.join(self.output_dir, 'the_ceiling.txt')
         with open(output, 'r') as f:
