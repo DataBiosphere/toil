aa51121178fb63be8abedb192695e62de93e6e7a
DailyDreaming <lblauvel@ucsc.edu>
2020-10-08 19:19:24 -0400

    Check types.

--- src/toil/cwl/cwltoil.py
+++ src/toil/cwl/cwltoil.py
@@ -367,10 +367,11 @@ class StepValueFrom:
     def eval_prep(self, step_inputs, file_store):
         for k, v in step_inputs.items():
             val = cast(CWLObjectType, v)
-            if val.get("contents") is None and self.source.input['loadContents'] is True:
-                fs_access = functools.partial(ToilFsAccess, file_store=file_store)
-                with fs_access('').open(cast(str, val["location"]), "rb") as f:
-                    val["contents"] = content_limit_respected_read(f)
+            if isinstance(val, dict) and isinstance(self.source.input, dict):
+                if val.get("contents") is None and self.source.input.get('loadContents') is True:
+                    fs_access = functools.partial(ToilFsAccess, file_store=file_store)
+                    with fs_access('').open(cast(str, val["location"]), "rb") as f:
+                        val["contents"] = content_limit_respected_read(f)
 
     def resolve(self) -> Any:
         """
