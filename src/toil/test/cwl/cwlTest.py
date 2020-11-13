d7864156f7846258d507fca5fa65127cd3032cff
DailyDreaming <lblauvel@ucsc.edu>
2020-11-13 16:37:14 -0700

    Run the kubernetes CWL tests.

--- src/toil/test/cwl/cwlTest.py
+++ src/toil/test/cwl/cwlTest.py
@@ -50,6 +50,9 @@ def run_conformance_tests(workDir, yml, caching=False, batchSystem=None, selecte
 
         args_passed_directly_to_toil = [f'--disableCaching={not caching}', '--clean=always']
 
+        if batchSystem == 'kubernetes' and 'CWL_K8_TEST_BUCKET' in os.environ:
+            args_passed_directly_to_toil.append(f'--jobStore=aws:us-west-2:{os.environ["CWL_K8_TEST_BUCKET"]}')
+
         if batchSystem:
             args_passed_directly_to_toil.append(f"--batchSystem={batchSystem}")
         cmd.extend(['--'] + args_passed_directly_to_toil)
