85e31a59ac307fc848280e98091bf4a8495b1829
DailyDreaming <lblauvel@ucsc.edu>
2020-12-16 17:58:14 -0800

    Remove unused functions from memoize.

--- src/toil/lib/memoize.py
+++ src/toil/lib/memoize.py
@@ -65,63 +65,6 @@ def sync_memoize(f):
     return new_f
 
 
-def rfc3339_datetime_re(anchor=True):
-    """
-    Returns a regular expression for syntactic validation of ISO date-times, RFC-3339 date-times
-    to be precise.
-
-
-    >>> bool( rfc3339_datetime_re().match('2013-11-06T15:56:39Z') )
-    True
-
-    >>> bool( rfc3339_datetime_re().match('2013-11-06T15:56:39.123Z') )
-    True
-
-    >>> bool( rfc3339_datetime_re().match('2013-11-06T15:56:39-08:00') )
-    True
-
-    >>> bool( rfc3339_datetime_re().match('2013-11-06T15:56:39.123+11:00') )
-    True
-
-    It anchors the matching to the beginning and end of a string by default ...
-
-    >>> bool( rfc3339_datetime_re().search('bla 2013-11-06T15:56:39Z bla') )
-    False
-
-    ... but that can be changed:
-
-    >>> bool( rfc3339_datetime_re( anchor=False ).search('bla 2013-11-06T15:56:39Z bla') )
-    True
-
-    >>> bool( rfc3339_datetime_re( anchor=False ).match('2013-11-06T15:56:39Z bla') )
-    True
-
-    Keep in mind that re.match() always anchors at the beginning:
-
-    >>> bool( rfc3339_datetime_re( anchor=False ).match('bla 2013-11-06T15:56:39Z') )
-    False
-
-    It does not check whether the actual value is a semantically valid datetime:
-
-    >>> bool( rfc3339_datetime_re().match('9999-99-99T99:99:99.9-99:99') )
-    True
-
-    If the regular expression matches, each component of the matching value will be exposed as a
-    captured group in the match object.
-
-    >>> rfc3339_datetime_re().match('2013-11-06T15:56:39Z').groups()
-    ('2013', '11', '06', '15', '56', '39', None, 'Z')
-    >>> rfc3339_datetime_re().match('2013-11-06T15:56:39.123Z').groups()
-    ('2013', '11', '06', '15', '56', '39', '123', 'Z')
-    >>> rfc3339_datetime_re().match('2013-11-06T15:56:39.123-08:30').groups()
-    ('2013', '11', '06', '15', '56', '39', '123', '-08:30')
-    """
-    return re.compile(
-        ('^' if anchor else '') +
-        '(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d+))?(Z|[+-]\d{2}:\d{2})' +
-        ('$' if anchor else ''))
-
-
 def parse_iso_utc(s):
     """
     Parses an ISO time with a hard-coded Z for zulu-time (UTC) at the end. Other timezones are
@@ -142,9 +85,10 @@ def parse_iso_utc(s):
     ...
     ValueError: Not a valid ISO datetime in UTC: 2016-04-27T00:28:04X
     """
-    m = rfc3339_datetime_re().match(s)
+    rfc3339_datetime = re.compile('^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d+))?(Z|[+-]\d{2}:\d{2})$')
+    m = rfc3339_datetime.match(s)
     if not m:
-        raise ValueError( 'Not a valid ISO datetime in UTC: ' + s )
+        raise ValueError(f'Not a valid ISO datetime in UTC: {s}')
     else:
         fmt = '%Y-%m-%dT%H:%M:%S' + ('.%f' if m.group(7) else '') + 'Z'
         return datetime.datetime.strptime(s, fmt)
