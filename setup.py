7172446ff20970ad031b7d1f1b973d8621845afb
DailyDreaming <lblauvel@ucsc.edu>
2021-01-20 21:46:17 -0800

    Update deps.

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ def run_setup():
     Calls setup(). This function exists so the setup() invocation preceded more internal
     functionality. The `version` module is imported dynamically by import_version() below.
     """
-    boto = 'boto==2.48.0'
-    boto3 = 'boto3>=1.7.50, <2.0'
-    futures = 'futures==3.1.1'
+    boto = 'boto>=2.48.0, <3'
+    boto3 = 'boto3>=1.7.50, <2'
+    futures = 'futures>=3.1.1, <4'
     pycryptodome = 'pycryptodome==3.5.1'
     pymesos = 'pymesos==0.3.15'
     psutil = 'psutil >= 3.0.1, <6'
