5a4a8e38078738276bee62724462921b2a138478
DailyDreaming <lblauvel@ucsc.edu>
2021-02-11 12:19:53 -0800

    Add make target to remove trailing whitespace.  Add docstrings.

--- contrib/admin/remove_trailing_whitespace.py
+++ contrib/admin/remove_trailing_whitespace.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""
+A script to remove all trailing whitespace from the Toil codebase's text files
+(mainly targeting .py files, but also things like .sh and .txt files).
+
+Run with:
+    remove_trailing_whitespace.py
+
+Note: This script preserves file permissions (and thus, executability) and that should be maintained.
+"""
 import os
 import sys
 
@@ -6,6 +15,8 @@ pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')) 
 sys.path.insert(0, pkg_root)  # noqa
 
 
+# changing either of these variables will change the targets of this script
+TOIL_DIRS_TO_PROCESS = {'attic', 'dashboard', 'docker', 'docs', 'src'}
 EXTENSIONS_TO_PROCESS = {'.csv',
                          '.cwl',
                          '.input',
@@ -25,7 +36,8 @@ EXTENSIONS_TO_PROCESS = {'.csv',
                          'Makefile'}
 
 
-def strip_whitespace_from_file(filename: str):
+def strip_trailing_whitespace_from_file(filename: str):
+    """Strips trailing whitespace from a file, in-place."""
     pristine_lines = []
     with open(filename, 'r') as r:
         for line in r:
@@ -36,20 +48,26 @@ def strip_whitespace_from_file(filename: str):
             w.write(pristine_line + '\n')
 
 
-def strip_whitespace_from_all_files_in_dir(dirname: str):
+def strip_trailing_whitespace_from_all_files_in_dir(dirname: str):
+    """
+    Strips trailing whitespace from all files in a directory, recursively.
+
+    Only strips files ending in one of the white-listed extensions in EXTENSIONS_TO_PROCESS.
+    Note: This includes things like "Dockerfile" that end in "Dockerfile".
+    """
     for dirpath, dirnames, filenames in os.walk(dirname):
         for f in filenames:
             for ext in EXTENSIONS_TO_PROCESS:
                 if f.endswith(ext):
-                    strip_whitespace_from_file(os.path.abspath(os.path.join(dirpath, f)))
+                    strip_trailing_whitespace_from_file(os.path.abspath(os.path.join(dirpath, f)))
                     break
         for d in dirnames:
-            strip_whitespace_from_all_files_in_dir(os.path.abspath(os.path.join(dirpath, d)))
+            strip_trailing_whitespace_from_all_files_in_dir(os.path.abspath(os.path.join(dirpath, d)))
 
 
 def main():
-    for directory in ['src', 'docs', 'docker', 'dashboard', 'attic']:
-        strip_whitespace_from_all_files_in_dir(os.path.join(pkg_root, directory))
+    for directory in TOIL_DIRS_TO_PROCESS:
+        strip_trailing_whitespace_from_all_files_in_dir(os.path.join(pkg_root, directory))
 
 
 if __name__ == '__main__':
