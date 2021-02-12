20b48f1ee085daf536ca1f2611c40a980b7a8b34
William Gao <wlgao@ucsc.edu>
2021-02-11 16:19:27 -0800

    Add WDL keys() function.

--- src/toil/wdl/wdl_functions.py
+++ src/toil/wdl/wdl_functions.py
@@ -962,3 +962,15 @@ def as_map(in_array: List[WDLPair]) -> dict:
         map[pair.left] = pair.right
 
     return map
+
+
+def keys(in_map: dict) -> list:
+    """
+    Given a Map, the `keys` function returns an Array consisting of the keys in
+    the Map. The order of the keys in the resulting Array is the same as the
+    order of the Pairs in the Map.
+
+    WDL syntax: Array[X] keys(Map[X,Y])
+    """
+
+    return list(in_map.keys())
