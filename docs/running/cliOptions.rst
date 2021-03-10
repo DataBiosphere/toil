fef23691ff27c9e5284ff54796f6d9756d87c7b8
Adam Novak <anovak@soe.ucsc.edu>
2021-03-10 13:00:52 -0800

    Add a test for parsing a format for multiple node types
    Document new node type format
    
    Start refactoring so node types are one or more instance types
    
    Continue refactoring and try to handle the TOil autoscaler not knowing about equivalence classes
    
    Fix provisioner syntax and undefined names
    
    Use slash node type separator
    
    Put leader node type in a set properly
    
    Stop making tuples on accident
    
    Pull in a boto3 that can override launch template by instance type in an ASG
    
    Deduplicate launch templates by name using AWS
    
    Drop trailing spaces
    
    Recurse to retry if we can't read our writes
    
    Use the launch template downloader we know works

--- docs/running/cliOptions.rst
+++ docs/running/cliOptions.rst
@@ -148,19 +148,22 @@ the logging module:
                         currently supported choices are 'aws' or 'gce'. The
                         default is None.
   --nodeTypes NODETYPES
-                        List of node types separated by commas. The syntax for
-                        each node type depends on the provisioner used. For
-                        the cgcloud and AWS provisioners this is the name of
-                        an EC2 instance type, optionally followed by a colon
-                        and the price in dollars to bid for a spot instance of
-                        that type, for example 'c3.8xlarge:0.42'. If no spot
-                        bid is specified, nodes of this type will be non-preemptable.
-                        It is acceptable to specify an instance as
-                        both preemptable and non-preemptable, including it
-                        twice in the list. In that case, preemptable nodes of
-                        that type will be preferred when creating new nodes
-                        once the maximum number of preemptable-nodes
-                        have been reached.
+                        List of worker node types separated by commas. The
+                        syntax for each node type depends on the provisioner
+                        used.  For the AWS provisioner this is a
+                        slash-separated list of equivalent EC2 instance types
+                        followed by a colon and the price in dollars to bid for
+                        a spot instance. For example:
+                        'c5.4xlarge/c5a.4xlarge:0.42'.
+                        If no spot bid is specified, nodes of this type will be
+                        non-preemptable (non-discounted and not subject to
+                        potential early termination based on the availability
+                        of discounted instances).  It is acceptable to specify
+                        an instance as both preemptable and non-preemptable,
+                        including it twice in the list. In that case,
+                        preemptable nodes of that type will be preferred when
+                        creating new nodes once the maximum number of
+                        preemptable-nodes has been reached.
   --minNodes MINNODES   Minimum number of nodes of each type in the cluster,
                         if using auto-scaling. This should be provided as a
                         comma-separated list of the same length as the list of
--- src/toil/common.py
+++ src/toil/common.py
@@ -32,6 +32,7 @@ from toil.batchSystems.options import (add_all_batchsystem_options,
 from toil.lib.humanize import bytes2human, human2bytes
 from toil.lib.retry import retry
 from toil.provisioners import (add_provisioner_options,
+                               parse_node_types,
                                check_valid_node_types,
                                cluster_factory)
 from toil.provisioners.aws import zone_to_region
@@ -77,7 +78,6 @@ class Config:
         # Autoscaling options
         self.provisioner = None
         self.nodeTypes = []
-        check_valid_node_types(self.provisioner, self.nodeTypes)
         self.minNodes = None
         self.maxNodes = [10]
         self.targetTime = defaultTargetTime
@@ -425,16 +425,16 @@ def addOptions(parser: ArgumentParser, config: Config = Config()):
                                           f"choices are {provisioner_choices}.  The default is {config.provisioner}.")
     autoscaling_options.add_argument('--nodeTypes', default=None,
                                      help="List of worker node types separated by commas. The syntax for each node "
-                                          "type depends on the provisioner used.  For the AWS provisioner this is the "
-                                          "name of an EC2 instance type, optionally followed by a colon and the price "
-                                          "in dollars to bid for a spot instance of that type.  For example: "
-                                          "'c3.8xlarge:0.42'.  If no spot bid is specified, nodes of this type will "
-                                          "be non-preemptable (non-discounted and not subject to potential early "
-                                          "termination based on the availability of discounted instances).  It is "
-                                          "acceptable to specify an instance as both preemptable and non-preemptable, "
-                                          "including it twice in the list. In that case, preemptable nodes of that "
-                                          "type will be preferred when creating new nodes once the maximum number of "
-                                          "preemptable-nodes has been reached.")
+                                          "type depends on the provisioner used.  For the AWS provisioner this is "
+                                          "a semicolon-separated list of equivalent EC2 instance types followed by "
+                                          "a colon and the price in dollars to bid for a spot instance. For example: "
+                                          "'c5.4xlarge;c5a.4xlarge:0.42'  If no spot bid is specified, nodes of this "
+                                          "type will be non-preemptable (non-discounted and not subject to potential "
+                                          "early termination based on the availability of discounted instances).  It "
+                                          "is acceptable to specify an instance as both preemptable and "
+                                          "non-preemptable, including it twice in the list. In that case, preemptable "
+                                          "nodes of that type will be preferred when creating new nodes once the "
+                                          "maximum number of preemptable-nodes has been reached.")
     autoscaling_options.add_argument('--minNodes', default=None,
                                      help="Mininum number of nodes of each type in the cluster, if using "
                                           "auto-scaling.  This should be provided as a comma-separated list of the "
@@ -873,7 +873,8 @@ class Toil:
                                                 nodeStorage=self.config.nodeStorage,
                                                 nodeStorageOverrides=self.config.nodeStorageOverrides,
                                                 sseKey=self.config.sseKey)
-            self._provisioner.setAutoscaledNodeTypes(self.config.nodeTypes)
+            node_types = parse_node_types(self.config.nodeTypes)
+            self._provisioner.setAutoscaledNodeTypes(node_types)
 
     @classmethod
     def getJobStore(cls, locator):
