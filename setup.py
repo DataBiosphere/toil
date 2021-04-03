d1ac34a083e1b58fcf4885bff72dbbd8ef642593
jonathanxu18 <jonathanxu.18@gmail.com>
2021-04-02 22:29:58 -0700

    Add datauri to setup.py

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ def run_setup():
     addict = 'addict>=2.2.1, <2.3'
     enlighten = 'enlighten>=1.5.2, <2'
     wdlparse = 'wdlparse==0.1.0'
+    datauri = 'python-datauri==1.0.0'
 
     core_reqs = [
         dill,
@@ -57,7 +58,8 @@ def run_setup():
         addict,
         pytz,
         pyyaml,
-        enlighten]
+        enlighten,
+        datauri]
     aws_reqs = [
         boto,
         boto3,
