85c1a332e831a57aaa46cc27652857759e88a68d
jonathanxu18 <jonathanxu.18@gmail.com>
2021-04-19 02:24:24 -0700

    Add typeshed as environment variable

--- contrib/admin/mypy-with-ignore.py
+++ contrib/admin/mypy-with-ignore.py
@@ -8,6 +8,7 @@ import os
 import subprocess
 import sys
 
+os.environ['MYPYPATH'] = 'contrib/typeshed'
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
