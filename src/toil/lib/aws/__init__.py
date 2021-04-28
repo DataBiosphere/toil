58ac59c8ff40bac3d640970371a23f6650d84cd2
DailyDreaming <lblauvel@ucsc.edu>
2021-04-28 10:52:50 -0700

    Add dependency stand alone test for cwl and wdl.

--- src/toil/lib/aws/__init__.py
+++ src/toil/lib/aws/__init__.py
@@ -25,3 +25,12 @@ def client(*args, **kwargs):
 def resource(*args, **kwargs):
     import boto3
     return boto3.resource(*args, **kwargs)
+
+
+def zone_to_region(zone: str):
+    """Get a region (e.g. us-west-2) from a zone (e.g. us-west-1c)."""
+    availability_zone = re.compile(r'^([a-z]{2}-[a-z]+-[1-9][0-9]*)([a-z])$')
+    m = availability_zone.match(zone)
+    if not m:
+        raise ValueError(f"Can't extract region from availability zone '{zone}'")
+    return m.group(1)
