d60769bc99de4a34b5393726721553f3f4cbae4c
jonathanxu18 <jonathanxu.18@gmail.com>
2021-08-24 16:47:57 -0700

    Adjust type hint

--- src/toil/batchSystems/registry.py
+++ src/toil/batchSystems/registry.py
@@ -78,7 +78,7 @@ BATCH_SYSTEM_FACTORY_REGISTRY = {
 BATCH_SYSTEMS = list(BATCH_SYSTEM_FACTORY_REGISTRY.keys())
 DEFAULT_BATCH_SYSTEM = 'single_machine'
 
-def addBatchSystemFactory(key: str, batchSystemFactory: Callable[[], Type[AbstractBatchSystem]]):
+def addBatchSystemFactory(key: str, batchSystemFactory: Callable[[], Type['AbstractBatchSystem']]):
     """
     Adds a batch system to the registry for workflow-supplied batch systems.
     """
