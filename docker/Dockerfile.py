6073cd4dee68b90262b3e803de09d9b29370fd07
Hexotical <lukaskoziol126@gmail.com>
2021-12-16 14:52:45 -0800

    Add python 3.10 tests

--- docker/Dockerfile.py
+++ docker/Dockerfile.py
@@ -30,7 +30,7 @@ dependencies = ' '.join(['libffi-dev',  # For client side encryption for extras 
                          f'{python}-dev',
                          'python3.8-distutils' if python == 'python3.8' else '',
                          'python3.9-distutils' if python == 'python3.9' else '',
-                         # 'python3.9-venv' if python == 'python3.9' else '',
+                         'python3.10-distutils' if python == 'python3.10' else '',
                          'python3-pip',
                          'libcurl4-openssl-dev',
                          'libssl-dev',
