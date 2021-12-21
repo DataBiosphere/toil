a5f1e3c48dff0a7ad0e6d29cb94b6832e3eb7d37
jonathanxu18 <jonathanxu.18@gmail.com>
2021-12-21 04:23:46 -0800

    Add test for ARM ami search

--- src/toil/test/lib/test_ec2.py
+++ src/toil/test/lib/test_ec2.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 
-@needs_aws_ec2
+#@needs_aws_ec2
 class AMITest(ToilTest):
     @classmethod
     def setUpClass(cls):
@@ -54,3 +54,11 @@ class AMITest(ToilTest):
             ami = aws_marketplace_flatcar_ami_search(self.ec2_client)
             self.assertEqual(len(ami), len('ami-02b46c73fed689d1c'))
             self.assertTrue(ami.startswith('ami-'))
+
+        with self.subTest('Test flatcar AMI finder architecture parameter.'):
+            amis = set()
+            for arch in ['amd64', 'arm64']:
+                ami = get_flatcar_ami(self.ec2_client, architecture=arch)
+                self.assertTrue(ami.startswith('ami-'))
+                amis.add(ami)
+            self.assertTrue(len(amis) == 2)
