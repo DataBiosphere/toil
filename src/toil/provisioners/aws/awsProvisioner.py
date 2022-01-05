6944886f98e5463e5285bce6f87dfc0c63bb30a8
jonathanxu18 <jonathanxu.18@gmail.com>
2022-01-04 20:24:52 -0800

    Fix exist check

--- src/toil/provisioners/aws/awsProvisioner.py
+++ src/toil/provisioners/aws/awsProvisioner.py
@@ -940,9 +940,10 @@ class AWSProvisioner(AbstractProvisioner):
         :return: The AMI ID (a string like 'ami-0a9a5d2b65cce04eb') for Flatcar.
         :rtype: str
         """
-        if self._architecture:
+        try:
             return get_flatcar_ami(self.aws.client(self._region, 'ec2'), self._architecture)
-        return get_flatcar_ami(self.aws.client(self._region, 'ec2'))
+        except:
+            return get_flatcar_ami(self.aws.client(self._region, 'ec2'))
 
     def _toNameSpace(self) -> str:
         assert isinstance(self.clusterName, (str, bytes))
