93e6b97db812daef7927aa4d76f1b75a09ff7b9f
William Gao <wlgao@ucsc.edu>
2022-04-02 12:44:05 -0700

    Change `toil kill` to use the job store shared file API to find pig.log
    Co-authored-by: Adam Novak <anovak@soe.ucsc.edu>

--- src/toil/utils/toilKill.py
+++ src/toil/utils/toilKill.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2021 Regents of the University of California
+# Copyright (C) 2015-2022 Regents of the University of California
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,8 +17,8 @@ import os
 import signal
 
 from toil.common import Config, Toil, parser_with_common_options
+from toil.jobStores.abstractJobStore import NoSuchJobStoreException
 from toil.statsAndLogging import set_logging_from_options
-from toil.jobStores.fileJobStore import FileJobStore
 
 logger = logging.getLogger(__name__)
 
@@ -30,32 +30,23 @@ def main() -> None:
     config = Config()
     config.setOptions(options)
 
-    job_store_type, _ = Toil.parseLocator(config.jobStore)
+    # Kill the pid recorded in the job store.
+    try:
+        job_store = Toil.resumeJobStore(config.jobStore)
+    except NoSuchJobStoreException:
+        logger.error("The job store %s does not exist.", config.jobStore)
+        return
 
-    if job_store_type != 'file':
-        # Remote (aws/google) jobstore; use the old (broken?) method
-        job_store = Toil.resumeJobStore(config.jobStore)
-        logger.info("Starting routine to kill running jobs in the toil workflow: %s", config.jobStore)
-        # TODO: This behaviour is now broken: https://github.com/DataBiosphere/toil/commit/a3d65fc8925712221e4cda116d1825d4a1e963a1
-        # There's no guarantee that the batch system in use can enumerate
-        # running jobs belonging to the job store we've attached to. And
-        # moreover we don't even bother trying to kill the leader at its
-        # recorded PID, even if it is a local process.
-        batch_system = Toil.createBatchSystem(job_store.config)  # Should automatically kill existing jobs, so we're good.
-        for job_id in batch_system.getIssuedBatchJobIDs():  # Just in case we do it again.
-            batch_system.killBatchJobs([job_id])
-        logger.info("All jobs SHOULD have been killed")
-    else:
-        # otherwise, kill the pid recorded in the jobstore.
-        # TODO: We assume thnis is a local PID.
-        job_store = Toil.resumeJobStore(config.jobStore)
-        assert isinstance(job_store, FileJobStore), "Need a FileJobStore which has a sharedFilesDir"
-        pid_log = os.path.join(job_store.sharedFilesDir, 'pid.log')
-        with open(pid_log) as f:
-            pid_to_kill = f.read().strip()
-        try:
-            os.kill(int(pid_to_kill), signal.SIGTERM)
-            logger.info("Toil process %s successfully terminated." % str(pid_to_kill))
-        except OSError:
-            logger.error("Toil process %s could not be terminated." % str(pid_to_kill))
-            raise
+    with job_store.read_shared_file_stream("pid.log") as f:
+        pid_to_kill = int(f.read().strip())
+
+    # TODO: We assume this is a local PID so we can't kill the leader if the
+    #  process is on a different machine. We also rely on the leader to be
+    #  responsive to clean up all its jobs.
+
+    try:
+        os.kill(pid_to_kill, signal.SIGTERM)
+        logger.info(f"Toil process %i successfully terminated.", pid_to_kill)
+    except OSError:
+        logger.error(f"Toil process %i could not be terminated.", pid_to_kill)
+        raise
