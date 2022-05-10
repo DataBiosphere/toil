f2149df3525481bc2d3ac73fc0df39716b9f9e7c
Adam Novak <anovak@soe.ucsc.edu>
2022-05-10 18:05:25 -0400

    Try to work around broken package source

--- docker/Dockerfile.py
+++ docker/Dockerfile.py
@@ -91,6 +91,10 @@ print(heredoc('''
     # make sure we don't use too new a version of setuptools (which can get out of sync with poetry and break things)
     ENV SETUPTOOLS_USE_DISTUTILS=stdlib
 
+    # Try to avoid "Failed to fetch ...  Undetermined Error" from apt
+    # See <https://stackoverflow.com/a/66523384>
+    RUN printf 'Acquire::http::Pipeline-Depth "0";\nAcquire::http::No-Cache=True;\nAcquire::BrokenProxy=true;\n' >/etc/apt/apt.conf.d/99fixbadproxy
+
     RUN apt-get -y update --fix-missing && apt-get -y upgrade && apt-get -y install apt-transport-https ca-certificates software-properties-common curl && apt-get clean && rm -rf /var/lib/apt/lists/*
 
     RUN add-apt-repository -y ppa:deadsnakes/ppa
