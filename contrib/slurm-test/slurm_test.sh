44b881fc1c03c7e8ad40bb3379bb08e8e6230193
Hexotical <lukaskoziol126@gmail.com>
2022-08-23 14:13:31 -0700

    Remove other tests from gitlab for speeed

--- contrib/slurm-test/slurm_test.sh
+++ contrib/slurm-test/slurm_test.sh
@@ -7,7 +7,7 @@ GIT_COMMIT=$(git rev-parse HEAD)
 docker exec slurm-test_slurmmaster_1 sudo apt install python3-pip -y
 docker exec slurm-test_slurmmaster_1 pip3 install "git+https://github.com/DataBiosphere/toil.git@${GIT_COMMIT}"
 docker exec slurm-test_slurmmaster_1 sinfo -N -l
-docker exec slurm-test_slurmmaster_1 python3 /home/admin/toilScript.py file:my-job-store --batchSystem slurm --disableCaching --retryCount 0
+docker exec slurm-test_slurmmaster_1 python3 /home/admin/toil_workflow.py file:my-job-store --batchSystem slurm --disableCaching --retryCount 0
 docker cp slurm-test_slurmmaster_1:/home/admin/output.txt output_Docker.txt
 docker-compose stop
 ./check_out.sh
