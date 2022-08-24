dee83dd208b787ece41cc3463d3940e9d395cdcc
Hexotical <lukaskoziol126@gmail.com>
2022-08-24 13:41:41 -0700

    Add sort.py workflow run to slurm testing

--- contrib/slurm-test/slurm_test.sh
+++ contrib/slurm-test/slurm_test.sh
@@ -5,6 +5,8 @@ docker ps
 docker cp toil_workflow.py slurm-test_slurmmaster_1:/home/admin
 #Assumes sort.py copied to this directory during gitlab run for ease of access
 docker cp sort.py slurm-test_slurmmaster_1:/home/admin
+docker cp fileToSort.py slurm-test_slurmmaster_1:/home/admin
+docker cp toil_workflow.py slurm-test_slurmmaster_1:/home/admin
 GIT_COMMIT=$(git rev-parse HEAD)
 docker exec slurm-test_slurmmaster_1 sudo apt install python3-pip -y
 docker exec slurm-test_slurmmaster_1 pip3 install "git+https://github.com/DataBiosphere/toil.git@${GIT_COMMIT}"
@@ -12,6 +14,7 @@ docker exec slurm-test_slurmmaster_1 sinfo -N -l
 docker exec slurm-test_slurmmaster_1 python3 /home/admin/toil_workflow.py file:my-job-store --batchSystem slurm --disableCaching --retryCount 0
 docker cp slurm-test_slurmmaster_1:/home/admin/output.txt output_Docker.txt
 docker exec slurm-test_slurmmaster_1 python3 /home/admin/sort.py file:my-job-store --batchSystem slurm --disableCaching --retryCount 0
+docker cp slurm-test_slurmmaster_1:/home/admin/sortedFile.txt sortedFile.txt
 docker-compose stop
 ./check_out.sh
 rm sort.py
