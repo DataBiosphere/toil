953561044797e17831fdc2637789a614f0d1c46b
Adam Novak <anovak@soe.ucsc.edu>
2022-08-29 18:15:41 -0400

    Tolerate vanishing bucket

--- src/toil/jobStores/aws/jobStore.py
+++ src/toil/jobStores/aws/jobStore.py
@@ -1213,15 +1213,20 @@ class AWSJobStore(AbstractJobStore):
 
                         for i in range(CONSISTENCY_TICKS):
                             # Sometimes we can create a multipart upload and not see it. Wait around for it.
-                            response = client.list_multipart_uploads(Bucket=bucket_name,
-                                                                     MaxUploads=1,
-                                                                     Prefix=compat_bytes(info.fileID))
-                            if 'Uploads' in response and len(response['Uploads']) != 0 and response['Uploads'][0]['UploadId'] == uploadId:
-                                logger.debug('Multipart upload visible as %s', uploadId)
-                                break
-                            else:
-                                logger.debug('Multipart upload %s is not visible; we see %s', uploadId, response.get('Uploads'))
-                                time.sleep(CONSISTENCY_TIME * 2 ** i)
+                            try:
+                                response = client.list_multipart_uploads(Bucket=bucket_name,
+                                                                         MaxUploads=1,
+                                                                         Prefix=compat_bytes(info.fileID))
+                                if 'Uploads' in response and len(response['Uploads']) != 0 and response['Uploads'][0]['UploadId'] == uploadId:
+                                    logger.debug('Multipart upload visible as %s', uploadId)
+                                    break
+                            except s3_boto3_client.exceptions.NoSuchBucket:
+                                # Sometimes we can make the multipart upload
+                                # but then not see the bucket when we look for
+                                # the upload.
+                                pass
+                            logger.debug('Multipart upload %s is not visible; we see %s', uploadId, response.get('Uploads'))
+                            time.sleep(CONSISTENCY_TIME * 2 ** i)
 
                         try:
                             for part_num in itertools.count():
