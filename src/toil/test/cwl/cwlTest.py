54e8027be7ab7e001fdbd1bdb5611a0d1e5d4161
Adam Novak <anovak@soe.ucsc.edu>
2022-10-19 12:30:09 -0400

    Enable job retries on Kubernetes for CWL tests to fix #4236

--- src/toil/test/cwl/cwlTest.py
+++ src/toil/test/cwl/cwlTest.py
@@ -668,6 +668,7 @@ class CWLv10Test(ToilTest):
     def test_kubernetes_cwl_conformance(self, **kwargs):
         return self.test_run_conformance(
             batchSystem="kubernetes",
+            extra_args=["--retryCount=3"],
             # This test doesn't work with
             # Singularity; see
             # https://github.com/common-workflow-language/cwltool/blob/7094ede917c2d5b16d11f9231fe0c05260b51be6/conformance-test.sh#L99-L117
@@ -756,6 +757,7 @@ class CWLv11Test(ToilTest):
     def test_kubernetes_cwl_conformance(self, **kwargs):
         return self.test_run_conformance(
             batchSystem="kubernetes",
+            extra_args=["--retryCount=3"],
             # These tests don't work with
             # Singularity; see
             # https://github.com/common-workflow-language/cwltool/blob/7094ede917c2d5b16d11f9231fe0c05260b51be6/conformance-test.sh#L99-L117
@@ -825,6 +827,7 @@ class CWLv12Test(ToilTest):
             )
         return self.test_run_conformance(
             batchSystem="kubernetes",
+            extra_args=["--retryCount=3"],
             # This test doesn't work with
             # Singularity; see
             # https://github.com/common-workflow-language/cwltool/blob/7094ede917c2d5b16d11f9231fe0c05260b51be6/conformance-test.sh#L99-L117
