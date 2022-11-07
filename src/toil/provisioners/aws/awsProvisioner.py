e48007a5e79feb2d45a1d41f47cd93eebf9ece4d
William Gao <williamgao520@gmail.com>
2022-11-07 11:54:49 -0800

    `toil destroy-cluster`: Terminate nodes in stopped state

--- src/toil/provisioners/aws/awsProvisioner.py
+++ src/toil/provisioners/aws/awsProvisioner.py
@@ -619,7 +619,7 @@ class AWSProvisioner(AbstractProvisioner):
         # Do the workers after the ASGs because some may belong to ASGs
         logger.info('Terminating any remaining workers ...')
         removed = False
-        instances = self._getNodesInCluster(both=True)
+        instances = self._getNodesInCluster(both=True, include_stopped_nodes=True)
         spotIDs = self._getSpotRequestIDs()
         if spotIDs:
             self.aws.boto2(self._region, 'ec2').cancel_spot_instance_requests(request_ids=spotIDs)
@@ -925,7 +925,7 @@ class AWSProvisioner(AbstractProvisioner):
         """
         Get the Boto 2 instance for the cluster's leader.
         """
-        instances = self._getNodesInCluster(both=True)
+        instances = self._getNodesInCluster(both=True, include_stopped_nodes=True)
         instances.sort(key=lambda x: x.launch_time)
         try:
             leader = instances[0]  # assume leader was launched first
@@ -988,7 +988,7 @@ class AWSProvisioner(AbstractProvisioner):
         self._terminateIDs(instanceIDs)
         logger.info('... Waiting for instance(s) to shut down...')
         for instance in instances:
-            wait_transition(instance, {'pending', 'running', 'shutting-down'}, 'terminated')
+            wait_transition(instance, {'pending', 'running', 'shutting-down', 'stopping', 'stopped'}, 'terminated')
         logger.info('Instance(s) terminated.')
 
     @awsRetry
@@ -1089,17 +1089,21 @@ class AWSProvisioner(AbstractProvisioner):
         return bdms
 
     @awsRetry
-    def _getNodesInCluster(self, instance_type: Optional[str] = None, preemptable=False, both=False) -> List[Boto2Instance]:
+    def _getNodesInCluster(self, instance_type: Optional[str] = None, preemptable=False, both=False, include_stopped_nodes=False) -> List[Boto2Instance]:
         """
         Get Boto2 instance objects for all nodes in the cluster.
         """
 
         allInstances = self.aws.boto2(self._region, 'ec2').get_only_instances(filters={'instance.group-name': self.clusterName})
+
         def instanceFilter(i):
             # filter by type only if nodeType is true
             rightType = not instance_type or i.instance_type == instance_type
             rightState = i.state == 'running' or i.state == 'pending'
+            if include_stopped_nodes:
+                rightState = rightState or i.state == 'stopping' or i.state == 'stopped'
             return rightType and rightState
+
         filteredInstances = [i for i in allInstances if instanceFilter(i)]
         if not preemptable and not both:
             return [i for i in filteredInstances if i.spot_instance_request_id is None]
