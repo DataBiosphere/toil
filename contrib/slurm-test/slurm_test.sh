894d0f6d024922a58e23f05c6e1db0a2b293b090
Adam Novak <anovak@soe.ucsc.edu>
2023-05-04 15:06:00 -0400

    Use docker-compose in the prebake

--- contrib/slurm-test/slurm_test.sh
+++ contrib/slurm-test/slurm_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-docker-compose up -d
+docker compose up -d
 docker ps
 docker cp toil_workflow.py slurm-test_slurmmaster_1:/home/admin
 docker cp -L sort.py slurm-test_slurmmaster_1:/home/admin
@@ -16,7 +16,7 @@ docker cp slurm-test_slurmmaster_1:/home/admin/output.txt output_Docker.txt
 # Test 2: Make sure that "sort" workflow runs under slurm
 docker exec slurm-test_slurmmaster_1 python3 /home/admin/sort.py file:my-job-store --batchSystem slurm --disableCaching --retryCount 0
 docker cp slurm-test_slurmmaster_1:/home/admin/sortedFile.txt sortedFile.txt
-docker-compose stop
+docker compose stop
 ./check_out.sh
 rm sort.py
 echo "Sucessfully ran workflow on slurm cluster"
