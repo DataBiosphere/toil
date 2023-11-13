6140dd3b52d7224a9b2ae864c9e2881272292a0d
DailyDreaming <lblauvel@ucsc.edu>
2023-11-13 09:46:30 -0800

    Linting.

--- src/toil/wdl/utils.py
+++ src/toil/wdl/utils.py
@@ -11,7 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-def get_version(iterable) -> str:
+from typing import Iterable
+
+
+def get_version(iterable: Iterable[str]) -> str:
     """
     Get the version of the WDL document.
 
