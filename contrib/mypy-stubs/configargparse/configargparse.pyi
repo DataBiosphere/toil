4a42b4134d7a0feb23e0ed730e7b55b768de3cb2
stxue1 <stxue@ucsc.edu>
2023-11-17 13:08:16 -0800

    deal with mypy

--- contrib/mypy-stubs/configargparse/configargparse.pyi
+++ contrib/mypy-stubs/configargparse/configargparse.pyi
@@ -27,6 +27,7 @@ class ArgumentParser(argparse.ArgumentParser):
     def __init__(self, *args: Any, **kwargs: Any) -> None: ...
     # There may be a better way of type hinting this without a type: ignore, but mypy gets unhappy pretty much no matter what as the signatures for parse_args doesn't match with its superclass in argparse
     def parse_args(self, args: Sequence[str] | None = None, namespace: Namespace | None = None, config_file_contents: str | None = None, env_vars: Any = None) -> Namespace: ...  # type: ignore[override]
+    def parse_known_args(self, args: Sequence[str] | None = ..., namespace: Namespace | None = ..., config_file_contents: Any = ..., env_vars: Any = ..., ignore_help_args: bool | None = ...) -> tuple[Namespace, list[str]]: ...  # type: ignore[override]
     def get_source_to_settings_dict(self) -> OrderedDict[Any, Any]: ...
 
 Namespace = argparse.Namespace
