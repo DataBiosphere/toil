af736d413e8c1e3a58aed0810f937ff7cc079a93
stxue1 <stxue@ucsc.edu>
2024-01-11 13:58:14 -0800

    Change docker security rules, remove --containall on singularity, add tzdata as dependency

--- docker/Dockerfile.py
+++ docker/Dockerfile.py
@@ -61,7 +61,9 @@ dependencies = ' '.join(['libffi-dev',  # For client side encryption for extras 
                          'libcurl4-nss-dev',
                          'libapr1',
                          # Dependencies for singularity
-                         'containernetworking-plugins'])
+                         'containernetworking-plugins',
+                         # Dependencies for singularity on kubernetes
+                         'tzdata'])
 
 
 def heredoc(s):
@@ -213,4 +215,6 @@ print(heredoc('''
 
     RUN echo '[ ! -z "$TERM" -a -r /etc/motd ] && cat /etc/motd' >> /etc/bash.bashrc \
         && printf '{motd}' > /etc/motd
+    
+    RUN ln -s /usr/share/zoneinfo/UTC /etc/localtime
 '''))
