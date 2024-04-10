1cd00cb9b57610df0c60372f5acaa034134d0fea
stxue1 <stxue@ucsc.edu>
2024-04-10 07:55:05 -0700

    Remove commented dead import

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# import imp
 import os
 import types
 from importlib.machinery import SourceFileLoader
