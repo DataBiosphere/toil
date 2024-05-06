05ade745fcfdf05cdee0d5af8fc075194e64e1cf
Lon Blauvelt <lblauvel@ucsc.edu>
2024-05-06 09:13:36 -0700

    Revert "Update to Python 3.12 (#4901)" (#4917)
    This reverts commit 460846d7ded3820acc505cccb9c866ea9a7a940a.

--- src/toil/test/utils/utilsTest.py
+++ src/toil/test/utils/utilsTest.py
@@ -56,7 +56,6 @@ class UtilsTest(ToilTest):
         super().setUp()
         self.tempDir = self._createTempDir()
         self.tempFile = get_temp_file(rootDir=self.tempDir)
-        self.outputFile = get_temp_file(rootDir=self.tempDir)
         self.outputFile = 'someSortedStuff.txt'
         self.toilDir = os.path.join(self.tempDir, "jobstore")
         self.assertFalse(os.path.exists(self.toilDir))
@@ -74,9 +73,9 @@ class UtilsTest(ToilTest):
             '-m',
             'toil.test.sort.sort',
             f'file:{self.toilDir}',
-            f'--fileToSort={self.tempFile}',
-            f'--outputFile={self.outputFile}',
             '--clean=never',
+            '--numLines=1',
+            '--lineLength=1'
         ]
 
         self.restart_sort_workflow_cmd = [
@@ -92,7 +91,7 @@ class UtilsTest(ToilTest):
         if os.path.exists(self.toilDir):
             shutil.rmtree(self.toilDir)
 
-        for f in [self.tempFile, self.outputFile, os.path.join(self.tempDir, "output.txt")]:
+        for f in ['fileToSort.txt', 'sortedFile.txt', 'output.txt']:
             if os.path.exists(f):
                 os.remove(f)
 
@@ -314,20 +313,8 @@ class UtilsTest(ToilTest):
 
     def testGetPIDStatus(self):
         """Test that ToilStatus.getPIDStatus() behaves as expected."""
-        print(f"I am {self.toilDir}")
-        logger.info(f"I am {self.toilDir}")
         wf = subprocess.Popen(self.sort_workflow_cmd)
-
-        for t in range(0, 6):
-            if os.path.exists(self.toilDir):
-                logger.error(f"I am {self.toilDir} and my jobstore exists")
-            else:
-                logger.error(f"I am {self.toilDir} and my jobstore DOES NOT exists")
-            time.sleep(5)
-            logger.critical(f"PID status: {ToilStatus.getPIDStatus(self.toilDir)}")
-            logger.info(f"Slept for {t*5}")
-
-        self.check_status('RUNNING', status_fn=ToilStatus.getPIDStatus, seconds=60)
+        self.check_status('RUNNING', status_fn=ToilStatus.getPIDStatus, seconds=20)
         wf.wait()
         self.check_status('COMPLETED', status_fn=ToilStatus.getPIDStatus)
 
@@ -343,19 +330,7 @@ class UtilsTest(ToilTest):
         opportunity to test the 'RUNNING' functionality of getStatus().
         """
         # --badWorker is set to force failure.
-        print(f"I am {self.toilDir}")
-        logger.info(f"I am {self.toilDir}")
         wf = subprocess.Popen(self.sort_workflow_cmd + ['--badWorker=1'])
-
-        for t in range(0, 6):
-            if os.path.exists(self.toilDir):
-                logger.error(f"I am {self.toilDir} and my jobstore exists")
-            else:
-                logger.error(f"I am {self.toilDir} and my jobstore DOES NOT exists")
-            time.sleep(5)
-            logger.critical(f"PID status: {ToilStatus.getStatus(self.toilDir)}")
-            logger.info(f"Slept for {t*5}")
-
         self.check_status('RUNNING', status_fn=ToilStatus.getStatus)
         wf.wait()
         self.check_status('ERROR', status_fn=ToilStatus.getStatus)
@@ -365,19 +340,9 @@ class UtilsTest(ToilTest):
     def testGetStatusFailedCWLWF(self):
         """Test that ToilStatus.getStatus() behaves as expected with a failing CWL workflow."""
         # --badWorker is set to force failure.
-        print(f"I am {self.toilDir}")
-        logger.info(f"I am {self.toilDir}")
         cmd = ['toil-cwl-runner', '--jobStore', self.toilDir, '--clean=never', '--badWorker=1',
-               'src/toil/test/cwl/sorttool.cwl', '--reverse', '--input', 'src/toil/test/cwl/whale.txt', f'--outdir={self.tempDir}']
+               'src/toil/test/cwl/sorttool.cwl', '--reverse', '--input', 'src/toil/test/cwl/whale.txt']
         wf = subprocess.Popen(cmd)
-        for t in range(0, 6):
-            if os.path.exists(self.toilDir):
-                logger.error(f"I am {self.toilDir} and my jobstore exists")
-            else:
-                logger.error(f"I am {self.toilDir} and my jobstore DOES NOT exists")
-            time.sleep(5)
-            logger.critical(f"PID status: {ToilStatus.getStatus(self.toilDir)}")
-            logger.info(f"Slept for {t*5}")
         self.check_status('RUNNING', status_fn=ToilStatus.getStatus)
         wf.wait()
         self.check_status('ERROR', status_fn=ToilStatus.getStatus)
@@ -386,19 +351,9 @@ class UtilsTest(ToilTest):
     @needs_docker
     def testGetStatusSuccessfulCWLWF(self):
         """Test that ToilStatus.getStatus() behaves as expected with a successful CWL workflow."""
-        print(f"I am {self.toilDir}")
-        logger.info(f"I am {self.toilDir}")
         cmd = ['toil-cwl-runner', '--jobStore', self.toilDir, '--clean=never',
-               'src/toil/test/cwl/sorttool.cwl', '--reverse', '--input', 'src/toil/test/cwl/whale.txt', f'--outdir={self.tempDir}']
+               'src/toil/test/cwl/sorttool.cwl', '--reverse', '--input', 'src/toil/test/cwl/whale.txt']
         wf = subprocess.Popen(cmd)
-        for t in range(0, 6):
-            if os.path.exists(self.toilDir):
-                logger.error(f"I am {self.toilDir} and my jobstore exists")
-            else:
-                logger.error(f"I am {self.toilDir} and my jobstore DOES NOT exists")
-            time.sleep(5)
-            logger.critical(f"PID status: {ToilStatus.getStatus(self.toilDir)}")
-            logger.info(f"Slept for {t*5}")
         self.check_status('RUNNING', status_fn=ToilStatus.getStatus, seconds=20)
         wf.wait()
         self.check_status('COMPLETED', status_fn=ToilStatus.getStatus)
