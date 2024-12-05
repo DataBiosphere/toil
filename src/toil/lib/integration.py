189c2315bae36eccb0644b7df0c481040e243628
Adam Novak <anovak@soe.ucsc.edu>
2024-12-05 17:49:11 -0500

    Sketch out how we might send some metrics one at a time

--- src/toil/lib/integration.py
+++ src/toil/lib/integration.py
@@ -17,14 +17,16 @@ Contains functions for integrating Toil with external services such as
 Dockstore.
 """
 
+import datetime
 import hashlib
 import logging
 import os
 import shutil
 import sys
 import tempfile
+import uuid
 import zipfile
-from typing import Any, Dict, List, Optional, Set, Tuple, cast
+from typing import Any, Literal, Optional, Union, TypedDict, cast
 
 from urllib.parse import urlparse, unquote, quote
 import requests
@@ -187,7 +189,7 @@ def find_workflow_on_dockstore(workflow: str, supported_languages: Optional[set[
 
     # If we don't like what we found we compose a useful error message.
     problems: list[str] = []
-    problem_type = RuntimeError
+    problem_type: type[Exception] = RuntimeError
     if trs_version is None:
         problems.append(f"Workflow {workflow} does not specify a version")
     elif trs_version not in workflow_versions:
@@ -384,6 +386,64 @@ def resolve_workflow(workflow: str, supported_languages: Optional[set[str]] = No
         # Pass other things through.
         return workflow
 
+# TODO: This is a https://schema.org/CompletedActionStatus
+# But what are the possible values?
+ExecutionStatus = Union[Literal["SUCCESSFUL"], Literal["FAILED"]]
+
+class RunExecution(TypedDict):
+    """
+    Dockstore metrics data for a workflow run.
+    """
+    executionId: str
+    """
+    Executor-generated unique execution ID.
+    """
+    dateExecuted: str
+    """
+    ISO 8601 UTC timestamp whr when the execution happend.
+
+    TODO: Is this start or end?
+    """
+
+    executionStatus: ExecutionStatus
+    """
+    Did the execution work?
+    """
+
+    # TODO: additionalProperties
+
+
+def send_workflow_metrics_to_dockstore(trs_workflow_id: str, trs_version: str, execution_id: uuid.UUID, succeeded: bool) -> None:
+    """
+    Send the status of a workflow execution to Dockstore.
+    
+    Assumes the workflow was executed now.
+    """
+
+    # Pack up into a RunExecution
+    execution = RunExecution(
+        executionId=str(execution_id),
+        dateExecuted=datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        executionStatus="SUCCESSFUL" if succeeded else "FAILED"
+    )
+
+    # Aggregate into a submission
+    to_post = {
+        "runExecutions": [execution],
+        "taskExecutions": [],
+        "validationExecutions": []
+    }
+
+    # Set the submission query string metadata
+    submission_params = {
+        "platform": "OTHER", # TODO: Should this be TOIL?
+        "description": "Workflow status from Toil"
+    }
+    
+    # TODO: Point at QA
+    endpoint_url = f"https://dockstore.org/api/ga4gh/v2/extended/{quote(trs_workflow_id, safe='')}/versions/{quote(trs_version, safe='')}/executions"
+
+    requests.post(endpoint_url, params=submission_params, json=to_post)
 
 
 
