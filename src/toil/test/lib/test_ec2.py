ad206081e64616bac1dc35ed6916e6f49b8f6f1c
Adam Novak <anovak@soe.ucsc.edu>
2025-01-30 15:27:17 -0500

    Tolerate not finding Flatcar releases in CI (#5201)
    * Use one outer retry loop and tolerate not finding Flatcar releases
    
    * Remove cruft and no-ops.
    
    * Remove cruft and no-ops.
    
    ---------
    
    Co-authored-by: DailyDreaming <lblauvel@ucsc.edu>

--- src/toil/test/lib/test_ec2.py
+++ src/toil/test/lib/test_ec2.py
@@ -14,13 +14,14 @@
 import logging
 import os
 
-import pytest
+from unittest import mock
 
 from toil.lib.aws.ami import (
     aws_marketplace_flatcar_ami_search,
     feed_flatcar_ami_release,
     flatcar_release_feed_amis,
     get_flatcar_ami,
+    ReleaseFeedUnavailableError
 )
 from toil.test import ToilTest, needs_aws_ec2, needs_online
 
@@ -69,15 +70,18 @@ class AMITest(ToilTest):
 
     def test_fetch_flatcar(self):
         with self.subTest("Test flatcar AMI from user is prioritized."):
-            os.environ["TOIL_AWS_AMI"] = "overridden"
-            ami = get_flatcar_ami(self.ec2_client)
-            self.assertEqual(ami, "overridden")
-            del os.environ["TOIL_AWS_AMI"]
+            with mock.patch.dict(os.environ, {"TOIL_AWS_AMI": "overridden"}):
+                ami = get_flatcar_ami(self.ec2_client)
+                self.assertEqual(ami, "overridden")
 
         with self.subTest("Test flatcar AMI returns an AMI-looking AMI."):
-            ami = get_flatcar_ami(self.ec2_client)
-            self.assertEqual(len(ami), len("ami-02b46c73fed689d1c"))
-            self.assertTrue(ami.startswith("ami-"))
+            try:
+                ami = get_flatcar_ami(self.ec2_client)
+                self.assertEqual(len(ami), len("ami-02b46c73fed689d1c"))
+                self.assertTrue(ami.startswith("ami-"))
+            except ReleaseFeedUnavailableError:
+                # Ignore any remote systems being down.
+                pass
 
         with self.subTest(
             "Test feed_flatcar_ami_release() returns an AMI-looking AMI."
@@ -90,16 +94,14 @@ class AMITest(ToilTest):
             "Test aws_marketplace_flatcar_ami_search() returns an AMI-looking AMI."
         ):
             ami = aws_marketplace_flatcar_ami_search(self.ec2_client)
-            self.assertEqual(len(ami), len("ami-02b46c73fed689d1c"))
-            self.assertTrue(ami.startswith("ami-"))
+            self.assertTrue(ami is None or len(ami), len("ami-02b46c73fed689d1c"))
+            self.assertTrue(ami is None or ami.startswith("ami-"))
 
-    # TODO: This will fail until https://github.com/flatcar/Flatcar/issues/962 is fixed
-    @pytest.mark.xfail
     def test_fetch_arm_flatcar(self):
         """Test flatcar AMI finder architecture parameter."""
-        amis = set()
-        for arch in ["amd64", "arm64"]:
-            ami = get_flatcar_ami(self.ec2_client, architecture=arch)
+        try:
+            ami = get_flatcar_ami(self.ec2_client, architecture="arm64")
             self.assertTrue(ami.startswith("ami-"))
-            amis.add(ami)
-        self.assertTrue(len(amis) == 2)
+        except ReleaseFeedUnavailableError:
+            # Ignore any remote systems being down.
+            pass
