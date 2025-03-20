1ee1d86d40f32446365201d35344682398d300b6
Michael R. Crusoe <michael.crusoe@gmail.com>
2025-03-20 19:59:29 +0100

    test/src/regularLogTest.py: add typing, switch to pytest from unittest

--- contrib/admin/mypy-with-ignore.py
+++ contrib/admin/mypy-with-ignore.py
@@ -98,7 +98,6 @@ def main():
         'src/toil/test/src/retainTempDirTest.py',
         'src/toil/test/src/realtimeLoggerTest.py',
         'src/toil/test/src/promisedRequirementTest.py',
-        'src/toil/test/src/regularLogTest.py',
         'src/toil/test/src/resumabilityTest.py',
         'src/toil/test/src/jobDescriptionTest.py',
         'src/toil/test/src/autoDeploymentTest.py',
