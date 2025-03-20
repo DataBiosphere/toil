cfef86bc16601bacb998430493bb86df915bf0af
Michael R. Crusoe <michael.crusoe@gmail.com>
2025-03-20 18:20:14 +0100

    test/src/jobTest.py: add typing, switch to pytest from unittest

--- contrib/admin/mypy-with-ignore.py
+++ contrib/admin/mypy-with-ignore.py
@@ -107,7 +107,6 @@ def main():
         'src/toil/test/src/fileStoreTest.py',
         'src/toil/test/src/__init__.py',
         'src/toil/test/src/systemTest.py',
-        'src/toil/test/src/jobTest.py',
         'src/toil/test/src/restartDAGTest.py',
         'src/toil/test/src/promisesTest.py',
         'src/toil/test/src/miscTests.py',
