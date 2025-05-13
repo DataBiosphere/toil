526d92256cf042226d8c849a4aaa735a851eb87e
hailey <hailey@Haileys-MacBook-Pro-2.local>
2025-05-13 15:09:03 -0700

    test public url access class methods

--- src/toil/test/lib/test_url.py
+++ src/toil/test/lib/test_url.py
@@ -23,12 +23,43 @@ logging.basicConfig(level=logging.DEBUG)
 
 class URLTest(ToilTest):
     """
-    Make sure we can get something for a user name when user names are not
-    available.
+    Test URLAccess class handling read, list,
+    and checking the size/existence of resources at given URL
     """
 
     def test_get_url_access(self):
         url = URLAccess()
         self.assertTrue(url.url_exists("https://raw.githubusercontent.com/DataBiosphere/toil/refs/heads/master/src/toil/batchSystems/abstractBatchSystem.py"))
 
+    def test_get_size(self):
+        url = URLAccess()
+        size = url.get_size("s3://toil-datasets/hello.txt")
+        self.assertIsInstance(size, int)
+        self.assertGreater(size, 0)
 
+    def test_get_is_directory(self):
+        url = URLAccess()
+        self.assertFalse(url.get_is_directory("s3://toil-datasets/hello.txt"))
+
+    def test_list_url(self):
+        url = URLAccess()
+        test_dir = url.list_url("s3://1000genomes/")
+        self.assertIsInstance(test_dir, list)
+        self.assertGreater(len(test_dir), 0)
+
+    def test_read_from_url(self):
+        import io
+        url = URLAccess()
+        output = io.BytesIO()
+        size, executable = url.read_from_url("s3://toil-datasets/hello.txt", output)
+        self.assertIsInstance(size, int)
+        self.assertGreater(size, 0)
+        self.assertFalse(executable)
+        self.assertGreater(len(output.getvalue()), 0)
+
+    def test_open_url(self):
+        url = URLAccess()
+        with url.open_url("s3://toil-datasets/hello.txt") as readable:
+            content = readable.read()
+            self.assertIsInstance(content, bytes)
+            self.assertGreater(len(content), 0)
