e78d38ecde2f45e8f03916ce1d1c6df8ce67336a
Adam Novak <anovak@soe.ucsc.edu>
2026-03-26 16:59:36 -0400

    Use edit distance module to try and constrain hinted string entropy

--- src/toil/test/jobStores/jobStoreTest.py
+++ src/toil/test/jobStores/jobStoreTest.py
@@ -33,6 +33,7 @@ from urllib.request import Request, urlopen
 
 import pytest
 from stubserver import FTPStubServer
+import edit_distance
 
 from toil.common import Config
 from toil.fileStores import FileID
@@ -603,6 +604,8 @@ class AbstractJobStoreTest:
             assert "lions" in other_many_hint_id
             assert "tigers" in other_many_hint_id
             assert "bears" in other_many_hint_id
+
+            assert edit_distance.edit_distance(many_hint_id, other_many_hint_id) < 3, "IDs using hints must be low-entropy and human-findable, while not colliding even across nodes!"
 
             forbidden_hint_id = self.jobstore_initialized.write_file(str(get_a_file()), hints=["", "whales", "/", """
             Call me Ishmael. Some years ago⁠—never mind how long precisely⁠—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off⁠—then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.
